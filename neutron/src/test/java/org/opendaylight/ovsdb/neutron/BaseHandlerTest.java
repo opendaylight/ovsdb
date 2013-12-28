83843759ba54e66de99ca6092c9b5d4ed0bf8b45
Dave Tucker <dave@dtucker.co.uk>
2013-12-28 20:19:37 +0000

    Added Mockito and PowerMock to the neutron pom.xml Unit test coverage for AdminConfigManager Unit test coverage for NetworkHandler
    Change-Id: Ice7582e2a9f4dd5a4b7c9fdac01e86196f48ccf4
    Signed-off-by: Dave Tucker <dave@dtucker.co.uk>

--- neutron/src/test/java/org/opendaylight/ovsdb/neutron/BaseHandlerTest.java
+++ neutron/src/test/java/org/opendaylight/ovsdb/neutron/BaseHandlerTest.java
@@ -6,39 +6,72 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Dave Tucker (HP) - Added unit tests for the BaseHandler class
+ *    Dave Tucker (HP) - Added unit tests for the BaseHandler class.
+ *                     - Migrated test cases to JUnit4
  *******************************************************************************/
 
 package org.opendaylight.ovsdb.neutron;
 
-import junit.framework.TestCase;
+import java.net.HttpURLConnection;
 
-public class BaseHandlerTest extends TestCase {
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
-    BaseHandler test = new BaseHandler();
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import org.opendaylight.controller.sal.utils.Status;
+import org.opendaylight.controller.sal.utils.StatusCode;
+
+@RunWith(JUnit4.class)
+public class BaseHandlerTest {
 
     String nullString = null;
     String uuid = "ce044452-f22e-4ea4-a3ec-d1cde80cf996";
     String tenantId = "8d62bfa112fb4247aa20edc74235c1ce";
     String neutronId = "6b8fd2";
 
-    public void testIsValidNeutronID() throws Exception {
-        assertFalse(test.isValidNeutronID(nullString));
-        assertTrue(test.isValidNeutronID(uuid));
-        assertTrue(test.isValidNeutronID(tenantId));
-        assertTrue(test.isValidNeutronID(neutronId));
+    @Test
+    public void testGetException() {
+        Status mockStatus = mock(Status.class);
+
+        when(mockStatus.getCode()).thenReturn(StatusCode.BADREQUEST);
+        assertEquals(HttpURLConnection.HTTP_BAD_REQUEST, BaseHandler.getException(mockStatus));
+
+        when(mockStatus.getCode()).thenReturn(StatusCode.CONFLICT);
+        assertEquals(HttpURLConnection.HTTP_CONFLICT, BaseHandler.getException(mockStatus));
+
+        when(mockStatus.getCode()).thenReturn(StatusCode.NOTACCEPTABLE);
+        assertEquals(HttpURLConnection.HTTP_NOT_ACCEPTABLE, BaseHandler.getException(mockStatus));
+
+        when(mockStatus.getCode()).thenReturn(StatusCode.NOTFOUND);
+        assertEquals(HttpURLConnection.HTTP_NOT_FOUND, BaseHandler.getException(mockStatus));
+
+        when(mockStatus.getCode()).thenReturn(StatusCode.INTERNALERROR);
+        assertEquals(HttpURLConnection.HTTP_INTERNAL_ERROR, BaseHandler.getException(mockStatus));
 
     }
 
-    public void testConvertNeutronIDToKey() throws Exception {
+    @Test
+    public void testIsValidNeutronID() {
+        assertFalse(BaseHandler.isValidNeutronID(nullString));
+        assertTrue(BaseHandler.isValidNeutronID(uuid));
+        assertTrue(BaseHandler.isValidNeutronID(tenantId));
+        assertTrue(BaseHandler.isValidNeutronID(neutronId));
 
-        String uuidResult = test.convertNeutronIDToKey(uuid);
+    }
+
+    @Test
+    public void testConvertNeutronIDToKey() {
+
+        String uuidResult = BaseHandler.convertNeutronIDToKey(uuid);
         String uuidExpected = "ce044452f22eea4a3ecd1cde80cf996";
 
-        String tenantResult = test.convertNeutronIDToKey(tenantId);
+        String tenantResult = BaseHandler.convertNeutronIDToKey(tenantId);
         String tenantExpected = "8d62bfa112fb247aa20edc74235c1ce";
 
-        String neutronResult = test.convertNeutronIDToKey(neutronId);
+        String neutronResult = BaseHandler.convertNeutronIDToKey(neutronId);
 
         assertEquals(uuidExpected, uuidResult);
         assertEquals(tenantExpected, tenantResult);

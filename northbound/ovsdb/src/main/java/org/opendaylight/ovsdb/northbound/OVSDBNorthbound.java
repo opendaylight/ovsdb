bcfeca48b160916da208546a629d676aeb87ef04
Dave Tucker <dave.j.tucker@hp.com>
2014-03-04 15:29:34 +0000

    Update Enunciate Documentation for OVSDB REST API
    Change-Id: Ia77888f75343075710de2c1522cbd048bd609543
    Signed-off-by: Dave Tucker <dave.j.tucker@hp.com>

--- northbound/ovsdb/src/main/java/org/opendaylight/ovsdb/northbound/OVSDBNorthbound.java
+++ northbound/ovsdb/src/main/java/org/opendaylight/ovsdb/northbound/OVSDBNorthbound.java
@@ -1,11 +1,11 @@
 /*
- * Copyright (C) 2013 Red Hat, Inc.
+ * Copyright (C) 2014 Red Hat, Inc. and others
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  *
- * Authors : Madhu Venugopal, Brent Salisbury
+ * Authors : Madhu Venugopal, Brent Salisbury, Dave Tucker
  */
 package org.opendaylight.ovsdb.northbound;
 
@@ -48,9 +48,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * This class provides REST APIs to Create, Read, Update and Delete OVSDB Row in any of the ovsdb table
- * database one at a time.
- */
+* OVSDB Northbound REST API.<br>
+* This class provides REST APIs to Create, Read, Update and Delete OVSDB Row in any of the ovsdb table
+* database one at a time. The JSON used to create rows is in the same format as the OVSDB JSON-RPC messages.
+* This format is documented in the <a href="http://openvswitch.org/ovs-vswitchd.conf.db.5.pdf>OVSDB Schema</a>
+* and in <a href="http://tools.ietf.org/rfc/rfc7047.txt">RFC 7047</a>.
+*
+* <br>
+* <br>
+* Authentication scheme : <b>HTTP Basic</b><br>
+* Authentication realm : <b>opendaylight</b><br>
+* Transport : <b>HTTP and HTTPS</b><br>
+* <br>
+* HTTPS Authentication is disabled by default.
+*/
 
 @Path("/")
 public class OVSDBNorthbound {
@@ -97,16 +108,207 @@ public class OVSDBNorthbound {
     /**
      * Create a Row
      *
-     * @param tableName name of the ovsdb table
+     * @param nodeType type of node e.g OVS
+     * @param nodeId ID of the node
+     * @param tableName name of the OVSDB table
      * @param row the {@link OVSDBRow} Row that is being inserted
      *
      * @return Response as dictated by the HTTP Response Status code
      *
-     *         <pre>
-     * Example:
+     * Examples:
+     *
+     * Create a Bridge Row:
+     * <pre>
      *
      * Request URL:
-     * https://localhost/controller/nb/v2/ovsdb/tables/bridge/rows
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/bridge/rows
+     *
+     * JSON:
+     * {
+     *   "row": {
+     *     "Bridge": {
+     *       "name": "bridge1",
+     *       "datapath_type": "OPENFLOW"
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create a Port Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/port/rows
+     *
+     * JSON:
+     * {
+     *   "parent_uuid": "b01cd26b-9c63-4216-8cf2-55f7087adab1",
+     *   "row": {
+     *     "Port": {
+     *       "name": "port1",
+     *       "mac": [
+     *         "set",
+     *         "00:00:00:00:00:01"
+     *       ],
+     *       "tag": [
+     *         "set",
+     *         200
+     *       ]
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create an Interface Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/interface/rows
+     *
+     * JSON:
+     * {
+     *   "parent_uuid": "c7b54c9b-9b25-4801-a81d-d7bc489d4840",
+     *   "row": {
+     *     "Interface": {
+     *       "name": "br2",
+     *       "mac": [
+     *         "set",
+     *         "00:00:bb:bb:00:01"
+     *       ],
+     *       "admin_state": "up"
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create an SSL Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/SSL/rows
+     *
+     * JSON:
+     * {
+     *   "row": {
+     *     "SSL": {
+     *       "name": "mySSL",
+     *       "ca_cert": "ca_cert",
+     *       "bootstrap_ca_cert": true,
+     *       "certificate": "pieceofpaper",
+     *       "private_key": "private"
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create an sFlow Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/sflow/rows
+     *
+     * JSON:
+     * {
+     *   "parent_uuid": "6b3072ba-a120-4db9-82f8-a8ce4eae6942",
+     *   "row": {
+     *     "sFlow": {
+     *       "agent": [
+     *         "set",
+     *         "agent_string"
+     *       ],
+     *       "targets": [
+     *         "set",
+     *         "targets_string"
+     *       ]
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create a QoS Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/qos/rows
+     *
+     * JSON:
+     * {
+     *   "parent_uuid": "b109dbcf-47bb-4121-b244-e623b3421d6e",
+     *   "row": {
+     *     "QoS": {
+     *       "type": "linux-htb"
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create a Queue Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/queue/rows
+     *
+     * {
+     *   "parent_uuid": "b16eae7d-7e97-46d2-95d1-333d1de4a3d7",
+     *   "row": {
+     *     "Queue": {
+     *       "dscp": [
+     *         "set",
+     *         "25"
+     *       ]
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create a Netflow Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/netflow/rows
+     *
+     * JSON:
+     * {
+     *   "parent_uuid": "b01cd26b-9c63-4216-8cf2-55f7087adab1",
+     *   "row": {
+     *     "NetFlow": {
+     *       "targets": [
+     *         "set",
+     *         [
+     *           "192.168.1.102:9998"
+     *         ]
+     *       ],
+     *       "active_timeout": "0"
+     *     }
+     *   }
+     * }
+     * </pre>
+     *
+     *
+     * Create a Manager Row:
+     * <pre>
+     *
+     * Request URL:
+     * POST http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/manager/rows
+     *
+     * JSON:
+     * {
+     *   "parent_uuid": "8d3fb89b-5fac-4631-a990-f5a4e7f5383a",
+     *   "row": {
+     *     "Manager": {
+     *       "target": "a_string",
+     *       "is_connected": true,
+     *       "state": "active"
+     *     }
+     *   }
+     * }
      * </pre>
      */
 
@@ -157,17 +359,45 @@ public class OVSDBNorthbound {
     /**
      * Read a Row
      *
-     * @param node OVSDB Node identifier
+     * @param nodeType type of node e.g OVS
+     * @param nodeId ID of the node
      * @param tableName name of the ovsdb table
      * @param rowUuid UUID of the row being read
      *
      * @return Row corresponding to the UUID.
      *
-     * <pre>
-     * Example:
+     * Examples
      *
-     * Request URL:
-     * https://localhost/controller/nb/v2/ovsdb/tables/bridge/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     * <pre>
+     * Get a specific Bridge Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/bridge/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific Port Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/port/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific Interface Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/interface/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific Controller Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/controller/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific SSL Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/SSL/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific sFlow Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/sflow/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific QoS Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/qos/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific Queue Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/queue/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific Netflow Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/netflow/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Get a specific Manager Row:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/manager/rows/6f4c602c-026f-4390-beea-d50d6d448100
      * </pre>
      */
 
@@ -207,16 +437,47 @@ public class OVSDBNorthbound {
     /**
      * Read all Rows of a table
      *
-     * @param node OVSDB Node identifier
+     * @param nodeType type of node e.g OVS
+     * @param nodeId ID of the node
      * @param tableName name of the ovsdb table
      *
      * @return All the Rows of a table
      *
-     * <pre>
-     * Example:
+     * Examples
      *
-     * Request URL:
-     * https://localhost/controller/nb/v2/ovsdb/tables/bridge/rows
+     * <pre>
+     * Get all Bridge Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/bridge/rows
+     *
+     * Get all Port Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/port/rows
+     *
+     * Get all Interface Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/interface/rows
+     *
+     * Get all Controller Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/controller/rows
+     *
+     * Get all SSL Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/SSL/rows
+     *
+     * Get all sFlow Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/sflow/rows
+     *
+     * Get all QoS Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/qos/rows
+     *
+     * Get all Queue Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/queue/rows
+     *
+     * Get all Netflow Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/netflow/rows
+     *
+     * Get all Manager Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/manager/rows
+     *
+     * Get all Open vSwitch Rows:
+     * GET http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/open_vswitch/rows
      * </pre>
      */
 
@@ -255,26 +516,37 @@ public class OVSDBNorthbound {
     /**
      * Update a Row
      *
-     * @param node OVSDB Node identifier
+     * @param nodeType type of node e.g OVS
+     * @param nodeId ID of the node
      * @param tableName name of the ovsdb table
      * @param rowUuid UUID of the row being updated
-     * @param row the {@link OVSDBRow} Row that is being inserted
+     * @param row the {@link OVSDBRow} Row that is being updated
      *
      * @return Response as dictated by the HTTP Response Status code
      *
-     * <pre>
      * Example:
      *
+     * Update the Bridge row to add a controller
+     * <pre>
+     *
      * Request URL:
-     * PUT https://localhost/controller/nb/v2/ovsdb/tables/bridge/rows/6f4c602c-026f-4390-beea-d50d6d448100
-     * BODY
+     * PUT http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/bridge/rows/b01cd26b-9c63-4216-8cf2-55f7087adab1
+     *
+     * JSON:
      * {
-     *    "row":{
-     *       "Bridge":{
-     *          "datapath_type":"OPENFLOWv1.3",
-     *          "stp_enable":true
-     *        }
+     *   "row": {
+     *     "Bridge": {
+     *       "controller": [
+     *         "set",
+     *         [
+     *           [
+     *             "uuid",
+     *             "a566e8b4-fc38-499b-8623-6087d5b36b72"
+     *           ]
+     *         ]
+     *       ]
      *     }
+     *   }
      * }
      * </pre>
      */
@@ -317,16 +589,45 @@ public class OVSDBNorthbound {
     /**
      * Delete a row
      *
+     * @param nodeType type of node e.g OVS
+     * @param nodeId ID of the node
      * @param tableName name of the ovsdb table
      * @param uuid UUID of the Row to be removed
+     *
      * @return Response as dictated by the HTTP Response Status code
      *
+     * Examples:
+     *
      * <pre>
-     * Example:
+     * Delete a specific Bridge Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/bridge/rows/6f4c602c-026f-4390-beea-d50d6d448100
      *
-     * Request URL:
-     * https://localhost/controller/nb/v2/ovsdb/tables/bridge/rows/41ab15a9-0dab-4675-a579-63019d4bcbec
+     * Delete a specific Port Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/port/rows/6f4c602c-026f-4390-beea-d50d6d448100
      *
+     * Delete a specific Interface Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/interface/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific Controller Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/controller/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific SSL Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/SSL/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific sFlow Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/sflow/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific QoS Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/qos/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific Queue Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/queue/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific Netflow Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/netflow/rows/6f4c602c-026f-4390-beea-d50d6d448100
+     *
+     * Delete a specific Manager Row:
+     * DELETE http://localhost:8080/ovsdb/nb/v2/node/OVS/HOST1/tables/manager/rows/6f4c602c-026f-4390-beea-d50d6d448100
      * </pre>
      */
     @Path("/node/{nodeType}/{nodeId}/tables/{tableName}/rows/{uuid}")

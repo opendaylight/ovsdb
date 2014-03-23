38bb806fed628c63da2fce4c0f42d811766ea492
Dave Tucker <dave.j.tucker@hp.com>
2014-03-23 00:24:41 +0000

    Separate API and Implementation in OVSDB Neutron
    It's regarded as a best practice in Java to separate API and
    Implementation. In OSGi we can take this one step further and place API
    and implementation in seperate bundles (a pattern used in the controller
    project). This is what I have attempted in this commit.
    
    - Create a new bundle 'neutron.impl'
    - Leave interfaces in 'neutron' bundle
    - Move implementations to 'neutron.impl' bundle
    - Add new bundle to root pom.xml
    - Add interfaces for AdminConfigManager, TenantNetworkManager and
      InternalNetworkManager
    - Fix typo in a method in TenantNetworkManager s/tenn/ten/
    
    This is a start, and work is not yet 100% complete, but as this is
    disruptive I would like to start getting things merged sooner rather
    than later.
    
    Remaining ToDos:
    - Consider moving Abstract Base Classes to API bundle
        - BaseHandler
        - ProviderNetworkManager
    
    Change-Id: Ib238059bebbbd55c5cc2cdada88629c3a927bd9b
    Signed-off-by: Dave Tucker <dave.j.tucker@hp.com>

---
+++ neutron/api/src/main/java/org/opendaylight/ovsdb/neutron/IAdminConfigManager.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Dave Tucker (HP) - Initial Commit of IAdminConfigManager
+ *******************************************************************************/
+package org.opendaylight.ovsdb.neutron;
+
+import org.opendaylight.controller.sal.core.Node;
+
+import java.net.InetAddress;
+
+public interface IAdminConfigManager {
+    public String getIntegrationBridgeName();
+    public void setIntegrationBridgeName(String integrationBridgeName);
+    public String getTunnelBridgeName();
+    public void setTunnelBridgeName(String tunnelBridgeName);
+    public String getExternalBridgeName();
+    public void setExternalBridgeName (String externalBridgeName);
+    public String getPatchToIntegration();
+    public void setPatchToIntegration(String patchToIntegration);
+    public String getPatchToTunnel();
+    public void setPatchToTunnel(String patchToTunnel);
+    public InetAddress getTunnelEndPoint(Node node);
+    public boolean isInterested (String tableName);
+}
---
+++ neutron/api/src/main/java/org/opendaylight/ovsdb/neutron/ITenantNetworkManager.java
@@ -0,0 +1,27 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Dave Tucker (HP) - Initial Commit of IAdminConfigManager
+ *******************************************************************************/
+package org.opendaylight.ovsdb.neutron;
+
+import org.opendaylight.controller.networkconfig.neutron.NeutronNetwork;
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.ovsdb.lib.table.Interface;
+
+public interface ITenantNetworkManager {
+    public int getInternalVlan(Node node, String networkId);
+    public void reclaimTenantNetworkInternalVlan(Node node, String portUUID, NeutronNetwork network);
+    public void networkCreated (String networkId);
+    public int networkCreated (Node node, String networkId);
+    public boolean isTenantNetworkPresentInNode(Node node, String segmentationId);
+    public String getNetworkIdForSegmentationId (String segmentationId);
+    public NeutronNetwork getTenantNetworkForInterface (Interface intf);
+    public void programTenantNetworkInternalVlan(Node node, String portUUID, NeutronNetwork network);
+    public void networkDeleted(String id);
+}

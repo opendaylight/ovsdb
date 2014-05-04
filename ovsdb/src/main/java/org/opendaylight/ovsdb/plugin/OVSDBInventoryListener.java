cf805d220fcc6bce6015cb390440ec5d676863d9
Hsin-Yi Shen <hshen@redhat.com>
2014-05-03 20:19:25 -0400

    Add support for Neutron port/network delete events.
    This commit will enable support for Neutron port delete and network delete events.
    It will also enable flow cleanup and tunnel port delete for ovsdb using OF 1.3
    
    Signed-off-by: Hsin-Yi Shen <hshen@redhat.com>
    Change-Id: Ic848125d21abc44c796ef2b1675cca9e50630f76

--- ovsdb/src/main/java/org/opendaylight/ovsdb/plugin/OVSDBInventoryListener.java
+++ ovsdb/src/main/java/org/opendaylight/ovsdb/plugin/OVSDBInventoryListener.java
@@ -17,5 +17,5 @@ public interface OVSDBInventoryListener {
     public void nodeRemoved(Node node);
     public void rowAdded(Node node, String tableName, String uuid, Table<?> row);
     public void rowUpdated(Node node, String tableName, String uuid, Table<?> old, Table<?> row);
-    public void rowRemoved(Node node, String tableName, String uuid, Table<?> row);
+    public void rowRemoved(Node node, String tableName, String uuid, Table<?> row, Object context);
 }

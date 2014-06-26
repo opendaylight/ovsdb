dfbae52b1f8d267d3e0b112c0f3942bd27828922
Dave Tucker <djt@redhat.com>
2014-06-27 00:36:04 +0100

    Refactor Open vSwitch Schema Integration Tests
    - Add a Junit suite that runs the test in correct order
    - Make changes to the pom to ensure only the suite is run rather than
      the individual test cases
    - Fix failing IT's for the IPFIX table using Junit Assumptions
    - Add test case to check correct exception is raised when a table is not
      supported in the current schema
    
    Change-Id: I074f047e964610c7397af040946dde1b562754c0
    Signed-off-by: Dave Tucker <djt@redhat.com>

---
+++ schemas/Open_vSwitch/src/test/java/org/opendaylight/ovsdb/schema/openvswitch/TearDown.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2014 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Authors : Madhu Venugopal
+ */
+
+package org.opendaylight.ovsdb.schema.openvswitch;
+
+import com.google.common.collect.Sets;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.junit.Before;
+import org.junit.Test;
+import org.opendaylight.ovsdb.lib.message.UpdateNotification;
+import org.opendaylight.ovsdb.lib.notation.Mutator;
+import org.opendaylight.ovsdb.lib.operations.OperationResult;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
+import static org.opendaylight.ovsdb.lib.operations.Operations.op;
+
+public class TearDown extends OpenVswitchSchemaTestBase {
+
+    @Before
+    public void setUp() throws ExecutionException, InterruptedException, TimeoutException, IOException {
+        super.setUp();
+    }
+
+    @Test
+    public void tearDown() throws InterruptedException, ExecutionException, IOException, TimeoutException {
+        Bridge bridge = this.ovs.getTypedRowWrapper(Bridge.class, null);
+        OpenVSwitch openVSwitch = this.ovs.getTypedRowWrapper(OpenVSwitch.class, null);
+
+        ListenableFuture<List<OperationResult>> results = this.ovs.transactBuilder()
+                .add(op.delete(bridge.getSchema())
+                        .where(bridge.getNameColumn().getSchema().opEqual(OpenVswitchSchemaTestBase.TEST_BRIDGE_NAME))
+                        .build())
+                .add(op.mutate(openVSwitch.getSchema())
+                        .addMutation(openVSwitch.getBridgesColumn().getSchema(), Mutator.DELETE, Sets.newHashSet(OpenVswitchSchemaSuiteIT.getTestBridgeUuid())))
+                .add(op.commit(true))
+                .execute();
+
+        List<OperationResult> operationResults = results.get();
+        System.out.println("Delete operation results = " + operationResults);
+        // tableCache = new HashMap<String, Map<UUID, Row>>();
+    }
+
+    @Override
+    public void update(Object context, UpdateNotification upadateNotification) {
+
+    }
+
+    @Override
+    public void locked(Object context, List<String> ids) {
+
+    }
+
+    @Override
+    public void stolen(Object context, List<String> ids) {
+
+    }
+}

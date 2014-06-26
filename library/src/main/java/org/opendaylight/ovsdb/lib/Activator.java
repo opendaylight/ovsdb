755fc4a9f27bdeb0c90c05ef80bfd9d892a5e250
Madhu Venugopal <mavenugo@gmail.com>
2014-06-26 10:40:25 -0700

    Introduced Activator in the Library module to make it OSGi friendly.
    PAX-exam is not included due to the current merge resolution happening in parallel.
    
    Change-Id: Ic25ae261905d2ea081d2bd936300cb92e7df4542
    Signed-off-by: Madhu Venugopal <mavenugo@gmail.com>

---
+++ library/src/main/java/org/opendaylight/ovsdb/lib/Activator.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2013 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Authors : Madhu Venugopal, Brent Salisbury
+ */
+package org.opendaylight.ovsdb.lib;
+
+import org.apache.felix.dm.DependencyActivatorBase;
+import org.apache.felix.dm.DependencyManager;
+import org.opendaylight.ovsdb.lib.impl.OvsdbConnectionService;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * OVSDB Library OSGi Activator
+ */
+public class Activator extends DependencyActivatorBase {
+    protected static final Logger logger = LoggerFactory
+            .getLogger(Activator.class);
+
+    @Override
+	public void init(BundleContext context, DependencyManager manager) throws Exception {
+        manager.add(createComponent()
+            .setInterface(OvsdbConnection.class.getName(), null)
+            .setImplementation(OvsdbConnectionService.class)
+        );
+        manager.createServiceDependency()
+               .setService(OvsdbConnectionListener.class)
+               .setCallbacks("registerForPassiveConnection", "unregisterFromPassiveConnection")
+               .setRequired(false);
+    }
+
+    @Override
+    public void destroy(BundleContext context, DependencyManager manager) throws Exception {}
+}

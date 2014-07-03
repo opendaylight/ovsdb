78c3f0e31461c4db9e3d9e89f196f47e0371b8b1
Sudheendra Murthy <sumurthy@ebay.com>
2014-07-02 21:06:51 -0700

    Bug-1190: Fix for select response
    Updated transact interface in OvsdbRPC to return JSON response and
    deserializing to OperationResult being handled in OvsdbClientImpl
    
    Change-Id: I061292435e68a638f83e69495710ffaa3a04479b
    Signed-off-by: Sudheendra Murthy <sudhi.vm@gmail.com>

---
+++ library/src/main/java/org/opendaylight/ovsdb/lib/impl/FutureTransformUtils.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2014 EBay Software Foundation
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Authors : Sudheendra Murthy
+ */
+
+package org.opendaylight.ovsdb.lib.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Function;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import org.opendaylight.ovsdb.lib.operations.Operation;
+import org.opendaylight.ovsdb.lib.operations.OperationResult;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FutureTransformUtils {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public final static ListenableFuture<List<OperationResult>> transformTransactResponse
+            (ListenableFuture<List<JsonNode>> transactResponseFuture, final List<Operation> operations) {
+        return Futures.transform(transactResponseFuture, new Function<List<JsonNode>, List<OperationResult>>() {
+            @Override
+            public List<OperationResult> apply(List<JsonNode> jsonNodes) {
+                final List<OperationResult> operationResults = new ArrayList<>();
+                for (int i = 0; i < jsonNodes.size(); i++) {
+                    JsonNode jsonNode = jsonNodes.get(i);
+                    OperationResult or;
+                    if (jsonNode.size() > 0) {
+                        Operation op = operations.get(i);
+                        switch (op.getOp()) {
+                            case "select":
+                                or = new OperationResult();
+                                or.setRows(op.getTableSchema().createRows(jsonNode));
+                                break;
+                            default:
+                                or = objectMapper.convertValue(jsonNode, OperationResult.class);
+                                break;
+                        }
+                    } else {
+                        or = new OperationResult();
+                    }
+                    operationResults.add(or);
+
+                }
+                return operationResults;
+            }
+        });
+    }
+}

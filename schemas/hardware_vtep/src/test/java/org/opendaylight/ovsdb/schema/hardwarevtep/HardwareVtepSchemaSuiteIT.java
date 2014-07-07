7c773edb82d6c6defb77fe8519766cf1fb926f4a
Matt Oswalt <matt@keepingitclassless.net>
2014-07-07 05:17:01 -0400

    Added new IT base for Hardware VTEP schema, Manager test case
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

---
+++ schemas/hardware_vtep/src/test/java/org/opendaylight/ovsdb/schema/hardwarevtep/HardwareVtepSchemaSuiteIT.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2014 Matt Oswalt
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Authors : Matt Oswalt
+ */
+
+package org.opendaylight.ovsdb.schema.hardwarevtep;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.opendaylight.ovsdb.lib.OvsdbClient;
+import org.opendaylight.ovsdb.lib.notation.Row;
+import org.opendaylight.ovsdb.lib.notation.UUID;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        LogicalRouterTestCases.class
+})
+
+public class HardwareVtepSchemaSuiteIT {
+    // Keep this data between test runs
+    static OvsdbClient ovsdbClient;
+    static UUID testManagerUuid;
+    static UUID testLogicalSwitchUuid;
+    static Map<String, Map<UUID, Row>> tableCache = new HashMap<String, Map<UUID, Row>>();
+
+    public static OvsdbClient getOvsdbClient() {
+        return ovsdbClient;
+    }
+
+    public static void setOvsdbClient(OvsdbClient ovsdbClient) {
+        HardwareVtepSchemaSuiteIT.ovsdbClient = ovsdbClient;
+    }
+
+    public static UUID getTestManagerUuid() {
+        return testManagerUuid;
+    }
+
+    public static void setTestManagerUuid(UUID testManagerUuid) {
+        HardwareVtepSchemaSuiteIT.testManagerUuid = testManagerUuid;
+    }
+
+    public static UUID getTestLogicalSwitchUuid() {
+        return testLogicalSwitchUuid;
+    }
+
+    public static void setTestLogicalSwitchUuid(UUID testLogicalSwitchUuid) {
+        HardwareVtepSchemaSuiteIT.testLogicalSwitchUuid = testLogicalSwitchUuid;
+    }
+
+    public static Map<String, Map<UUID, Row>> getTableCache() {
+        return tableCache;
+    }
+}

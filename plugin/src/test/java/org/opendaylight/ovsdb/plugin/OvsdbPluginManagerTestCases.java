2e4445391706e4244703dba43dc6114e17a6fa02
Madhu Venugopal <mavenugo@gmail.com>
2014-07-30 12:57:18 -0700

    DO NOT MERGE : pushing change to check the Jenkins issue with the Plugin IT.
    Change-Id: I8cc8225332910bcf0c56babb10010d55c9e8df99
    Signed-off-by: Madhu Venugopal <mavenugo@gmail.com>

--- plugin/src/test/java/org/opendaylight/ovsdb/plugin/OvsdbPluginManagerTestCases.java
+++ plugin/src/test/java/org/opendaylight/ovsdb/plugin/OvsdbPluginManagerTestCases.java
@@ -14,9 +14,9 @@ import org.opendaylight.controller.sal.core.Node;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class OvsdbTestSetManagerIT extends OvsdbTestBase {
+public class OvsdbPluginManagerTestCases extends OvsdbTestBase {
     private static final Logger logger = LoggerFactory
-            .getLogger(OvsdbTestSetManagerIT.class);
+            .getLogger(OvsdbPluginManagerTestCases.class);
 
     @Test
     public void setManager() throws Throwable{

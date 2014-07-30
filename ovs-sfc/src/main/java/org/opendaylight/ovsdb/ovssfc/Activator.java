93a521f8cd7c10e7a82382ade0cf61055223be18
Sam Hague <shague@redhat.com>
2014-07-30 15:41:19 -0400

    Init bundle files fo ovs-sfc.
    Change-Id: Ia0a4985c4fed44562de8804cbdbe71f07d06514b
    Signed-off-by: Sam Hague <shague@redhat.com>

---
+++ ovs-sfc/src/main/java/org/opendaylight/ovsdb/ovssfc/Activator.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2014 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Authors : Sam Hague
+ */
+package org.opendaylight.ovsdb.ovssfc;
+
+import org.opendaylight.controller.sal.core.ComponentActivatorAbstractBase;
+
+public class Activator extends ComponentActivatorAbstractBase {
+
+    /**
+     * Function called when the activator starts just after some initializations
+     * are done by the ComponentActivatorAbstractBase.
+     */
+    @Override
+    public void init() {
+    }
+
+    /**
+     * Function called when the activator stops just before the cleanup done by
+     * ComponentActivatorAbstractBase
+     *
+     */
+    @Override
+    public void destroy() {
+    }
+}

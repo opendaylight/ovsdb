26e300014e6524548f87af4939c49d061f99ac77
Sam Hague <shague@redhat.com>
2014-08-27 19:49:26 -0400

    Add ovssfc to osdvb distribution
    Change-Id: Ibcbf2783d1125fbf08314feb210ef7177165b96a
    Signed-off-by: Sam Hague <shague@redhat.com>

--- ovs-sfc/src/main/java/org/opendaylight/ovsdb/ovssfc/SfpDataListener.java
+++ ovs-sfc/src/main/java/org/opendaylight/ovsdb/ovssfc/SfpDataListener.java
@@ -12,35 +12,86 @@ package org.opendaylight.ovsdb.ovssfc;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfp.rev140701.ServiceFunctionPaths;
+import org.opendaylight.yang.gen.v1.urn.cisco.params.xml.ns.yang.sfc.sfp.rev140701.service.function.paths.ServiceFunctionPath;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.Map;
+import java.util.Set;
 
 public class SfpDataListener extends AbstractDataListener {
     private static final Logger logger = LoggerFactory.getLogger(SfpDataListener.class);
 
-    public SfpDataListener(DataBroker dataBroker) {
+    public SfpDataListener (DataBroker dataBroker) {
         setDataBroker(dataBroker);
-        setIID(InstanceIdentifier.builder(ServiceFunctionPaths.class).build());
+        setIID(InstanceIdentifierUtils.createServiceFunctionPathsPath());
         registerAsDataChangeListener();
     }
 
     @Override
-    public void onDataChanged(final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change ) {
+    public void onDataChanged (final AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> change ) {
+        Map<InstanceIdentifier<?>, DataObject> dataObject;
+
         logger.trace("\nOVSSFC Enter: {}", Thread.currentThread().getStackTrace()[1]);
+/* Keeping for reference in case I need to use it
+        DataObject dom = change.getOriginalSubtree();
+        if (dom instanceof ServiceFunctionPaths) {
+            ServiceFunctionPaths serviceFunctionPaths = (ServiceFunctionPaths) dom;
+            logger.trace("\nOVSSFC ORIGINAL SUBTREE: sfps:\n   {}", serviceFunctionPaths.toString());
 
-        Map<InstanceIdentifier<?>, DataObject> dataCreatedObject = change.getCreatedData();
+            //OvsSfcProvider.getOvsSfcProvider().eventHandler.enqueueSfpEvent(SfcEvent.Action.CREATE, serviceFunctionPaths);
+        }
+        dom = change.getUpdatedSubtree();
+        if (dom instanceof ServiceFunctionPaths) {
+            ServiceFunctionPaths serviceFunctionPaths = (ServiceFunctionPaths) dom;
+            logger.trace("\nOVSSFC UPDATED SUBTREE: sfps:\n   {}", serviceFunctionPaths.toString());
 
-        Map<InstanceIdentifier<?>, DataObject> dataUpdatedConfigurationObject = change.getUpdatedData();
-        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataUpdatedConfigurationObject.entrySet()) {
+            OvsSfcProvider.getOvsSfcProvider().eventHandler.enqueueSfpEvent(SfcEvent.Action.UPDATE, serviceFunctionPaths);
+        }
+
+        dataObject = change.getOriginalData();
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataObject.entrySet()) {
             if (entry.getValue() instanceof ServiceFunctionPaths) {
-                ServiceFunctionPaths updatedServiceFunctionPaths = (ServiceFunctionPaths) entry.getValue();
-                logger.trace("\nOVSSFC sfp:\n   {}", updatedServiceFunctionPaths.toString());
+                ServiceFunctionPaths serviceFunctionPaths = (ServiceFunctionPaths) entry.getValue();
+                logger.trace("\nOVSSFC ORIGINAL: sfps:\n   {}", serviceFunctionPaths.toString());
+
+                //OvsSfcProvider.getOvsSfcProvider().eventHandler.enqueueSfpEvent(SfcEvent.Action.CREATE, serviceFunctionPaths);
+            }
+        }
+*/
+        dataObject = change.getCreatedData();
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataObject.entrySet()) {
+            if (entry.getValue() instanceof ServiceFunctionPaths) {
+                ServiceFunctionPaths serviceFunctionPaths = (ServiceFunctionPaths) entry.getValue();
+                logger.trace("\nOVSSFC CREATE: sfps:\n   {}", serviceFunctionPaths.toString());
+
+                OvsSfcProvider.getOvsSfcProvider().eventHandler.enqueueSfpsEvent(SfcEvent.Action.CREATE, serviceFunctionPaths);
             }
         }
 
-        logger.trace("\nOVSSFC Exit: {}", Thread.currentThread().getStackTrace()[1]);
+        dataObject = change.getUpdatedData();
+        for (Map.Entry<InstanceIdentifier<?>, DataObject> entry : dataObject.entrySet()) {
+            if (entry.getValue() instanceof ServiceFunctionPaths) {
+                ServiceFunctionPaths updatedServiceFunctionPaths = (ServiceFunctionPaths) entry.getValue();
+                logger.trace("\nOVSSFC UPDATE: sfps:\n   {}", updatedServiceFunctionPaths.toString());
+
+                OvsSfcProvider.getOvsSfcProvider().eventHandler.enqueueSfpsEvent(SfcEvent.Action.UPDATE, updatedServiceFunctionPaths);
+            }
+        }
+
+        Map<InstanceIdentifier<?>, DataObject> originalDataObject = change.getOriginalData();
+        Set<InstanceIdentifier<?>> iID = change.getRemovedPaths();
+        for (InstanceIdentifier instanceIdentifier : iID) {
+            DataObject dObject = originalDataObject.get(instanceIdentifier);
+            if (dObject instanceof ServiceFunctionPath) {
+                ServiceFunctionPath serviceFunctionPath = (ServiceFunctionPath) dataObject;
+                logger.trace("\nOVSSFC DELETE: sfp:\n   {}", serviceFunctionPath.toString());
+
+                OvsSfcProvider.getOvsSfcProvider().eventHandler.enqueueSfpEvent(SfcEvent.Action.DELETE, serviceFunctionPath);
+            }
+        }
+
+       logger.trace("\nOVSSFC Exit: {}", Thread.currentThread().getStackTrace()[1]);
     }
 }

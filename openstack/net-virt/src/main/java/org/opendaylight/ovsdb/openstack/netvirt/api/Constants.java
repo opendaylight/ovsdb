954beeea1c8dfbe432ccb3bed12745b3f9b3f63c
Brent Salisbury <brent.salisbury@gmail.com>
2014-08-28 15:37:11 -0400

    A few adds to netvirt constants for OF usage
    -Patch#1: A handful of protocol and TCP flags.
    -Patch#2: change tcp_flags to int.
    
    Change-Id: I2d5a1fce4b2ccd44c640fcfa265e3acf602b5d8f
    Signed-off-by: Brent Salisbury <brent.salisbury@gmail.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/Constants.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/Constants.java
@@ -60,4 +60,38 @@ public final class Constants {
     public static final String PROVIDER_TYPE_PROPERTY = "providerType";
     public static final String OPENFLOW_VERSION_PROPERTY = "openflowVersion";
     public static final String EVENT_HANDLER_TYPE_PROPERTY = "eventHandlerType";
+
+    /*
+    * int representation TCP Flag constant values as interpreted by Open vSwitch
+    */
+    public static final int TCP_FIN = 0x0001;
+    public static final int TCP_SYN = 0x002;
+    public static final int TCP_RST = 0x004;
+    public static final int TCP_PSH = 0x008;
+    public static final int TCP_ACK = 0x010;
+    public static final int TCP_URG = 0x020;
+    public static final int TCP_ECE = 0x040;
+    public static final int TCP_CWR = 0x080;
+    public static final int TCP_NS = 0x100;
+
+    /*
+    * Short representations of IANA Assigned Protocol Numbers
+    */
+    public static final short ICMP_SHORT = 1;
+    public static final short TCP_SHORT = 6;
+    public static final short UDP_SHORT = 17;
+
+    /*
+    * String representations of Protocol
+    */
+    public static final String ICMP = "icmp";
+    public static final String TCP = "tcp";
+    public static final String UDP = "udp";
+
+    /*
+    * Long representations of commonly used EtherTypes
+    */
+    public static final long LLDP_ETHERTYPE = 0x88CCL;
+    public static final long IPV4_ETHERTYPE = 0x8000L;
+
 }

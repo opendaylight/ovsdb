cb4d69c4b919bb0ef5ecd4173d2f0a87a14f9885
Srini Seetharaman <srini.seetharaman@gmail.com>
2014-09-01 18:59:07 -0700

    Adding logic to handle the neutron LBaaS instance creation call. After the call, the required information will be stored in a Map <UUID, LoadBalancerConfiguration object>. Still TODO: FlowMods
    Change-Id: Ice363f5d86cb5111e113ebda8a58c4c3a5702cfe
    Signed-off-by: Srini Seetharaman <srini.seetharaman@gmail.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/LBaaSHandler.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/LBaaSHandler.java
@@ -12,15 +12,24 @@ package org.opendaylight.ovsdb.openstack.netvirt;
 
 import org.opendaylight.controller.networkconfig.neutron.INeutronLoadBalancerAware;
 import org.opendaylight.controller.networkconfig.neutron.INeutronLoadBalancerPoolAware;
+import org.opendaylight.controller.networkconfig.neutron.INeutronLoadBalancerPoolCRUD;
 import org.opendaylight.controller.networkconfig.neutron.INeutronLoadBalancerPoolMemberAware;
+import org.opendaylight.controller.networkconfig.neutron.INeutronLoadBalancerPoolMemberCRUD;
+import org.opendaylight.controller.networkconfig.neutron.INeutronPortCRUD;
 import org.opendaylight.controller.networkconfig.neutron.NeutronLoadBalancer;
 import org.opendaylight.controller.networkconfig.neutron.NeutronLoadBalancerPool;
 import org.opendaylight.controller.networkconfig.neutron.NeutronLoadBalancerPoolMember;
-
+import org.opendaylight.controller.networkconfig.neutron.NeutronPort;
+import org.opendaylight.controller.networkconfig.neutron.Neutron_IPs;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.collect.Maps;
+
 import java.net.HttpURLConnection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Handle requests for OpenStack Neutron v2.0 LBaaS API calls.
@@ -32,23 +41,76 @@ public class LBaaSHandler extends AbstractHandler
         implements INeutronLoadBalancerAware, INeutronLoadBalancerPoolAware,
             INeutronLoadBalancerPoolMemberAware {
 
-    static final Logger logger = LoggerFactory.getLogger(LBaaSHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(LBaaSHandler.class);
+    private static final String PROTOCOL_HTTP = "HTTP";
+    private static final String PROTOCOL_HTTPS = "HTTPS";
+
+    // The implementation for each of these services is resolved by the OSGi Service Manager
+    private volatile INeutronLoadBalancerPoolCRUD neutronLBPoolCache;
+    private volatile INeutronLoadBalancerPoolMemberCRUD neutronLBPoolMemberCache;
+    private volatile INeutronPortCRUD neutronPortsCache;
+    private Map<String, LoadBalancerConfiguration> loadbalancersCache = Maps.newHashMap();
 
     @Override
     public int canCreateNeutronLoadBalancer(NeutronLoadBalancer neutronLoadBalancer) {
-        return HttpURLConnection.HTTP_CREATED;
+        if (loadbalancersCache.containsKey(neutronLoadBalancer.getLoadBalancerID()))
+            return HttpURLConnection.HTTP_CONFLICT;
+        return HttpURLConnection.HTTP_OK;
     }
 
+    /**
+     * Assuming that the pool information is fully populated before this call is made,
+     * we go with creating the LoadBalancerConfiguration object for this call with
+     * all information that is necessary to insert flow_mods
+     */
     @Override
     public void neutronLoadBalancerCreated(NeutronLoadBalancer neutronLoadBalancer) {
         logger.debug("Neutron LB Creation : {}", neutronLoadBalancer.toString());
-        //TODO: Trigger flowmod addition
-        int result = HttpURLConnection.HTTP_BAD_REQUEST;
 
-        result = canCreateNeutronLoadBalancer(neutronLoadBalancer);
-        if (result != HttpURLConnection.HTTP_CREATED) {
+        int result = canCreateNeutronLoadBalancer(neutronLoadBalancer);
+        if (result != HttpURLConnection.HTTP_OK) {
             logger.debug("Neutron Load Balancer creation failed {} ", result);
             return;
+        }
+
+        String loadBalancerID = neutronLoadBalancer.getLoadBalancerID();
+        String loadBalancerName = neutronLoadBalancer.getLoadBalancerName();
+        String loadBalancerVip = neutronLoadBalancer.getLoadBalancerVipAddress();
+        String loadBalancerSubnetID = neutronLoadBalancer.getLoadBalancerVipSubnetID();
+        LoadBalancerConfiguration newLB = new LoadBalancerConfiguration(loadBalancerName, loadBalancerVip);
+
+        String memberID, memberIP, memberMAC, memberProtocol;
+        Integer memberPort;
+
+        for (NeutronLoadBalancerPool neutronLBPool: neutronLBPoolCache.getAllNeutronLoadBalancerPools()) {
+            List<? extends NeutronLoadBalancerPoolMember> members =
+                (List<? extends NeutronLoadBalancerPoolMember>)neutronLBPool.getLoadBalancerPoolMembers();
+            memberProtocol = neutronLBPool.getLoadBalancerPoolProtocol();
+            /*
+             * Only HTTP and HTTPS are supported as of this version
+             * TODO: Support all TCP load-balancers
+             */
+            if (!(memberProtocol.equalsIgnoreCase(PROTOCOL_HTTP) ||
+                  memberProtocol.equalsIgnoreCase(PROTOCOL_HTTPS)))
+                continue;
+            for (NeutronLoadBalancerPoolMember neutronLBPoolMember: members) {
+                if (neutronLBPoolMember.getPoolMemberSubnetID().equals(loadBalancerSubnetID)) {
+                    memberID = neutronLBPoolMember.getPoolMemberID();
+                    memberIP = neutronLBPoolMember.getPoolMemberAddress();
+                    memberPort = neutronLBPoolMember.getPoolMemberProtoPort();
+                    memberMAC = this.getMacAddress(memberIP);
+                    if (memberMAC == null)
+                        continue;
+                    newLB.addMember(memberID, memberIP, memberMAC, memberProtocol, memberPort);
+                }
+            }
+        }
+        if (newLB.isValid()) {
+            logger.trace("Neutron LB pool configuration invalid for {} ", loadBalancerName);
+            return;
+        } else {
+            loadbalancersCache.put(loadBalancerID, newLB);
+            //TODO: Trigger flowmod addition
         }
     }
 
@@ -64,40 +126,42 @@ public class LBaaSHandler extends AbstractHandler
 
     @Override
     public int canDeleteNeutronLoadBalancer(NeutronLoadBalancer neutronLoadBalancer) {
+        if (!loadbalancersCache.containsKey(neutronLoadBalancer.getLoadBalancerID()))
+            return HttpURLConnection.HTTP_NOT_ACCEPTABLE;
         return HttpURLConnection.HTTP_OK;
     }
 
     @Override
     public void neutronLoadBalancerDeleted(NeutronLoadBalancer neutronLoadBalancer) {
         logger.debug("Neutron LB Deletion : {}", neutronLoadBalancer.toString());
-        //TODO: Trigger flowmod removals
+
         int result = canDeleteNeutronLoadBalancer(neutronLoadBalancer);
         if  (result != HttpURLConnection.HTTP_OK) {
             logger.error(" delete Neutron NeutronLoadBalancer Pool validation failed for result - {} ", result);
             return;
         }
+        loadbalancersCache.remove(neutronLoadBalancer.getLoadBalancerID());
+        //TODO: Trigger flowmod removals
     }
 
     /**
      * Invoked when a NeutronLoadBalancer Pools creation is requested
      * to indicate if the specified Rule can be created.
      *
-     * @param neutronLoadBalancerPool  An instance of proposed new Neutron LoadBalancer Pool object.
+     * @param neutronLBPool  An instance of proposed new Neutron LoadBalancer Pool object.
      * @return A HTTP status code to the creation request.
      */
 
     @Override
-    public int canCreateNeutronLoadBalancerPool(NeutronLoadBalancerPool neutronLoadBalancerPool) {
-        return HttpURLConnection.HTTP_CREATED;
+    public int canCreateNeutronLoadBalancerPool(NeutronLoadBalancerPool neutronLBPool) {
+        return HttpURLConnection.HTTP_OK;
     }
 
     @Override
-    public void neutronLoadBalancerPoolCreated(NeutronLoadBalancerPool neutronLoadBalancerPool) {
-        logger.debug("Neutron LB Pool Creation : {}", neutronLoadBalancerPool.toString());
-        int result = HttpURLConnection.HTTP_BAD_REQUEST;
-
-        result = canCreateNeutronLoadBalancerPool(neutronLoadBalancerPool);
-        if (result != HttpURLConnection.HTTP_CREATED) {
+    public void neutronLoadBalancerPoolCreated(NeutronLoadBalancerPool neutronLBPool) {
+        logger.debug("Neutron LB Pool Creation : {}", neutronLBPool.toString());
+        int result = canCreateNeutronLoadBalancerPool(neutronLBPool);
+        if (result != HttpURLConnection.HTTP_OK) {
             logger.debug("Neutron Load Balancer creation failed {} ", result);
             return;
         }
@@ -109,21 +173,21 @@ public class LBaaSHandler extends AbstractHandler
     }
 
     @Override
-    public void neutronLoadBalancerPoolUpdated(NeutronLoadBalancerPool neutronLoadBalancerPool) {
-        logger.debug("Neutron LB Pool updated : {}", neutronLoadBalancerPool.toString());
+    public void neutronLoadBalancerPoolUpdated(NeutronLoadBalancerPool neutronLBPool) {
+        logger.debug("Neutron LB Pool updated : {}", neutronLBPool.toString());
         return;
     }
 
     @Override
-    public int canDeleteNeutronLoadBalancerPool(NeutronLoadBalancerPool neutronLoadBalancerPool) {
+    public int canDeleteNeutronLoadBalancerPool(NeutronLoadBalancerPool neutronLBPool) {
         return HttpURLConnection.HTTP_OK;
     }
 
     @Override
-    public void neutronLoadBalancerPoolDeleted(NeutronLoadBalancerPool neutronLoadBalancerPool) {
-        logger.debug("Neutron LB Pool Deletion : {}", neutronLoadBalancerPool.toString());
+    public void neutronLoadBalancerPoolDeleted(NeutronLoadBalancerPool neutronLBPool) {
+        logger.debug("Neutron LB Pool Deletion : {}", neutronLBPool.toString());
 
-        int result = canDeleteNeutronLoadBalancerPool(neutronLoadBalancerPool);
+        int result = canDeleteNeutronLoadBalancerPool(neutronLBPool);
         if  (result != HttpURLConnection.HTTP_OK) {
             logger.error(" delete Neutron NeutronLoadBalancer Pool validation failed for result - {} ", result);
             return;
@@ -133,23 +197,21 @@ public class LBaaSHandler extends AbstractHandler
      * Invoked when a NeutronLoadBalancer Pool Members creation is requested
      * to indicate if the specified Rule can be created.
      *
-     * @param neutronLoadBalancerPoolMember  An instance of proposed new Neutron LoadBalancer Pool Member object.
+     * @param neutronLBPoolMember  An instance of proposed new Neutron LoadBalancer Pool Member object.
      * @return A HTTP status code to the creation request.
      */
 
     @Override
-    public int canCreateNeutronLoadBalancerPoolMember(NeutronLoadBalancerPoolMember neutronLoadBalancerPoolMember) {
-        return HttpURLConnection.HTTP_CREATED;
+    public int canCreateNeutronLoadBalancerPoolMember(NeutronLoadBalancerPoolMember neutronLBPoolMember) {
+        return HttpURLConnection.HTTP_OK;
     }
 
     @Override
-    public void neutronLoadBalancerPoolMemberCreated(NeutronLoadBalancerPoolMember neutronLoadBalancerPoolMember) {
-        logger.debug("Neutron LB Pool Member Creation : {}", neutronLoadBalancerPoolMember.toString());
+    public void neutronLoadBalancerPoolMemberCreated(NeutronLoadBalancerPoolMember neutronLBPoolMember) {
+        logger.debug("Neutron LB Pool Member Creation : {}", neutronLBPoolMember.toString());
 
-        int result = HttpURLConnection.HTTP_BAD_REQUEST;
-
-        result = canCreateNeutronLoadBalancerPoolMember(neutronLoadBalancerPoolMember);
-        if (result != HttpURLConnection.HTTP_CREATED) {
+        int result = canCreateNeutronLoadBalancerPoolMember(neutronLBPoolMember);
+        if (result != HttpURLConnection.HTTP_OK) {
             logger.debug("Neutron Load Balancer creation failed {} ", result);
             return;
         }
@@ -161,21 +223,21 @@ public class LBaaSHandler extends AbstractHandler
     }
 
     @Override
-    public void neutronLoadBalancerPoolMemberUpdated(NeutronLoadBalancerPoolMember neutronLoadBalancerPoolMember) {
-        logger.debug("Neutron LB Pool Member updated : {}", neutronLoadBalancerPoolMember.toString());
+    public void neutronLoadBalancerPoolMemberUpdated(NeutronLoadBalancerPoolMember neutronLBPoolMember) {
+        logger.debug("Neutron LB Pool Member updated : {}", neutronLBPoolMember.toString());
         return;
     }
 
     @Override
-    public int canDeleteNeutronLoadBalancerPoolMember(NeutronLoadBalancerPoolMember neutronLoadBalancerPoolMember) {
+    public int canDeleteNeutronLoadBalancerPoolMember(NeutronLoadBalancerPoolMember neutronLBPoolMember) {
         return HttpURLConnection.HTTP_OK;
     }
 
     @Override
-    public void neutronLoadBalancerPoolMemberDeleted(NeutronLoadBalancerPoolMember neutronLoadBalancerPoolMember) {
-        logger.debug("Neutron LB Pool Member Deletion : {}", neutronLoadBalancerPoolMember.toString());
+    public void neutronLoadBalancerPoolMemberDeleted(NeutronLoadBalancerPoolMember neutronLBPoolMember) {
+        logger.debug("Neutron LB Pool Member Deletion : {}", neutronLBPoolMember.toString());
 
-        int result = canDeleteNeutronLoadBalancerPoolMember(neutronLoadBalancerPoolMember);
+        int result = canDeleteNeutronLoadBalancerPoolMember(neutronLBPoolMember);
         if  (result != HttpURLConnection.HTTP_OK) {
             logger.error(" delete Neutron NeutronLoadBalancer Pool Member validation failed for result - {} ", result);
             return;
@@ -203,5 +265,31 @@ public class LBaaSHandler extends AbstractHandler
                 break;
         }
     }
-}
 
+    /**
+     * Look up in the NeutronPortsCRUD cache and return the MAC address for a corresponding IP address
+     * @param ipAddr IP address of a member or VM
+     * @return MAC address registered with that IP address
+     */
+    private String getMacAddress(String ipAddr) {
+            List<Neutron_IPs> fixedIPs;
+            Iterator<Neutron_IPs> fixedIPIterator;
+            Neutron_IPs ip;
+
+            List<NeutronPort> allPorts = neutronPortsCache.getAllPorts();
+         Iterator<NeutronPort> i = allPorts.iterator();
+         while (i.hasNext()) {
+             NeutronPort port = i.next();
+             fixedIPs = port.getFixedIPs();
+             if (fixedIPs != null && fixedIPs.size() > 0) {
+                 fixedIPIterator = fixedIPs.iterator();
+                 while (fixedIPIterator.hasNext()) {
+                     ip = fixedIPIterator.next();
+                     if (ip.getIpAddress().equals(ipAddr))
+                         return port.getMacAddress();
+                 }
+             }
+         }
+        return null;
+    }
+}

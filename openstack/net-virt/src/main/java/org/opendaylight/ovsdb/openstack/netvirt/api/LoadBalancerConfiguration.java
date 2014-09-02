ee7d65417b977779ec4c3b54dea3b3b5e81a9f36
Srini Seetharaman <srini.seetharaman@gmail.com>
2014-09-02 12:32:55 -0700

    Making changes to LBaaSHandler and Activator to add logic for LB Neutron calls.
    - Cleaning up LBaaSHandler class to only handle the NEUTRON_LOAD_BALANCER event
    - Cleaning up LBaaSHandler to only be INeutronLoadBalancerAware
    - Adding the service LoadBalancerProvider and adding dependency in Activator
    - Adding logic to handle the neutron LB instance creation call. After the call,
      the information will be stored in a Map <UUID, LoadBalancerConfiguration>
      object. Still TODO: FlowMods through the LoadBalancerService.
    
    Change-Id: Ic20d10ea680730de59bbe60a12c47fc6684a6bab
    Signed-off-by: Srini Seetharaman <srini.seetharaman@gmail.com>

---
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/LoadBalancerConfiguration.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (C) 2014 SDN Hub, LLC.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Authors : Srini Seetharaman
+ */
+
+package org.opendaylight.ovsdb.openstack.netvirt.api;
+
+import com.google.common.collect.Maps;
+
+import java.util.Map;
+
+/**
+ * Store configuration for each load balancer instance created.
+ */
+
+public class LoadBalancerConfiguration {
+    public static final String PROTOCOL_HTTP = "HTTP";
+    public static final String PROTOCOL_HTTPS = "HTTPS";
+    public static final Integer PROTOCOL_HTTP_PORT = 80;
+    public static final Integer PROTOCOL_HTTPS_PORT = 443;
+
+    public class LoadBalancerPoolMember {
+        String ipAddr;
+        String macAddr;
+        String protocol;
+        Integer port;
+        int index;
+
+        public LoadBalancerPoolMember(String ipAddr, String macAddr, String protocol, Integer port) {
+            this.ipAddr = ipAddr;
+            this.macAddr = macAddr;
+            this.protocol = protocol;
+            this.port = port;
+            this.index = -1;
+        }
+        public LoadBalancerPoolMember(String ipAddr, String macAddr, String protocol, Integer port, int index) {
+            this.ipAddr = ipAddr;
+            this.macAddr = macAddr;
+            this.protocol = protocol;
+            this.port = port;
+            this.index = index;
+        }
+        public String getIP() {
+            return ipAddr;
+        }
+        public String getMAC() {
+            return macAddr;
+        }
+        public String getProtocol() {
+            return protocol;
+        }
+        public Integer getPort() {
+            return port;
+        }
+        public int getIndex() {
+            return index;
+        }
+        public void setIndex(int index) {
+            this.index = index;
+        }
+        public boolean equals(LoadBalancerPoolMember other) {
+            if (other.ipAddr != ipAddr)
+                return false;
+            else if (other.macAddr != macAddr)
+                return false;
+            else if (other.protocol != protocol)
+                return false;
+            else if (other.port != port)
+                return false;
+            //Ignore Index
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "LoadBalancerPoolMember [ip=" + ipAddr + ", mac=" + macAddr +
+                    ", protocol=" + protocol + ", port=" + port + ", index=" + index + "]";
+        }
+
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = super.hashCode();
+            result = prime * result + ((ipAddr == null) ? 0 : ipAddr.hashCode());
+            result = prime * result + ((macAddr == null) ? 0 : macAddr.hashCode());
+            result = prime * result + ((protocol == null) ? 0 : protocol.hashCode());
+            result = prime * result + ((port == null) ? 0 : port.hashCode());
+            result = prime * result + index;
+            return result;
+        }
+    }
+
+    private String name;
+    private String vip;
+    private Map <String, LoadBalancerPoolMember> members;
+
+    public LoadBalancerConfiguration() {
+        this.members = Maps.newHashMap();
+    }
+
+    public LoadBalancerConfiguration(String name, String vip) {
+        this.members = Maps.newHashMap();
+        this.name = name;
+        this.vip = vip;
+    }
+
+    public Map<String, LoadBalancerPoolMember> getMembers() {
+        return this.members;
+    }
+
+    public Map<String, LoadBalancerPoolMember> addMember(String uuid, LoadBalancerPoolMember member) {
+        //If index is not set for this object, update it before inserting
+        if (member.getIndex() == -1)
+            member.setIndex(members.size());
+        this.members.put(uuid, member);
+        return this.members;
+    }
+    public Map<String, LoadBalancerPoolMember> addMember(String uuid, String ipAddr, String macAddr, String protocol, Integer port) {
+        this.members.put(uuid,
+                new LoadBalancerPoolMember(ipAddr, macAddr, protocol, port, members.size()));
+        return this.members;
+    }
+    public Map<String, LoadBalancerPoolMember> removeMember(String uuid) {
+        this.members.remove(uuid);
+        return this.members;
+    }
+
+    public boolean isValid() {
+        if (members.size() == 0)
+            return false;
+        return true;
+    }
+    public void setVip(String vip) {
+        this.vip = vip;
+    }
+
+    public String getVip() {
+        return this.vip;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}

2db32f018dd73393de5f142f38d719795528bb67
Madhu Venugopal <mavenugo@gmail.com>
2014-09-11 06:06:36 -0700

    Bug 1811 Fixed the pipeline by moving L2Fwd to the end.
    Change-Id: I4192003c7b24d7bfb39abbfbcaf0772bb473a2a3
    Signed-off-by: Madhu Venugopal <mavenugo@gmail.com>

--- openstack/net-virt-providers/src/test/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/PipelineOrchestratorTest.java
+++ openstack/net-virt-providers/src/test/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/PipelineOrchestratorTest.java
@@ -21,9 +21,9 @@ public class PipelineOrchestratorTest {
         assertEquals(orchestrator.getNextServiceInPipeline(Service.INGRESS_ACL), Service.LOAD_BALANCER);
         assertEquals(orchestrator.getNextServiceInPipeline(Service.LOAD_BALANCER), Service.ROUTING);
         assertEquals(orchestrator.getNextServiceInPipeline(Service.ROUTING), Service.L2_REWRITE);
-        assertEquals(orchestrator.getNextServiceInPipeline(Service.L2_REWRITE), Service.L2_FORWARDING);
-        assertEquals(orchestrator.getNextServiceInPipeline(Service.L2_FORWARDING), Service.EGRESS_ACL);
+        assertEquals(orchestrator.getNextServiceInPipeline(Service.L2_REWRITE), Service.EGRESS_ACL);
         assertEquals(orchestrator.getNextServiceInPipeline(Service.EGRESS_ACL), Service.OUTBOUND_NAT);
-        assertNull(orchestrator.getNextServiceInPipeline(Service.OUTBOUND_NAT));
+        assertEquals(orchestrator.getNextServiceInPipeline(Service.OUTBOUND_NAT), Service.L2_FORWARDING);
+        assertNull(orchestrator.getNextServiceInPipeline(Service.L2_FORWARDING));
     }
 }

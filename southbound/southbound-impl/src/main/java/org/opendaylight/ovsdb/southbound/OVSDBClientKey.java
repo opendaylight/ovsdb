1e2dcc194d690c287a1c1c108d49f6d9779aa9bf
Ed Warnicke <eaw@cisco.com>
2015-01-19 13:17:53 -0700

    Rough musings towards an MD-SAL OVSDB SB
    This patch is incomplete, it only manages to accept
    ovsdb connections and log them, but its a start.
    
    PatchSet2: Added a simple starting model around tunneling
    PatchSet3: Added additional tunnel info
    PatchSet4: Added ovsdb.yang model.  Separated tunnel into tunnel.yang
    PatchSet5: Renamed tunnel.yang to overlay.yang.  Added topology-type info.
    PatchSet6: Added in dummy DataChangeListener for OvsdbNodes.
               Moved mapping functions into a mapping class.
               Handled tracking of OvsdbClients
    PatchSet7:  Responded to comments:
                Fixed whitespace
                Fixed parent for aggregator
                Fixed features from odl-southbound- to odl-ovsdb-southbound-
    PatchSet10: Added DataChangeListner to listen for new ovsdb-nodes in topology
                and connect to them.
    PatchSet15: Response to Colin's comments
    PatchSet18: Fixed feature dependencies.  Depends on patch
                https://git.opendaylight.org/gerrit/#/c/14791/ in controller
    PatchSet19: Fixed small error in startup order
    
    
    Change-Id: I13179ec4a1c338ae239f79c0812adf4bc4d6e76d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OVSDBClientKey.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.southbound;
+
+import org.opendaylight.ovsdb.lib.OvsdbClient;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.overlay.rev150105.IpPortLocator;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+public class OVSDBClientKey {
+    /*
+     * This class is immutable.  If you are in anyway changing its fields after
+     * creation, your are doing it wrong :)
+     */
+    private IpAddress ipaddress;
+    private PortNumber port;
+
+    OVSDBClientKey(IpPortLocator locator) {
+        ipaddress = locator.getIp();
+        port = locator.getPort();
+    }
+
+    OVSDBClientKey(OvsdbClient client) {
+        ipaddress = SouthboundMapper.createIpAddress(client.getConnectionInfo().getRemoteAddress());
+        port = new PortNumber(client.getConnectionInfo().getRemotePort());
+    }
+
+    public IpAddress getIp() {
+        return ipaddress;
+    }
+
+    public PortNumber getPort() {
+        return port;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result
+                + ((ipaddress == null) ? 0 : ipaddress.hashCode());
+        result = prime * result + ((port == null) ? 0 : port.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        OVSDBClientKey other = (OVSDBClientKey) obj;
+        if (ipaddress == null) {
+            if (other.ipaddress != null)
+                return false;
+        } else if (!ipaddress.equals(other.ipaddress))
+            return false;
+        if (port == null) {
+            if (other.port != null)
+                return false;
+        } else if (!port.equals(other.port))
+            return false;
+        return true;
+    }
+
+    InstanceIdentifier<Node> toInstanceIndentifier() {
+        return SouthboundMapper.createInstanceIdentifier(ipaddress,port);
+    }
+}

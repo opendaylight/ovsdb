bbd4ea37074415858e536e9f5c116f233c2f59a8
Ed Warnicke <eaw@cisco.com>
2015-01-19 13:17:53 -0700

    Rough musings towards an MD-SAL OVSDB SB
    This patch is incomplete, it only manages to accept
    ovsdb connections and log them, but its a start.
    
    PatchSet2: Added a simple starting model around tunneling
    PatchSet3: Added additional tunnel info
    PatchSet4: Added ovsdb.yang model.  Separated tunnel into tunnel.yang
    PatchSet5: Renamed tunnel.yang to overlay.yang.  Added topology-type info.
    PatchSet6: Added in dummy DataChangeListener for OvsdbNodes.
               Moved mapping functions into a mapping class.
               Handled tracking of OvsdbClients
    PatchSet7:  Responded to comments:
                Fixed whitespace
                Fixed parent for aggregator
                Fixed features from odl-southbound- to odl-ovsdb-southbound-
    PatchSet10: Added DataChangeListner to listen for new ovsdb-nodes in topology
                and connect to them.
    PatchSet15: Response to Colin's comments
    PatchSet18: Fixed feature dependencies.  Depends on patch
                https://git.opendaylight.org/gerrit/#/c/14791/ in controller
    PatchSet19: Fixed small error in startup order
    
    Change-Id: I13179ec4a1c338ae239f79c0812adf4bc4d6e76d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/SouthboundConstants.java
@@ -0,0 +1,10 @@
+package org.opendaylight.ovsdb.southbound;
+
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+
+public class SouthboundConstants {
+    public static final TopologyId OVSDB_TOPOLOGY_ID = new TopologyId(new Uri("ovsdb:1"));
+    public static final String OVSDB_URI_PREFIX = "ovsdb";
+    public static final Integer DEFAULT_OVSDB_PORT = 6640;
+}

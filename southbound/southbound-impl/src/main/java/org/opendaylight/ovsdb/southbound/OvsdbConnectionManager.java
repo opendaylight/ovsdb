a1c96e88853610b62de8b8dd6714d09449cb0777
Ed Warnicke <eaw@cisco.com>
2015-01-19 13:17:53 -0700

    Rough musings towards an MD-SAL OVSDB SB
    This patch is incomplete, it only manages to accept
    ovsdb connections and log them, but its a start.
    
    PatchSet2: Added a simple starting model around tunneling
    PatchSet3: Added additional tunnel info
    PatchSet4: Added ovsdb.yang model.  Separated tunnel into tunnel.yang
    PatchSet5: Renamed tunnel.yang to overlay.yang.  Added topology-type info.
    PatchSet6: Added in dummy DataChangeListener for OvsdbNodes.
               Moved mapping functions into a mapping class.
               Handled tracking of OvsdbClients
    PatchSet7:  Responded to comments:
                Fixed whitespace
                Fixed parent for aggregator
                Fixed features from odl-southbound- to odl-ovsdb-southbound-
    PatchSet10: Added DataChangeListner to listen for new ovsdb-nodes in topology
                and connect to them.
    PatchSet15: Response to Colin's comments
    PatchSet18: Fixed feature dependencies.  Depends on patch
                https://git.opendaylight.org/gerrit/#/c/14791/ in controller
    
    Change-Id: I13179ec4a1c338ae239f79c0812adf4bc4d6e76d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionManager.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.southbound;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.ovsdb.lib.OvsdbClient;
+import org.opendaylight.ovsdb.lib.OvsdbConnectionListener;
+import org.opendaylight.ovsdb.lib.impl.OvsdbConnectionService;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OvsdbConnectionManager implements OvsdbConnectionListener, AutoCloseable {
+    Map<OVSDBClientKey,OvsdbClient> clients = new ConcurrentHashMap<OVSDBClientKey,OvsdbClient>();
+    private static final Logger LOG = LoggerFactory.getLogger(OvsdbConnectionManager.class);
+
+    DataBroker db;
+
+    public OvsdbConnectionManager(DataBroker db) {
+        this.db = db;
+    }
+
+    @Override
+    public void connected(OvsdbClient client) {
+        LOG.info("OVSDB Connection from {}:{}",client.getConnectionInfo().getRemoteAddress(),
+                client.getConnectionInfo().getRemotePort());
+        OVSDBClientKey key = new OVSDBClientKey(client);
+        clients.put(key, client);
+        WriteTransaction transaction = db.newWriteOnlyTransaction();
+        transaction.put(LogicalDatastoreType.OPERATIONAL, key.toInstanceIndentifier(),
+                SouthboundMapper.createNode(client));
+        // TODO - Check the future and retry if needed
+        transaction.submit();
+    }
+
+    @Override
+    public void disconnected(OvsdbClient client) {
+        LOG.info("OVSDB Disconnect from {}:{}",client.getConnectionInfo().getRemoteAddress(),
+                client.getConnectionInfo().getRemotePort());
+        OVSDBClientKey key = new OVSDBClientKey(client);
+        WriteTransaction transaction = db.newWriteOnlyTransaction();
+        transaction.delete(LogicalDatastoreType.OPERATIONAL, key.toInstanceIndentifier());
+        // TODO - Check the future and retry if needed
+        transaction.submit();
+        clients.remove(key);
+    }
+
+    public OvsdbClient connect(OvsdbNodeAugmentation ovsdbNode) throws UnknownHostException {
+        // TODO handle case where we already have a connection
+        // TODO use transaction chains to handle ordering issues between disconnected and connected when writing to the operational store
+        InetAddress ip = SouthboundMapper.createInetAddress(ovsdbNode.getIp());
+        OvsdbClient client = OvsdbConnectionService.getService().connect(ip, ovsdbNode.getPort().getValue().intValue());
+        OVSDBClientKey key = new OVSDBClientKey(client);
+        clients.put(key, client);
+        connected(client); // For connections from the controller to the ovs instance, the library doesn't call this method for us
+        return client;
+    }
+
+    @Override
+    public void close() throws Exception {
+        for(OvsdbClient client: clients.values()) {
+            client.disconnect();
+        }
+    }
+
+
+}
---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/SouthboundMapper.java
@@ -0,0 +1,103 @@
+package org.opendaylight.ovsdb.southbound;
+
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.opendaylight.ovsdb.lib.OvsdbClient;
+import org.opendaylight.ovsdb.lib.OvsdbConnectionInfo;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.IpAddress;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv4Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Ipv6Address;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.PortNumber;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SouthboundMapper {
+    private static final Logger LOG = LoggerFactory.getLogger(SouthboundMapper.class);
+
+    public static Node createNode(OvsdbClient client) {
+        NodeBuilder nodeBuilder = new NodeBuilder();
+        nodeBuilder.setNodeId(createNodeId(client.getConnectionInfo()));
+        nodeBuilder.addAugmentation(OvsdbNodeAugmentation.class, createOvsdbAugmentation(client));
+        return nodeBuilder.build();
+    }
+
+    public static OvsdbNodeAugmentation createOvsdbAugmentation(OvsdbClient client) {
+        OvsdbNodeAugmentationBuilder ovsdbNodeBuilder = new OvsdbNodeAugmentationBuilder();
+        ovsdbNodeBuilder.setIp(createIpAddress(client.getConnectionInfo().getRemoteAddress()));
+        ovsdbNodeBuilder.setPort(new PortNumber(client.getConnectionInfo().getRemotePort()));
+        return ovsdbNodeBuilder.build();
+    }
+
+    public static IpAddress createIpAddress(InetAddress address) {
+        IpAddress ip = null;
+        if(address instanceof Inet4Address) {
+            ip = createIpAddress((Inet4Address)address);
+        } else if (address instanceof Inet6Address) {
+            ip = createIpAddress((Inet6Address)address);
+        }
+        return ip;
+    }
+
+    public static IpAddress createIpAddress(Inet4Address address) {
+        Ipv4Address ipv4 = new Ipv4Address(address.getHostAddress());
+        return new IpAddress(ipv4);
+    }
+
+    public static IpAddress createIpAddress(Inet6Address address) {
+        Ipv6Address ipv6 = new Ipv6Address(address.getHostAddress());
+        return new IpAddress(ipv6);
+    }
+
+    public static InstanceIdentifier<Node> createInstanceIdentifier(OvsdbClient client) {
+        return createInstanceIdentifier(createIpAddress(client.getConnectionInfo().getRemoteAddress()),
+                new PortNumber(client.getConnectionInfo().getRemotePort()));
+    }
+    public static InstanceIdentifier<Node> createInstanceIdentifier(IpAddress ip, PortNumber port) {
+        InstanceIdentifier<Node> path = InstanceIdentifier
+                .create(NetworkTopology.class)
+                .child(Topology.class, new TopologyKey(SouthboundConstants.OVSDB_TOPOLOGY_ID))
+                .child(Node.class,createNodeKey(ip,port));
+        LOG.info("Created ovsdb path: {}",path);
+        return path;
+    }
+
+    public static NodeKey createNodeKey(IpAddress ip, PortNumber port) {
+        return new NodeKey(createNodeId(ip,port));
+    }
+
+    public static NodeId createNodeId(OvsdbConnectionInfo connectionInfo) {
+        return createNodeId(createIpAddress(connectionInfo.getRemoteAddress()),
+                new PortNumber(connectionInfo.getRemotePort()));
+    }
+    public static NodeId createNodeId(IpAddress ip, PortNumber port) {
+        String uriString = SouthboundConstants.OVSDB_URI_PREFIX + ":/" + new String(ip.getValue()) +
+                   ":" + port.getValue();
+        Uri uri = new Uri(uriString);
+        NodeId nodeId = new NodeId(uri);
+        return nodeId;
+    }
+
+    public static InetAddress createInetAddress(IpAddress ip) throws UnknownHostException {
+        if(ip.getIpv4Address() != null) {
+            return InetAddress.getByName(ip.getIpv4Address().getValue());
+        } else if(ip.getIpv6Address() != null) {
+            return InetAddress.getByName(ip.getIpv6Address().getValue());
+        } else {
+            throw new UnknownHostException("IP Address has no value");
+        }
+    }
+}

c871dcf61693aa6d6fb7cad0be42e90ff331e89a
Sharad Mishra <sharad.d.mishra@intel.com>
2015-02-11 10:13:18 -0800

    MD-SAL OVSDB SB - handle update to ovsdb node.
    Patch to handle ovsdb node update.
    
    Change-Id: Ia8a279de1ec17e7149794f4ef6e0b5900e04dccc
    Signed-off-by: Sharad Mishra <sharad.d.mishra@intel.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbNodeDataChangeListener.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbNodeDataChangeListener.java
@@ -47,22 +47,34 @@ public class OvsdbNodeDataChangeListener implements DataChangeListener, AutoClos
            // TODO validate we have the correct kind of InstanceIdentifier
            if(created.getValue() instanceof OvsdbNodeAugmentation) {
                try {
+                LOG.info("sharad in connect");
                 cm.connect((OvsdbNodeAugmentation)created.getValue());
             } catch (UnknownHostException e) {
                 LOG.warn("Failed to connect to ovsdbNode", e);
             }
            }
        }
-       // TODO handle case of updates to ovsdb nodes as needed
 
         Map<InstanceIdentifier<?>, DataObject> originalDataObject = changes.getOriginalData();
         Set<InstanceIdentifier<?>> iID = changes.getRemovedPaths();
         for (InstanceIdentifier instanceIdentifier : iID) {
             if (originalDataObject.get(instanceIdentifier) instanceof OvsdbNodeAugmentation) {
                 try {
+                    LOG.info("sharad in disconnect");
                     cm.disconnect((OvsdbNodeAugmentation)originalDataObject.get(instanceIdentifier));
                 } catch (UnknownHostException e) {
                     LOG.warn("Failed to disconnect ovsdbNode", e);
+                }
+            }
+        }
+
+        for( Entry<InstanceIdentifier<?>, DataObject> updated : changes.getUpdatedData().entrySet()) {
+            if (updated.getValue() instanceof OvsdbNodeAugmentation) {
+                try {
+                    LOG.info("sharad in update");
+                    cm.connect((OvsdbNodeAugmentation)updated.getValue());
+                } catch (UnknownHostException e) {
+                    LOG.warn("Failed to update ovsdbNode", e);
                 }
             }
         }

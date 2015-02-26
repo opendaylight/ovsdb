0a0d2a0d382bd49640f6105cbdabd8798a01ac22
Eric Multanen <eric.w.multanen@intel.com>
2015-02-26 07:49:15 -0800

    Add Data Change Listeners for Managed Nodes and Ports
    Instantiate data change listeners in the OVSDB
    SoutboundProvider for the OVSDB Managed Node and
    Port Termination Point augmentations.
    
    Change-Id: I699ed9522a5efef5d209c2361583bf0d248239bb
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbPortTerminationPointDataChangeListener.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2015 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.southbound;
+
+import java.net.UnknownHostException;
+import java.util.Map.Entry;
+import java.util.Map;
+import java.util.Set;
+
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.DataChangeListener;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataBroker.DataChangeScope;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbPortTerminationPointAugmentation;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class OvsdbPortTerminationPointDataChangeListener implements DataChangeListener, AutoCloseable {
+
+    private ListenerRegistration<DataChangeListener> registration;
+    private OvsdbConnectionManager cm;
+    private static final Logger LOG = LoggerFactory.getLogger(OvsdbPortTerminationPointDataChangeListener.class);
+
+    OvsdbPortTerminationPointDataChangeListener(DataBroker db, OvsdbConnectionManager cm) {
+        LOG.info("Registering OvsdbPortTerminationPointDataChangeListener");
+        this.cm = cm;
+        InstanceIdentifier<OvsdbPortTerminationPointAugmentation> path = InstanceIdentifier
+                .create(NetworkTopology.class)
+                .child(Topology.class, new TopologyKey(SouthboundConstants.OVSDB_TOPOLOGY_ID))
+                .child(Node.class)
+                .child(TerminationPoint.class)
+                .augmentation(OvsdbPortTerminationPointAugmentation.class);
+        registration = db.registerDataChangeListener(LogicalDatastoreType.CONFIGURATION, path, this, DataChangeScope.ONE);
+
+    }
+
+    @Override
+    public void onDataChanged(
+            AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
+       LOG.info("Received change to ovsdbNode: {}", changes);
+
+       // TODO validate we have the correct kind of InstanceIdentifier
+       // TODO handle case of updates to ovsdb ports as needed
+
+       // TODO handle case of deletes to ovsdb ports as needed
+    }
+
+    @Override
+    public void close() throws Exception {
+        registration.close();
+    }
+
+}

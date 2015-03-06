11fc04f1d06dcb023f9478a3f9beee1aada064dc
Sam Hague <shague@redhat.com>
2015-03-05 22:20:11 -0500

    Initial patch to deprecate sal utils Servicehelper
    Change-Id: I952e22647bf64f0bd889e1151536751f935bc0eb
    Signed-off-by: Sam Hague <shague@redhat.com>

---
+++ utils/servicehelper/src/test/java/org/opendaylight/ovsdb/utils/servicehelper/ServiceHelperTest.java
@@ -0,0 +1,51 @@
+/*
+ *  Copyright (C) 2015 Red Hat, Inc.
+ *
+ *  This program and the accompanying materials are made available under the
+ *  terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ *  and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ *  Authors : Sam Hague
+ */
+package org.opendaylight.ovsdb.utils.servicehelper;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Matchers.any;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.springframework.osgi.mock.MockBundle;
+
+/**
+ * JUnit test for
+ * {@link org.opendaylight.ovsdb.utils.servicehelper.ServiceHelper}
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(FrameworkUtil.class)
+public class ServiceHelperTest {
+    @Test
+    /**
+     * Test method for
+     * {@link ServiceHelper#getGlobalInstance(Class, Object)}
+     */
+    public void getGlobalInstanceTest () {
+        Bundle bundle = new MockBundle();
+
+        PowerMockito.mockStatic(FrameworkUtil.class);
+        PowerMockito.when(FrameworkUtil.getBundle(any(Class.class)))
+                .thenReturn(null)
+                .thenReturn(bundle);
+
+        Object object = ServiceHelper.getGlobalInstance(Test.class, this);
+        assertNull("Service should be null", object);
+
+        object = ServiceHelper.getGlobalInstance(Test.class, this);
+        assertNotNull("Service should not be null", object);
+    }
+}

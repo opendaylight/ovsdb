7e40b4c26425c7f5912c3b6ba92dd138e4a55dec
Ed Warnicke <eaw@cisco.com>
2015-03-15 17:16:22 -0500

    DO NOT MERGE (YET) - Add controller to bridge
    Change-Id: I6f7b7b53201da053605fe965908a00785651e46e
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/BridgeCreateCommand.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/BridgeCreateCommand.java
@@ -9,7 +9,11 @@ package org.opendaylight.ovsdb.southbound.ovsdb.transact;
 
 import static org.opendaylight.ovsdb.lib.operations.Operations.op;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
 import org.opendaylight.ovsdb.lib.notation.Mutator;
@@ -17,9 +21,11 @@ import org.opendaylight.ovsdb.lib.notation.UUID;
 import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
 import org.opendaylight.ovsdb.lib.schema.typed.TyperUtils;
 import org.opendaylight.ovsdb.schema.openvswitch.Bridge;
+import org.opendaylight.ovsdb.schema.openvswitch.Controller;
 import org.opendaylight.ovsdb.schema.openvswitch.OpenVSwitch;
 import org.opendaylight.ovsdb.southbound.SouthboundMapper;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.bridge.attributes.ControllerEntry;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
@@ -48,12 +54,20 @@ public class BridgeCreateCommand implements TransactCommand {
                     && SouthboundMapper.createOvsdbBridgeProtocols(omn).size() > 0){
                 bridge.setProtocols(SouthboundMapper.createOvsdbBridgeProtocols(omn));
             }
-            String namedUuid = "Bridge_" + omn.getBridgeName().getValue();
-            transaction.add(op.insert(bridge).withId(namedUuid));
+            Map<UUID,Controller> controllerMap = SouthboundMapper.createOvsdbController(omn, transaction.getDatabaseSchema());
+            for(Entry<UUID,Controller >entry: controllerMap.entrySet()) {
+                transaction.add(op.insert(entry.getValue()).withId(entry.getKey().toString()));
+            }
+            if(!controllerMap.isEmpty()) {
+                bridge.setController(controllerMap.keySet());
+            }
+
+            String bridgeNamedUuid = "Bridge_" + omn.getBridgeName().getValue();
+            transaction.add(op.insert(bridge).withId(bridgeNamedUuid));
 
             // OpenVSwitchPart
             OpenVSwitch ovs = TyperUtils.getTypedRowWrapper(transaction.getDatabaseSchema(), OpenVSwitch.class);
-            ovs.setBridges(Sets.newHashSet(new UUID(namedUuid)));
+            ovs.setBridges(Sets.newHashSet(new UUID(bridgeNamedUuid)));
             transaction.add(op.mutate(ovs).addMutation(ovs.getBridgesColumn().getSchema(),
                     Mutator.INSERT,
                     ovs.getBridgesColumn().getData())

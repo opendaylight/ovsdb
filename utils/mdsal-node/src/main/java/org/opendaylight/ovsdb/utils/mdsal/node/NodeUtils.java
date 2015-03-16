2f51a5faf431f1591db6a67cb718f04ccd41937c
Flavio Fernandes <ffernand@redhat.com>
2015-03-16 14:30:54 -0400

    DO NOT MERGE -- Remove org.opendaylight.controller.sal
    Change-Id: Icbee1c52e13f44f42c5ce13e68b98dadbac2b70f
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

--- utils/mdsal-node/src/main/java/org/opendaylight/ovsdb/utils/mdsal/node/NodeUtils.java
+++ utils/mdsal-node/src/main/java/org/opendaylight/ovsdb/utils/mdsal/node/NodeUtils.java
@@ -9,19 +9,12 @@
  */
 package org.opendaylight.ovsdb.utils.mdsal.node;
 
-import java.util.ArrayList;
-import java.util.List;
-import org.opendaylight.controller.sal.core.ConstructionException;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.NodeId;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.NodeKey;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class NodeUtils {
-    protected static final Logger LOG = LoggerFactory.getLogger(NodeUtils.class);
-
     public static String getId (String identifier) {
         String id = identifier;
 
@@ -41,40 +34,5 @@ public class NodeUtils {
                 .build();
 
         return node;
-    }
-
-    public static Node getMdsalNode (org.opendaylight.controller.sal.core.Node salNode) {
-        String identifier = salNode.getNodeIDString();
-
-        NodeId nodeId = new NodeId("OVS" + "|" + identifier);
-        NodeKey nodeKey = new NodeKey(nodeId);
-        Node node = new NodeBuilder()
-                .setId(nodeId)
-                .setKey(nodeKey)
-                .build();
-
-        return node;
-    }
-
-    public static org.opendaylight.controller.sal.core.Node getSalNode (Node mdsalNode) {
-        String identifier = NodeUtils.getId(mdsalNode.getId().getValue());
-        org.opendaylight.controller.sal.core.Node node = null;
-
-        try {
-            node = new org.opendaylight.controller.sal.core.Node("OVS", identifier);
-        } catch (ConstructionException e) {
-            LOG.error("Failed to allocate sal Node", e);
-        }
-
-        return node;
-    }
-
-    public static List<org.opendaylight.controller.sal.core.Node> getSalNodes (List<Node> mdsalNodes) {
-        List<org.opendaylight.controller.sal.core.Node> nodes = new ArrayList<>();
-
-        for (Node mdsalNode : mdsalNodes) {
-            nodes.add(NodeUtils.getSalNode(mdsalNode));
-        }
-        return nodes;
     }
 }

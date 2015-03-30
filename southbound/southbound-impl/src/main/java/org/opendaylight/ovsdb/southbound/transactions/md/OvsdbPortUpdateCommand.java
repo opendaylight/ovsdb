11d2c7851296f8589cd7de9670d2a95ce5811350
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-03-30 14:01:04 -0400

    Added trunks code to go with the model.
    Change-Id: I514f8d8ad7d786901061b14909735cee9bb412f6
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbPortUpdateCommand.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbPortUpdateCommand.java
@@ -10,6 +10,7 @@ package org.opendaylight.ovsdb.southbound.transactions.md;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
@@ -34,6 +35,8 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.re
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeName;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.Trunks;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.TrunksBuilder;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TpId;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
@@ -69,6 +72,8 @@ public class OvsdbPortUpdateCommand extends AbstractTransactionCommand {
                 for (Port port : portUpdatedRows) {
                     if (portUUID.equals(port.getUuid())) {
                         Collection<Long> vlanId = port.getTagColumn().getData();
+                        Set<Short> portTrunks = new HashSet<Short>();
+                        portTrunks = port.getTrunks().getData();
                         bridgeName = bridge.getName();
                         NodeId bridgeId = SouthboundMapper.createManagedNodeId(
                                 getKey(), new OvsdbBridgeName(bridgeName));
@@ -107,6 +112,12 @@ public class OvsdbPortUpdateCommand extends AbstractTransactionCommand {
                                     ovsdbTerminationPointBuilder.setVlanTag(new VlanId(id));
                                 }
                             }
+                            List<Trunks> modelTrunks = new ArrayList<Trunks>();
+                            for (Short trunk: portTrunks) {
+                                Integer trunkIntValue = new Integer(trunk);
+                                modelTrunks.add(new TrunksBuilder().setTrunk(new VlanId(trunkIntValue)).build());
+                            }
+                            ovsdbTerminationPointBuilder.setTrunks(modelTrunks);
                             Column<GenericTableSchema, Set<UUID>> iface = port.getInterfacesColumn();
                             Set<UUID> ifUuid = iface.getData();
                             Collection<Interface> ifUpdateRows = TyperUtils.extractRowsUpdated(

9e6b4db61d9f0334bbb2ebdc38842017e5cf2876
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-04-06 15:50:56 -0400

    DO NOT MERGE (yet): Trying to fix NetVirtIT
    Change-Id: I60946d641f5c51175b1e8c36f62a8681cfb04dbc
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- integrationtest/src/test/java/org/opendaylight/ovsdb/integrationtest/netvirt/NetVirtIT.java
+++ integrationtest/src/test/java/org/opendaylight/ovsdb/integrationtest/netvirt/NetVirtIT.java
@@ -6,32 +6,51 @@ import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.features;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.logLevel;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.useOwnExamBundlesStartLevel;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
+
 import javax.inject.Inject;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opendaylight.ovsdb.integrationtest.OvsdbIntegrationTestBase;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.ProbeBuilder;
+import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.Constants;
 import org.osgi.framework.InvalidSyntaxException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 @RunWith(PaxExam.class)
+@ExamReactorStrategy(PerMethod.class)
 public class NetVirtIT extends OvsdbIntegrationTestBase {
     private Logger LOG = LoggerFactory.getLogger(NetVirtIT.class);
     private static final String NETVIRT = "org.opendaylight.ovsdb.openstack.net-virt-providers";
 
     @Inject
     private BundleContext bc;
+
+    @ProbeBuilder
+    public TestProbeBuilder probeConfiguration(TestProbeBuilder probe) {
+        probe.setHeader(
+            Constants.IMPORT_PACKAGE,
+                "*,org.apache.felix.service.*;status=provisional");
+        return probe;
+    }
 
     @Configuration
     public Option[] config() {
@@ -63,7 +82,9 @@ public class NetVirtIT extends OvsdbIntegrationTestBase {
                 features("mvn:org.opendaylight.neutron/features-neutron/0.5.0-SNAPSHOT/xml/features",
                         "odl-neutron-service"),
                 features("mvn:org.opendaylight.ovsdb/features-ovsdb/1.1.0-SNAPSHOT/xml/features",
-                        "odl-ovsdb-openstack")
+                        "odl-ovsdb-openstack"),
+
+                useOwnExamBundlesStartLevel(4)
         };
     }
 
@@ -73,11 +94,11 @@ public class NetVirtIT extends OvsdbIntegrationTestBase {
 
         // TODO: this is where we should connect, but it fails currently because of
         // dependency issues.
-        //try {
-        //    node = getPluginTestConnection();
-        //} catch (Exception e) {
-        //    fail("Exception : " + e.getMessage());
-        //}
+        try {
+           Node node = getPluginTestConnection();
+        } catch (Exception e) {
+            Assert.fail("Exception : " + e.getMessage());
+        }
         isBundleReady(bc, NETVIRT);
         // To be certain that all the bundles are Active sleep some more.
         Thread.sleep(5000);

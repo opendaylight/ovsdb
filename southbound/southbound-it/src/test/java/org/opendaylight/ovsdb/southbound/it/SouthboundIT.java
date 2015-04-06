3bb59b36f14874ab2ec4e0e6573e86882a3b2ba3
Sam Hague <shague@redhat.com>
2015-04-06 05:36:18 -0400

    Add copyright's to southbound-it files.
    Change-Id: I0e0353233b068873bd1ba983eb11c7efde83bc1d
    Signed-off-by: Sam Hague <shague@redhat.com>

--- southbound/southbound-it/src/test/java/org/opendaylight/ovsdb/southbound/it/SouthboundIT.java
+++ southbound/southbound-it/src/test/java/org/opendaylight/ovsdb/southbound/it/SouthboundIT.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package org.opendaylight.ovsdb.southbound.it;
 
 import static org.junit.Assert.assertFalse;
@@ -42,6 +49,12 @@ import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Integration test for
+ * {@link org.opendaylight.ovsdb.southbound}
+ *
+ * @author Sam Hague (shague@redhat.com)
+ */
 @RunWith(PaxExam.class)
 @ExamReactorStrategy(PerClass.class)
 public class SouthboundIT extends AbstractMdsalTestBase {

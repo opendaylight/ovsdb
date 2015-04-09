8b74e0403dd438cd9904ab0cd0762c994a2ca54b
Ed Warnicke <eaw@cisco.com>
2015-04-08 18:15:59 -0700

    Simplify with utility functions.
    Change-Id: If9a86e4c18dc5723826054b1a3b50f7ebc0d5086
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/OpenVSwitchBridgeAddCommand.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/OpenVSwitchBridgeAddCommand.java
@@ -5,14 +5,12 @@ import static org.opendaylight.ovsdb.lib.operations.Operations.op;
 import java.util.List;
 
 import org.opendaylight.ovsdb.lib.notation.Mutator;
-import org.opendaylight.ovsdb.lib.notation.UUID;
 import org.opendaylight.ovsdb.lib.operations.Insert;
 import org.opendaylight.ovsdb.lib.operations.Operation;
 import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
 import org.opendaylight.ovsdb.lib.schema.typed.TyperUtils;
 import org.opendaylight.ovsdb.schema.openvswitch.Bridge;
 import org.opendaylight.ovsdb.schema.openvswitch.OpenVSwitch;
-import org.opendaylight.ovsdb.southbound.SouthboundMapper;
 
 import com.google.common.collect.Sets;
 
@@ -22,22 +20,13 @@ public class OpenVSwitchBridgeAddCommand implements TransactCommand {
     public void execute(TransactionBuilder transaction) {
         List<Operation> operations = transaction.getOperations();
         Bridge bridge = TyperUtils.getTypedRowWrapper(transaction.getDatabaseSchema(), Bridge.class);
-        for (int count = 0;count < operations.size();count++) {
-            Operation operation = operations.get(count);
-            if (operation instanceof Insert && operation.getTableSchema().equals(bridge.getSchema())) {
-                Insert insert = (Insert)operation;
-                String uuidString = insert.getUuidName() != null
-                        ? insert.getUuidName() : SouthboundMapper.getRandomUUID();
-                insert.setUuidName(uuidString);
-
-                // OpenVSwitchPart
-                UUID uuid = new UUID(uuidString);
-                OpenVSwitch ovs = TyperUtils.getTypedRowWrapper(transaction.getDatabaseSchema(), OpenVSwitch.class);
-                ovs.setBridges(Sets.newHashSet(uuid));
-                transaction.add(op.mutate(ovs).addMutation(ovs.getBridgesColumn().getSchema(),
-                        Mutator.INSERT,
-                        ovs.getBridgesColumn().getData()));
-            }
+        List<Insert> inserts = TransactUtils.extractInsert(transaction, bridge.getSchema());
+        for (Insert insert:inserts) {
+            OpenVSwitch ovs = TyperUtils.getTypedRowWrapper(transaction.getDatabaseSchema(), OpenVSwitch.class);
+            ovs.setBridges(Sets.newHashSet(TransactUtils.setOrExtractUUID(insert)));
+            transaction.add(op.mutate(ovs).addMutation(ovs.getBridgesColumn().getSchema(),
+                    Mutator.INSERT,
+                    ovs.getBridgesColumn().getData()));
         }
     }
 }

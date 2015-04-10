034044e3e9a035175c9315350e3ee313cffcf262
Marcus Koontz <marcus.g.koontz@intel.com>
2015-04-10 04:51:07 -0700

    First Draft Overlay Topology - DO NOT MERGE
    -Updated Licenses
    -Added OverlayProvider to SouthboundImplModule
    
    This is a work in progress. Please comment and critique.
    
    Change-Id: Ic2454947b34d74c46ff881b4c64bdf0b625265f0
    Signed-off-by: Marcus Koontz <marcus.g.koontz@intel.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/southbound/impl/rev141210/SouthboundImplModule.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/southbound/impl/rev141210/SouthboundImplModule.java
@@ -7,6 +7,7 @@
  */
 package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.southbound.impl.rev141210;
 
+import org.opendaylight.overlay.OverlayProvider;
 import org.opendaylight.ovsdb.southbound.InstanceIdentifierCodec;
 import org.opendaylight.ovsdb.southbound.SouthboundProvider;
 import org.opendaylight.ovsdb.southbound.SouthboundUtil;
@@ -31,9 +32,27 @@ public class SouthboundImplModule extends org.opendaylight.yang.gen.v1.urn.opend
     public java.lang.AutoCloseable createInstance() {
         SouthboundUtil.setInstanceIdentifierCodec(new InstanceIdentifierCodec(getSchemaServiceDependency(),
                 getBindingNormalizedNodeSerializerDependency()));
-        SouthboundProvider provider = new SouthboundProvider();
-        getBrokerDependency().registerProvider(provider);
-        return provider;
+        SouthboundProvider southboundProvider = new SouthboundProvider();
+        OverlayProvider overlayProvider = new OverlayProvider();
+        getBrokerDependency().registerProvider(southboundProvider);
+        getBrokerDependency().registerProvider(overlayProvider);
+
+        class ProviderAggregator implements java.lang.AutoCloseable {
+            public SouthboundProvider southboundProvider;
+            public OverlayProvider overlayProvider;
+
+            public ProviderAggregator(OverlayProvider overlayProvider, SouthboundProvider southboundProvider) {
+                this.southboundProvider = southboundProvider;
+                this.overlayProvider = overlayProvider;
+            }
+
+            @Override
+            public void close() throws Exception {
+                this.overlayProvider.close();
+                this.southboundProvider.close();
+            }
+        }
+        return new ProviderAggregator(overlayProvider, southboundProvider);
     }
 
 }

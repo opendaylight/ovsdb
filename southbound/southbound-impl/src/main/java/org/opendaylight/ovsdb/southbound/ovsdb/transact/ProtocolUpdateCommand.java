73349d750d89141ad6a818a44da643905d15569c
Ed Warnicke <eaw@cisco.com>
2015-04-11 19:29:32 -0700

    Break out Protocol Update and Remove into seperate commands
    In addition, they now have the necessary logic to handle
    Updates as well as Creates.
    
    Change-Id: Id19e51a7e8951123d3b08c510a61cbef84328c9d
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/ProtocolUpdateCommand.java
@@ -0,0 +1,70 @@
+package org.opendaylight.ovsdb.southbound.ovsdb.transact;
+
+import static org.opendaylight.ovsdb.lib.operations.Operations.op;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.ovsdb.lib.notation.Mutator;
+import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
+import org.opendaylight.ovsdb.lib.schema.typed.TyperUtils;
+import org.opendaylight.ovsdb.schema.openvswitch.Bridge;
+import org.opendaylight.ovsdb.southbound.SouthboundConstants;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.bridge.attributes.ProtocolEntry;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import com.google.common.base.Optional;
+import com.google.common.collect.Sets;
+
+public class ProtocolUpdateCommand extends AbstractTransactCommand {
+
+    private Map<InstanceIdentifier<ProtocolEntry>, ProtocolEntry> protocols;
+    private Map<InstanceIdentifier<OvsdbBridgeAugmentation>, OvsdbBridgeAugmentation> bridges;
+
+    public ProtocolUpdateCommand(BridgeOperationalState state,
+            AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
+        super(state, changes);
+        protocols = TransactUtils.extractCreatedOrUpdated(getChanges(), ProtocolEntry.class);
+        bridges = TransactUtils.extractCreatedOrUpdated(getChanges(), OvsdbBridgeAugmentation.class);
+    }
+
+    @Override
+    public void execute(TransactionBuilder transaction) {
+        for (Entry<InstanceIdentifier<ProtocolEntry>, ProtocolEntry> entry: protocols.entrySet()) {
+            Optional<ProtocolEntry> operationalProtocolEntryOptional =
+                    getOperationalState().getProtocolEntry(entry.getKey());
+            if (!operationalProtocolEntryOptional.isPresent()) {
+                InstanceIdentifier<OvsdbBridgeAugmentation> bridgeIid =
+                        entry.getKey().firstIdentifierOf(OvsdbBridgeAugmentation.class);
+                Optional<OvsdbBridgeAugmentation> bridgeOptional =
+                        getOperationalState().getOvsdbBridgeAugmentation(bridgeIid);
+                OvsdbBridgeAugmentation ovsdbBridge = null;
+                if (bridgeOptional.isPresent()) {
+                    ovsdbBridge = bridgeOptional.get();
+                } else {
+                    ovsdbBridge = bridges.get(bridgeIid);
+                }
+                if (ovsdbBridge != null
+                        && ovsdbBridge.getBridgeName() != null
+                        && entry.getValue() != null
+                        && entry.getValue().getProtocol() != null) {
+                    String protocolString = SouthboundConstants.OVSDB_PROTOCOL_MAP.get(entry.getValue().getProtocol());
+                    if (protocolString != null) {
+                        Bridge bridge = TyperUtils.getTypedRowWrapper(transaction.getDatabaseSchema(), Bridge.class);
+                        bridge.setName(ovsdbBridge.getBridgeName().getValue());
+                        bridge.setProtocols(Sets.newHashSet(protocolString));
+                        transaction.add(op.mutate(bridge)
+                                .addMutation(bridge.getProtocolsColumn().getSchema(),
+                                        Mutator.INSERT,bridge.getProtocolsColumn().getData())
+                                .where(bridge.getNameColumn().getSchema().opEqual(bridge.getNameColumn().getData()))
+                                .build());
+                    }
+                }
+            }
+        }
+    }
+
+}

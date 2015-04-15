8ba8086dd6fa50337341f6b29032abce2a960e7e
Sam Hague <shague@redhat.com>
2015-04-15 13:59:22 -0400

    Add constant class for shared strings in SouthboundIT
    patchset 3: fix checkstyle error
    
    Change-Id: If0c4441848ccf7e002f1bf43a6186465431c7d8a
    Signed-off-by: Sam Hague <shague@redhat.com>

--- southbound/southbound-it/src/test/java/org/opendaylight/ovsdb/southbound/it/SouthboundIT.java
+++ southbound/southbound-it/src/test/java/org/opendaylight/ovsdb/southbound/it/SouthboundIT.java
@@ -16,6 +16,7 @@ import static org.ops4j.pax.exam.CoreOptions.maven;
 import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.ObjectArrays;
 import com.google.common.util.concurrent.CheckedFuture;
 import com.google.common.util.concurrent.FutureCallback;
 import com.google.common.util.concurrent.Futures;
@@ -53,6 +54,7 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption;
 import org.ops4j.pax.exam.options.MavenUrlReference;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
@@ -69,14 +71,6 @@ import org.slf4j.LoggerFactory;
 @ExamReactorStrategy(PerClass.class)
 public class SouthboundIT extends AbstractMdsalTestBase {
     private static final Logger LOG = LoggerFactory.getLogger(SouthboundIT.class);
-    private static final String SERVER_IPADDRESS = "ovsdbserver.ipaddress";
-    private static final String SERVER_PORT = "ovsdbserver.port";
-    private static final String CONNECTION_TYPE = "ovsdbserver.connection";
-    private static final String CONNECTION_TYPE_ACTIVE = "active";
-    private static final String CONNECTION_TYPE_PASSIVE = "passive";
-    private static final int CONNECTION_INIT_TIMEOUT = 10000;
-    private static final String DEFAULT_SERVER_IPADDRESS = "127.0.0.1";
-    private static final String DEFAULT_SERVER_PORT = "6640";
     private static Boolean writeStatus = false;
     private static Boolean readStatus = false;
     private static Boolean deleteStatus = false;
@@ -127,19 +121,36 @@ public class SouthboundIT extends AbstractMdsalTestBase {
     }
 
     @Override
+    public Option[] getLoggingOptions() {
+        Option[] options = new Option[] {
+                editConfigurationFilePut(SouthboundITConstants.ORG_OPS4J_PAX_LOGGING_CFG,
+                        "log4j.logger.org.opendaylight.ovsdb.southbound-impl",
+                        LogLevelOption.LogLevel.DEBUG.name())
+        };
+        options = ObjectArrays.concat(options, super.getLoggingOptions(), Option.class);
+        return options;
+    }
+
+    @Override
     public Option[] getPropertiesOptions() {
         Properties props = new Properties(System.getProperties());
-        String addressStr = props.getProperty(SERVER_IPADDRESS, DEFAULT_SERVER_IPADDRESS);
-        String portStr = props.getProperty(SERVER_PORT, DEFAULT_SERVER_PORT);
-        String connectionType = props.getProperty(CONNECTION_TYPE, CONNECTION_TYPE_ACTIVE);
+        String addressStr = props.getProperty(SouthboundITConstants.SERVER_IPADDRESS,
+                SouthboundITConstants.DEFAULT_SERVER_IPADDRESS);
+        String portStr = props.getProperty(SouthboundITConstants.SERVER_PORT,
+                SouthboundITConstants.DEFAULT_SERVER_PORT);
+        String connectionType = props.getProperty(SouthboundITConstants.CONNECTION_TYPE,
+                SouthboundITConstants.CONNECTION_TYPE_ACTIVE);
 
         LOG.info("Using the following properties: mode= {}, ip:port= {}:{}",
                 connectionType, addressStr, portStr);
 
         Option[] options = new Option[] {
-                editConfigurationFilePut(CUSTOM_PROPERTIES, SERVER_IPADDRESS, addressStr),
-                editConfigurationFilePut(CUSTOM_PROPERTIES, SERVER_PORT, portStr),
-                editConfigurationFilePut(CUSTOM_PROPERTIES, CONNECTION_TYPE, connectionType)
+                editConfigurationFilePut(SouthboundITConstants.CUSTOM_PROPERTIES,
+                        SouthboundITConstants.SERVER_IPADDRESS, addressStr),
+                editConfigurationFilePut(SouthboundITConstants.CUSTOM_PROPERTIES,
+                        SouthboundITConstants.SERVER_PORT, portStr),
+                editConfigurationFilePut(SouthboundITConstants.CUSTOM_PROPERTIES,
+                        SouthboundITConstants.CONNECTION_TYPE, connectionType)
         };
         return options;
     }
@@ -161,13 +172,13 @@ public class SouthboundIT extends AbstractMdsalTestBase {
         dataBroker = SouthboundProvider.getDb();
         Assert.assertNotNull("db should not be null", dataBroker);
 
-        addressStr = bc.getProperty(SERVER_IPADDRESS);
-        portStr = bc.getProperty(SERVER_PORT);
-        connectionType = bc.getProperty(CONNECTION_TYPE);
+        addressStr = bc.getProperty(SouthboundITConstants.SERVER_IPADDRESS);
+        portStr = bc.getProperty(SouthboundITConstants.SERVER_PORT);
+        connectionType = bc.getProperty(SouthboundITConstants.CONNECTION_TYPE);
 
         LOG.info("Using the following properties: mode= {}, ip:port= {}:{}",
                 connectionType, addressStr, portStr);
-        if (connectionType.equalsIgnoreCase(CONNECTION_TYPE_ACTIVE)) {
+        if (connectionType.equalsIgnoreCase(SouthboundITConstants.CONNECTION_TYPE_ACTIVE)) {
             if (addressStr == null) {
                 fail(usage());
             }
@@ -179,9 +190,9 @@ public class SouthboundIT extends AbstractMdsalTestBase {
 
     @Test
     public void testPassiveNode() throws InterruptedException {
-        if (connectionType.equalsIgnoreCase(CONNECTION_TYPE_PASSIVE)) {
+        if (connectionType.equalsIgnoreCase(SouthboundITConstants.CONNECTION_TYPE_PASSIVE)) {
             //Wait for CONNECTION_INIT_TIMEOUT for the Passive connection to be initiated by the ovsdb-server.
-            Thread.sleep(CONNECTION_INIT_TIMEOUT);
+            Thread.sleep(SouthboundITConstants.CONNECTION_INIT_TIMEOUT);
         }
     }
 

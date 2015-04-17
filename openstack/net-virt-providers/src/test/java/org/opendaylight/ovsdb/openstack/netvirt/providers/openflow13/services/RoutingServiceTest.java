79c9514af7021d4c65341115f1938d2bcde5b1da
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-04-17 13:32:10 -0400

    Add JUnit testing for RoutingService class.
    Change-Id: Ie40d1009a840f646260c676b5338088d926b73bc
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ openstack/net-virt-providers/src/test/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/services/RoutingServiceTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2015 Inocybe and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13.services;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyBoolean;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.net.InetAddress;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.controller.md.sal.binding.api.WriteTransaction;
+import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
+import org.opendaylight.controller.md.sal.common.api.data.TransactionCommitFailedException;
+import org.opendaylight.neutron.spi.NeutronSecurityGroup;
+import org.opendaylight.neutron.spi.NeutronSecurityRule;
+import org.opendaylight.ovsdb.openstack.netvirt.api.Action;
+import org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13.MdsalConsumer;
+import org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13.PipelineOrchestrator;
+import org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13.Service;
+import org.opendaylight.ovsdb.plugin.api.Status;
+import org.opendaylight.ovsdb.plugin.api.StatusCode;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.inventory.rev130819.nodes.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+import com.google.common.util.concurrent.CheckedFuture;
+
+/**
+ * Unit test fort {@link RoutingService}
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class RoutingServiceTest {
+
+    @InjectMocks RoutingService routingService = new RoutingService();
+
+    @Mock private MdsalConsumer mdsalConsumer;
+    @Mock private PipelineOrchestrator orchestrator;
+
+    @Mock private ReadWriteTransaction readWriteTransaction;
+    @Mock private WriteTransaction writeTransaction;
+    @Mock private CheckedFuture<Void, TransactionCommitFailedException> commitFuture;
+
+    private static final String SEGMENTATION_ID = "2";
+    private static final String HOST_ADDRESS = "127.0.0.1";
+    private static final String MAC_ADDRESS = "87:1D:5E:02:40:B8";
+
+    @Before
+    public void setUp() throws Exception {
+        when(readWriteTransaction.submit()).thenReturn(commitFuture);
+        when(writeTransaction.submit()).thenReturn(commitFuture);
+
+        DataBroker dataBroker = mock(DataBroker.class);
+        when(dataBroker.newReadWriteTransaction()).thenReturn(readWriteTransaction);
+        when(dataBroker.newWriteOnlyTransaction()).thenReturn(writeTransaction);
+
+        when(mdsalConsumer.getDataBroker()).thenReturn(dataBroker);
+
+        when(orchestrator.getNextServiceInPipeline(any(Service.class))).thenReturn(Service.ARP_RESPONDER);
+    }
+
+    /**
+     * Test method {@link RoutingService#programRouterInterface(Node, Long, String, String, String, InetAddress, int, Action)}
+     */
+    @Test
+    public void testProgramRouterInterface() throws Exception {
+        InetAddress address = mock(InetAddress.class);
+        when(address.getHostAddress()).thenReturn(HOST_ADDRESS);
+
+        assertEquals("Error, did not return the expected StatusCode", new Status(StatusCode.SUCCESS), routingService.programRouterInterface(mock(Node.class), Long.valueOf(123), SEGMENTATION_ID, SEGMENTATION_ID, MAC_ADDRESS, address, 1, Action.ADD));
+        verify(readWriteTransaction, times(2)).put(any(LogicalDatastoreType.class), any(InstanceIdentifier.class), any(Node.class), anyBoolean());
+        verify(readWriteTransaction, times(1)).submit();
+        verify(commitFuture, times(1)).get();
+
+        assertEquals("Error, did not return the expected StatusCode", new Status(StatusCode.SUCCESS), routingService.programRouterInterface(mock(Node.class), Long.valueOf(123), SEGMENTATION_ID, SEGMENTATION_ID, MAC_ADDRESS, address, 1, Action.DELETE));
+        verify(writeTransaction, times(1)).delete(any(LogicalDatastoreType.class), any(InstanceIdentifier.class));
+        verify(readWriteTransaction, times(1)).submit();
+        verify(commitFuture, times(2)).get(); // 1 + 1 above
+    }
+
+    /**
+     * Test method {@link RoutingService#programDefaultRouteEntry(Node, Long, String, String, InetAddress, Action)}
+     */
+    @Test
+    public void testProgramDefaultRouteEntry() throws Exception {
+        InetAddress address = mock(InetAddress.class);
+        when(address.getHostAddress()).thenReturn(HOST_ADDRESS);
+
+        assertEquals("Error, did not return the expected StatusCode", new Status(StatusCode.SUCCESS), routingService.programDefaultRouteEntry(mock(Node.class), Long.valueOf(123), SEGMENTATION_ID, MAC_ADDRESS, address, Action.ADD));
+        verify(readWriteTransaction, times(2)).put(any(LogicalDatastoreType.class), any(InstanceIdentifier.class), any(Node.class), anyBoolean());
+        verify(readWriteTransaction, times(1)).submit();
+        verify(commitFuture, times(1)).get();
+
+        assertEquals("Error, did not return the expected StatusCode", new Status(StatusCode.SUCCESS), routingService.programDefaultRouteEntry(mock(Node.class), Long.valueOf(123), SEGMENTATION_ID, MAC_ADDRESS, address, Action.DELETE));
+        verify(writeTransaction, times(1)).delete(any(LogicalDatastoreType.class), any(InstanceIdentifier.class));
+        verify(readWriteTransaction, times(1)).submit();
+        verify(commitFuture, times(2)).get(); // 1 + 1 above
+    }
+
+}

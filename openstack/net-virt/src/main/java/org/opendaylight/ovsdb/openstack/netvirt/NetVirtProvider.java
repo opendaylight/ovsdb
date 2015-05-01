8fff4e5c0934f3838a57860bbd28c644b522d623
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-05-01 17:42:42 -0400

    DO NOT MERGE YET: Initial Model for Config Subystem (WIP)
    Change-Id: I3e59e909145d764ffff059d00d4ce64d63156b17
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/NetVirtProvider.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.openstack.netvirt;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NetVirtProvider implements BindingAwareProvider, AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(NetVirtProvider.class);
+
+    private BundleContext bundleContext;
+
+    private ActivatorV2 activatorV2;
+
+    public NetVirtProvider(BundleContext bundleContext) {
+        LOG.warn("BundleContext set to: {}",bundleContext);
+        this.bundleContext = bundleContext;
+    }
+
+    @Override
+    public void onSessionInitiated(ProviderContext arg0) {
+        LOG.info("NetVirtProvider Session Initiated");
+        this.activatorV2 = new ActivatorV2();
+        try {
+            LOG.warn("BundleContext found to be: {}",bundleContext);
+            this.activatorV2.start(bundleContext);
+        } catch (Exception e) {
+            LOG.warn("Unable to start NetVirt because: ",e);
+        }
+    }
+
+
+    @Override
+    public void close() throws Exception {
+        this.activatorV2.stop(bundleContext);
+    }
+}

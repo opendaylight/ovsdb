9de9446dc413ad27c806b39f8f87521633ee1187
Prateek Garg <prategar@cisco.com>
2015-05-02 14:19:35 -0700

    Adding the capability to use system-id from external ids of open_vswitch table in case of tcp connection from ovs. This is required as connection port changes after ovs reconnects to controller. This change makes sure that the iid of ovs is always consistent in case of tcp connection of ovs. 1. Save the iid for ovsdb node in external ids when ptcp connection is established. This is similar to bridge creation flow. 2. Create the iid for ovsdb node at one place (in OvsdbConnectionInstance) and provide it to all required commands. 3. iid of ovsdb is created based on external ids of ovs. There would not be any change in ptcp use cases as in step 1, the iid was saved in external ids which would be used if present. 4. Pass the createMissingParents attribute for transaction merge/put on operational store as the exception: Metadata not available for modification NodeModification was thrown for multiple cases.
    Change-Id: Iaa84f2a43367fbc1537fd1ff4e33df0a1a548650
    Signed-off-by: Prateek Garg <prategar@cisco.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionInstance.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionInstance.java
@@ -38,6 +38,8 @@ import org.opendaylight.ovsdb.southbound.ovsdb.transact.TransactInvokerImpl;
 import org.opendaylight.ovsdb.southbound.transactions.md.OvsdbNodeCreateCommand;
 import org.opendaylight.ovsdb.southbound.transactions.md.TransactionInvoker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ConnectionInfo;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,13 +53,16 @@ public class OvsdbConnectionInstance implements OvsdbClient {
     private TransactionInvoker txInvoker;
     private Map<DatabaseSchema,TransactInvoker> transactInvokers = new HashMap<DatabaseSchema,TransactInvoker>();
     private MonitorCallBack callback;
+    private InstanceIdentifier<Node> connectionIid;
 
     OvsdbConnectionInstance(ConnectionInfo key,OvsdbClient client,TransactionInvoker txInvoker) {
         this.connectionInfo = key;
         this.client = client;
         this.txInvoker = txInvoker;
+        this.connectionIid = SouthboundMapper.createInstanceIdentifier(
+                SouthboundUtil.getOpenVswitchExternalIds(this.client),key);
         registerCallBack();
-        txInvoker.invoke(new OvsdbNodeCreateCommand(key, null,null));
+        txInvoker.invoke(new OvsdbNodeCreateCommand(key, null,null,connectionIid));
     }
 
     public void transact(TransactCommand command) {
@@ -67,7 +72,7 @@ public class OvsdbConnectionInstance implements OvsdbClient {
     }
 
     private void registerCallBack() {
-        this.callback = new OvsdbMonitorCallback(connectionInfo,txInvoker);
+        this.callback = new OvsdbMonitorCallback(connectionInfo,txInvoker,connectionIid);
         try {
             List<String> databases = getDatabases().get();
             if (databases != null) {
@@ -191,5 +196,9 @@ public class OvsdbConnectionInstance implements OvsdbClient {
 
     public void setMDConnectionInfo(ConnectionInfo key) {
         this.connectionInfo = key;
+    }
+
+    public InstanceIdentifier<Node> getConnectionIid() {
+        return connectionIid;
     }
 }
--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbMonitorCallback.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbMonitorCallback.java
@@ -13,6 +13,8 @@ import org.opendaylight.ovsdb.lib.schema.DatabaseSchema;
 import org.opendaylight.ovsdb.southbound.transactions.md.OvsdbOperationalCommandAggregator;
 import org.opendaylight.ovsdb.southbound.transactions.md.TransactionInvoker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ConnectionInfo;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,16 +23,18 @@ public class OvsdbMonitorCallback implements MonitorCallBack {
     private static final Logger LOG = LoggerFactory.getLogger(OvsdbMonitorCallback.class);
     private TransactionInvoker txInvoker;
     private ConnectionInfo key;
+    InstanceIdentifier<Node> connectionIid;
 
-    OvsdbMonitorCallback(ConnectionInfo key,TransactionInvoker txInvoker) {
+    OvsdbMonitorCallback(ConnectionInfo key,TransactionInvoker txInvoker,InstanceIdentifier<Node> connectionIid) {
         this.txInvoker = txInvoker;
         this.key = key;
+        this.connectionIid = connectionIid;
     }
 
     @Override
     public void update(TableUpdates result, DatabaseSchema dbSchema) {
         LOG.debug("result: {} dbSchema: {}",result,dbSchema);
-        txInvoker.invoke(new OvsdbOperationalCommandAggregator(key, result, dbSchema));
+        txInvoker.invoke(new OvsdbOperationalCommandAggregator(key, result, dbSchema, connectionIid));
     }
 
     @Override

30b6fa3eb80b149a088e003585b604b7c369cc8b
Prateek Garg <prategar@cisco.com>
2015-05-02 14:19:35 -0700

    Adding the capability to use system-id from external ids of open_vswitch table in case of tcp connection from ovs. This is required as connection port changes after ovs reconnects to controller. This change makes sure that the iid of ovs is always consistent in case of tcp connection of ovs. 1. Save the iid for ovsdb node in external ids when ptcp connection is established. This is similar to bridge creation flow. 2. Create the iid for ovsdb node at one place (in OvsdbConnectionInstance) and provide it to all required commands. 3. iid of ovsdb is created based on external ids of ovs. There would not be any change in ptcp use cases as in step 1, the iid was saved in external ids which would be used if present. 4. Pass the createMissingParents attribute for transaction merge/put on operational store as the exception: Metadata not available for modification NodeModification was thrown for multiple cases.
    Change-Id: Iaa84f2a43367fbc1537fd1ff4e33df0a1a548650
    Signed-off-by: Prateek Garg <prategar@cisco.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/AbstractTransactionCommand.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/AbstractTransactionCommand.java
@@ -3,12 +3,15 @@ package org.opendaylight.ovsdb.southbound.transactions.md;
 import org.opendaylight.ovsdb.lib.message.TableUpdates;
 import org.opendaylight.ovsdb.lib.schema.DatabaseSchema;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ConnectionInfo;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
 public abstract class AbstractTransactionCommand implements TransactionCommand {
 
     private TableUpdates updates;
     private DatabaseSchema dbSchema;
     private ConnectionInfo key;
+    private InstanceIdentifier<Node> connectionIid;
 
     public TableUpdates getUpdates() {
         return updates;
@@ -22,14 +25,20 @@ public abstract class AbstractTransactionCommand implements TransactionCommand {
         return key;
     }
 
+    public InstanceIdentifier<Node> getConnectionIid() {
+        return connectionIid;
+    }
+
     protected AbstractTransactionCommand() {
         // NO OP
     }
 
-    public AbstractTransactionCommand(ConnectionInfo key,TableUpdates updates, DatabaseSchema dbSchema) {
+    public AbstractTransactionCommand(ConnectionInfo key,TableUpdates updates, DatabaseSchema dbSchema,
+            InstanceIdentifier<Node> connectionIid) {
         this.updates = updates;
         this.dbSchema = dbSchema;
         this.key = key;
+        this.connectionIid = connectionIid;
     }
 
 }

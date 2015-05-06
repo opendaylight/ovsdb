ca345931de73d97a2fd86226185d102811cf098c
Anil Vishnoi <vishnoianil@gmail.com>
2015-05-06 18:42:03 +0530

    OvsdbDataChangeListener : Implemented port (termination point) delete event processing
    Change-Id: I6cd3ef89927aaae829815b2a9c625b6f89907162
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/SouthboundHandler.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/SouthboundHandler.java
@@ -18,6 +18,7 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.re
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -74,7 +75,7 @@ public class SouthboundHandler extends AbstractHandler
     }
 
     @Override
-    public void ovsdbUpdate(Node node, OvsdbType ovsdbType, Action action) {
+    public void ovsdbUpdate(Node node, DataObject resourceAugmentationData, OvsdbType ovsdbType, Action action) {
         logger.info("ovsdbUpdate: {} - {} - {}", node, ovsdbType, action);
         this.enqueueEvent(new SouthboundEvent(node, ovsdbTypeToSouthboundEventType(ovsdbType), action));
     }

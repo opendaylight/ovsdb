0c2bc233237a49a0b64ee0dff16ecc99b5ac1325
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2015-05-13 15:29:03 -0400

    Skeleton for the ovsdb-ui feature (DO NOT MERGE) Folders structure and basic files to create and add a external module on DLUX. It also include a CRUD implementation of the southbounds REST API.
    Change-Id: If48eb4d055db3b91529e76e75503aa4c68b0c6a3
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ ovsdb-ui/src/main/resources/ovsdb-ui/ovsdb.controller.js
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2015 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+define(['jquery', 'ovsdb-ui/ovsdb.module','ovsdb-ui/ovsdb.services'], function($, ovsdb) {
+  'use strict';
+
+  var RootOvsdbCtrl = function($rootScope) {
+      $rootScope['section_logo'] = 'logo_ovsdb';
+  };
+  RootOvsdbCtrl.$inject = ['$rootScope'];
+
+  var BaseOvsdbCtrl = function($scope) {
+      $scope['err'] = {
+        "message": "",
+        "tag": "",
+        "type": ""
+      };
+
+      $scope.showError = function() {
+        $('#errorMessage').fadeIn().delay(3000).fadeOut();
+      };
+  };
+  BaseOvsdbCtrl.$inject = ['$scope'];
+
+  var OvsdbCtrl = function($scope, TopologyNetworkSvc) {
+      BaseOvsdbCtrl.call(this, $scope);
+
+  };
+  OvsdbCtrl.$inject = ['$scope', 'TopologyNetworkSvc'];
+  OvsdbCtrl.prototype = Object.create(BaseOvsdbCtrl.prototype);
+
+  ovsdb.register.controller('RootOvsdbCtrl', RootOvsdbCtrl);
+  ovsdb.register.controller('OvsdbCtrl', OvsdbCtrl);
+});

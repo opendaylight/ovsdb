550db3d22d1ecc7aba5e5feec46b65ad5baebfb3
Flavio Fernandes <ffernand@redhat.com>
2015-05-26 21:17:32 +0000

    Revert "Added controller is-connected code"
    This reverts commit dcc846b12b09e8b998b4ee361617dbf54f65dafa.
    
    To be re-visited, once regression is resolved.
    See bug: https://bugs.opendaylight.org/show_bug.cgi?id=3379
    Ref: https://git.opendaylight.org/gerrit/#/c/18766/
    
    Change-Id: I9fa30c70ab988f5e7f14d4e5ff31c945eda1ad69
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbControllerUpdateCommand.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbControllerUpdateCommand.java
@@ -7,13 +7,10 @@
  */
 package org.opendaylight.ovsdb.southbound.transactions.md;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.controller.md.sal.common.api.data.ReadFailedException;
 import org.opendaylight.ovsdb.lib.message.TableUpdates;
 import org.opendaylight.ovsdb.lib.notation.UUID;
 import org.opendaylight.ovsdb.lib.schema.DatabaseSchema;
@@ -21,25 +18,15 @@ import org.opendaylight.ovsdb.lib.schema.typed.TyperUtils;
 import org.opendaylight.ovsdb.schema.openvswitch.Bridge;
 import org.opendaylight.ovsdb.schema.openvswitch.Controller;
 import org.opendaylight.ovsdb.southbound.SouthboundMapper;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
-import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeName;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.bridge.attributes.ControllerEntry;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.bridge.attributes.ControllerEntryBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.bridge.attributes.ControllerEntryKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ConnectionInfo;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ManagedNodeEntry;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Optional;
 
 public class OvsdbControllerUpdateCommand extends AbstractTransactionCommand {
-    private static final Logger LOG = LoggerFactory.getLogger(OvsdbControllerUpdateCommand.class);
+
+
+
     private Map<UUID, Controller> updatedControllerRows;
     private Map<UUID, Bridge> updatedBridgeRows;
 
@@ -53,18 +40,10 @@ public class OvsdbControllerUpdateCommand extends AbstractTransactionCommand {
 
     @Override
     public void execute(ReadWriteTransaction transaction) {
-        final InstanceIdentifier<Node> connectionIId = SouthboundMapper.createInstanceIdentifier(getConnectionInfo());
-        if ( (updatedBridgeRows == null && updatedControllerRows == null )
-                || ( updatedBridgeRows.isEmpty() && updatedControllerRows.isEmpty())) {
-            return;
-        }
-        Optional<Node> node = readNode(transaction, connectionIId);
-        if (node.isPresent()) {
-            updateController(transaction, node.get());
-        }
         for (Bridge bridge: updatedBridgeRows.values()) {
             setController(transaction, bridge);
         }
+
     }
 
     private void setController(ReadWriteTransaction transaction, Bridge bridge) {
@@ -77,68 +56,4 @@ public class OvsdbControllerUpdateCommand extends AbstractTransactionCommand {
         }
     }
 
-    private void updateController(ReadWriteTransaction transaction, Node node) {
-        for (Entry<UUID, Controller> controllerUpdate : updatedControllerRows.entrySet()) {
-            Controller controller = controllerUpdate.getValue();
-            Optional<String> bridgeName = getControllerBridge(controllerUpdate.getValue().getUuid());
-
-            if (!bridgeName.isPresent()) {
-                bridgeName = getControllerBridge( transaction, node, controllerUpdate.getValue());
-            }
-            if (bridgeName.isPresent()) {
-                ControllerEntryKey controlletEntryKey
-                    = new ControllerEntryKey(new Uri(controller.getTargetColumn().getData()));
-                ControllerEntry controllerEntry = new ControllerEntryBuilder()
-                        .setControllerUuid(new Uuid(controller.getUuid().toString()))
-                        .setIsConnected(controllerUpdate.getValue().getIsConnectedColumn().getData())
-                        .build();
-                InstanceIdentifier<ControllerEntry> controllerPath =
-                        SouthboundMapper.createInstanceIdentifier(
-                                getConnectionInfo(), new OvsdbBridgeName(bridgeName.get()))
-                        .augmentation(OvsdbBridgeAugmentation.class)
-                        .child(ControllerEntry.class,controlletEntryKey);
-                transaction.merge(LogicalDatastoreType.OPERATIONAL, controllerPath, controllerEntry);
-            }
-        }
-    }
-
-    private Optional<String> getControllerBridge(UUID controllerUUID) {
-        for (UUID bridgeUUID : updatedBridgeRows.keySet()) {
-            if (this.updatedBridgeRows.get(bridgeUUID).getControllerColumn().getData().contains(controllerUUID)) {
-                return Optional.of(this.updatedBridgeRows.get(bridgeUUID).getNameColumn().getData());
-            }
-        }
-        return Optional.absent();
-    }
-
-    private Optional<String> getControllerBridge(
-            final ReadWriteTransaction transaction, Node node, Controller controller) {
-        OvsdbNodeAugmentation ovsdbNode = node.getAugmentation(OvsdbNodeAugmentation.class);
-        List<ManagedNodeEntry> managedNodes = ovsdbNode.getManagedNodeEntry();
-        for ( ManagedNodeEntry managedNodeEntry : managedNodes ) {
-            @SuppressWarnings("unchecked")
-            Node managedNode = readNode(transaction
-                    ,(InstanceIdentifier<Node>)managedNodeEntry.getBridgeRef().getValue()).get();
-            ControllerEntryBuilder controllerBuidler = new ControllerEntryBuilder();
-            ControllerEntryKey controllerKey = new ControllerEntryKey(new Uri(controller.getTargetColumn().getData()));
-            controllerBuidler.setKey(controllerKey);
-            OvsdbBridgeAugmentation ovsdbNodeAugment
-                = managedNode.getAugmentation(OvsdbBridgeAugmentation.class);
-            return Optional.of(ovsdbNodeAugment.getBridgeName().getValue());
-        }
-        return Optional.absent();
-    }
-
-    private Optional<Node> readNode(final ReadWriteTransaction transaction, final InstanceIdentifier<Node> nodePath) {
-        Optional<Node> node = Optional.absent();
-        try {
-            node = transaction.read(
-                    LogicalDatastoreType.OPERATIONAL, nodePath)
-                    .checkedGet();
-        } catch (final ReadFailedException e) {
-            LOG.warn("Read Operational/DS for Node fail! {}",
-                    nodePath, e);
-        }
-        return node;
-    }
 }

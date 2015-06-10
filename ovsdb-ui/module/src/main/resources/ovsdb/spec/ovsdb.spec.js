3afc2ba2e053e8fc2dd2f87191aa938a21105cf1
Maxime Millette-Coulombe <mmcoulombe@inocybe.com>
2015-06-10 16:27:26 -0400

    Skeleton for the ovsdb-ui feature
    Folders structure and basic files to create and add a external
    module on DLUX. It also include a CRUD implementation of
    the southbounds REST API.
    
    Change-Id: If48eb4d055db3b91529e76e75503aa4c68b0c6a3
    Signed-off-by: Maxime Millette-Coulombe <mmcoulombe@inocybe.com>

---
+++ ovsdb-ui/module/src/main/resources/ovsdb/spec/ovsdb.spec.js
@@ -0,0 +1,7 @@
+define(['ovsdb/ovsdb.controller'], function() {
+  describe('A fake test', function() {
+    it('Should be retuning true', function() {
+      expect(true).toBe(true);
+    });
+  });
+});

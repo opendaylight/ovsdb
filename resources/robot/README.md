5006218d59c3bf8527d488ce31416d335ad46da4
Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
2015-06-11 23:34:13 -0400

    Added Vagrant images to run CSIT.
    Change-Id: I147cb90878f8f8af6d535b2b15988e9221777a4c
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>

---
+++ resources/robot/README.md
@@ -0,0 +1,4 @@
+# Purpose
+Spawn 2 VMs running CentOS 7.0 x64 with OpenVSwitch
+- OVS1
+- OVS2
---
+++ resources/robot/puppet/manifests/controller.pp
@@ -0,0 +1,19 @@
+  $base_packages = [
+    "kernel-headers",
+    "kernel-devel",
+    "gcc",
+    "make",
+    "python-devel",
+    "openssl-devel",
+    "graphviz",
+    "kernel-debug-devel",
+    "automake",
+    "rpm-build",
+    "redhat-rpm-config",
+    "libtool",
+    "git"
+  ]
+
+  package { $base_packages:
+    ensure => installed,
+  }

5f7d9b97e39ab107bf392710b1e8c083bef8e969
Ben Eze <beze@inocybe.ca>
2015-06-18 10:20:55 -0400

    Added Vagrant images to run CSIT.
    Change-Id: I147cb90878f8f8af6d535b2b15988e9221777a4c
    Signed-off-by: Gabriel Robitaille-Montpetit <grmontpetit@inocybe.com>
    Signed-off-by: Ben Eze <beze@inocybe.ca>

---
+++ resources/robot/puppet/manifests/ovsnode.pp
@@ -0,0 +1,39 @@
+  notice("specified ovs version to install: ${ovsversion}")
+
+  exec { "mktarget":
+    command => "/bin/mkdir -p /vagrant/shared/",
+    cwd     => "/",
+  }
+
+  file { [
+           "/vagrant/shared/",
+         ]:
+    ensure => directory,
+  }
+
+  exec { "download_ovs":
+    command => "/usr/bin/wget https://www.dropbox.com/s/v34fbw6t0mvtoi8/openvswitch-${ovsversion}-1.x86_64.rpm?dl=0 -O /vagrant/shared/openvswitch-${ovsversion}-1.x86_64.rpm",
+    cwd     => "/root",
+    creates => "/vagrant/shared/openvswitch-${ovsversion}-1.x86_64.rpm",
+    require => [
+                  Exec["mktarget"],
+               ],
+  }
+
+  exec { "install_ovs":
+    command => "yum localinstall -y /vagrant/shared/openvswitch-${ovsversion}-1.x86_64.rpm",
+    cwd => "/root",
+    path    => ["/bin", "/usr/bin"],
+    require => [
+                  Exec["download_ovs"],
+               ],
+  }
+
+  exec { "start_ovs":
+    command => "systemctl start openvswitch.service",
+    require => [
+                  Exec["install_ovs"],
+               ],
+    cwd => "/root",
+    path    => ["/bin", "/usr/bin"],
+  }

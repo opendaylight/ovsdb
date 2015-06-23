a01bc707f4b50d917a559cacfb446c6ef15cef05
Flavio Fernandes <ffernand@redhat.com>
2015-06-23 11:33:24 -0400

    Bug 3841 - wrong severity level: getTenantNetwork did not find network
    when using normal ovsdb net-virt, I see benign logs like these:
    
    WARN  | ntDispatcherImpl | TenantNetworkManagerImpl | 271 - org.opendaylight.ovsdb.openstack.net-virt - 1.1.0.SNAPSHOT | getTenantNetwork did not find network for patch-int
    
    these should be debug at best; not warning.
    
    Change-Id: I6f0b0aa6880c2b6892e5b9974ce5df6c0d217bf5
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/impl/TenantNetworkManagerImpl.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/impl/TenantNetworkManagerImpl.java
@@ -124,18 +124,14 @@ public class TenantNetworkManagerImpl implements ConfigInterface, TenantNetworkM
                 if (neutronNetwork != null) {
                     logger.debug("mapped to {}", neutronNetwork);
                 } else {
-                    logger.debug("getTenantNetwork: did not find neutronNetwork in cache");
+                    logger.debug("getTenantNetwork: did not find neutronNetwork in cache from neutronPort {}",
+                                 neutronPortId);
                 }
             } else {
-                logger.debug("getTenantNetwork: did not find neutronPort in cache");
+                logger.warn("getTenantNetwork did not find network for {}", terminationPointAugmentation.getName());
             }
         } else {
             logger.debug("getTenantNetwork: did not find {} in external_ids", Constants.EXTERNAL_ID_INTERFACE_ID);
-        }
-        if (neutronNetwork != null) {
-            logger.debug("mapped to {}", neutronNetwork);
-        } else {
-            logger.warn("getTenantNetwork did not find network for {}", terminationPointAugmentation.getName());
         }
         return neutronNetwork;
     }

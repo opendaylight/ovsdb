564d6866df4df5d6b0f61f3428aac1471ca1a6c7
Flavio Fernandes <ffernand@redhat.com>
2015-06-30 11:58:50 -0400

    DO NOT MERGE: firt stab at providing netvirt:1 topology in md-sal
    Change-Id: Ibe71d823b986fcdc9c12b6fa2a3994ae158fb696
    Signed-off-by: Flavio Fernandes <ffernand@redhat.com>

--- openstack/net-virt-providers/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/providers/NetvirtProvidersProvider.java
+++ openstack/net-virt-providers/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/providers/NetvirtProvidersProvider.java
@@ -3,6 +3,8 @@ package org.opendaylight.ovsdb.openstack.netvirt.providers;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.ovsdb.openstack.netvirt.api.Southbound;
+import org.opendaylight.ovsdb.utils.servicehelper.ServiceHelper;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,6 +39,9 @@ public class NetvirtProvidersProvider implements BindingAwareProvider, AutoClose
         this.activator = new ConfigActivator(providerContext);
         try {
             activator.start(bundleContext);
+
+            // Southbound southbound = (Southbound) ServiceHelper.getGlobalInstance(Southbound.class, this);
+            // southbound.initializeNetvirtTopology();
         } catch (Exception e) {
             LOG.warn("Failed to start Netvirt: ", e);
         }

edce3944121fc34c6f42d9a1a2a668e860920cf5
Aswin Suryanarayanan <aswin.suryanarayanan@hp.com>
2015-07-06 20:09:52 +0530

    Added fixed DHCP security rules, which will be added on a VM create.
    Change-Id: I8652c986acf3488ddf661ed265bff4cf6fb5ed1e
    Signed-off-by: Aswin Suryanarayanan <aswin.suryanarayanan@hp.com>
    (cherry picked from commit 8565b7700aa90efbf849472d10e9b4c41a403abe)

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/SecurityServicesManager.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/SecurityServicesManager.java
@@ -9,8 +9,13 @@
 
 package org.opendaylight.ovsdb.openstack.netvirt.api;
 
+import java.util.List;
+
+import org.opendaylight.neutron.spi.NeutronPort;
 import org.opendaylight.neutron.spi.NeutronSecurityGroup;
+import org.opendaylight.neutron.spi.Neutron_IPs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.*;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 
 /**
  * Open vSwitch isolates Tenant Networks using VLANs on the Integration Bridge
@@ -31,5 +36,42 @@ public interface SecurityServicesManager {
      * @return the security group in port
      */
     public NeutronSecurityGroup getSecurityGroupInPort(OvsdbTerminationPointAugmentation intf);
+     /**
+     * Gets the DHCP server port corresponding to a network.
+     *
+     * @param intf the intf
+     * @return the security group in port
+     */
+    public NeutronPort getDHCPServerPort(OvsdbTerminationPointAugmentation intf);
 
+    /**
+     * Is the port a compute port.
+     *
+     * @param intf the intf
+     * @return the security group in port
+     */
+    public boolean isComputePort(OvsdbTerminationPointAugmentation intf);
+
+    /**
+     * Is this the last port in the subnet to which interface belongs to.
+     *
+     * @param intf the intf
+     * @return the security group in port
+     */
+    public boolean isLastPortinSubnet(Node node, OvsdbTerminationPointAugmentation intf);
+
+    /**
+     * Is this the last port in the bridge to which interface belongs to.
+     *
+     * @param intf the intf
+     * @return the security group in port
+     */
+    public boolean isLastPortinBridge(Node node, OvsdbTerminationPointAugmentation intf);
+    /**
+     * Returns the  list of ip adddress assigned to the interface.
+     *
+     * @param intf the intf
+     * @return the security group in port
+     */
+    public List<Neutron_IPs> getIpAddress(Node node, OvsdbTerminationPointAugmentation intf);
 }

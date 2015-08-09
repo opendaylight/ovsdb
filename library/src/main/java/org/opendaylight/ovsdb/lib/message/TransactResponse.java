82e76df8697019ce70c6bb4cf599c29d40d6d313
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 11:54:30 -0400

    Fix license header violations in library
    Change-Id: I8f070f8841200bc6233d51b7d918625a44e25587
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- library/src/main/java/org/opendaylight/ovsdb/lib/message/TransactResponse.java
+++ library/src/main/java/org/opendaylight/ovsdb/lib/message/TransactResponse.java
@@ -1,12 +1,11 @@
 /*
- * Copyright (C) 2013 EBay Software Foundation
+ * Copyright (c) 2013, 2015 EBay Software Foundation and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
- *
- * Authors : Ashwin Raveendran, Madhu Venugopal
  */
+
 package org.opendaylight.ovsdb.lib.message;
 
 import java.util.ArrayList;

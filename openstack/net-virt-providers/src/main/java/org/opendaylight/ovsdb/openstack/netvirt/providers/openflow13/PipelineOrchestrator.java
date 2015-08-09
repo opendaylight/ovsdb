9c7350aa2a2503ebf8bb41f392fc1b36166d7559
Thanh Ha <thanh.ha@linuxfoundation.org>
2015-08-09 11:38:32 -0400

    Fix license header violations in net-virt-providers
    Change-Id: Id60cb77eb7b2b16135a7ad99d71f4d3967b615bb
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>

--- openstack/net-virt-providers/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/PipelineOrchestrator.java
+++ openstack/net-virt-providers/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/PipelineOrchestrator.java
@@ -1,11 +1,13 @@
 /*
- * Copyright (C) 2014 Red Hat, Inc.
+ * Copyright (c) 2014, 2015 Red Hat, Inc. and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13;
+
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.osgi.framework.ServiceReference;
 

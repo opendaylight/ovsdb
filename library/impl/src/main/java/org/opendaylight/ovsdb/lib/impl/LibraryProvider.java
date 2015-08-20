091799ec627ad041985e83dd6ce2b0f6443988c6
Stephen Kitt <skitt@redhat.com>
2015-08-20 14:04:01 +0200

    Switch from Felix to config-driven dependencies [WIP]
    The library module was regenerated using
    opendaylight-startup-archetype 1.1.0-SNAPSHOT.
    
    Change-Id: Ic6fb1c14f98b1557b9c87a702b20ef8a3173daf7
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

---
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/impl/LibraryProvider.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2015 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.lib.impl;
+
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LibraryProvider implements BindingAwareProvider, AutoCloseable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LibraryProvider.class);
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+        LOG.info("LibraryProvider Session Initiated");
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("LibraryProvider Closed");
+    }
+
+}
---
+++ library/it/src/test/java/org/opendaylight/ovsdb/lib/it/LibraryIT.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright © 2015 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.lib.it;
+
+import static org.ops4j.pax.exam.CoreOptions.composite;
+import static org.ops4j.pax.exam.CoreOptions.maven;
+import static org.ops4j.pax.exam.karaf.options.KarafDistributionOption.editConfigurationFilePut;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opendaylight.controller.mdsal.it.base.AbstractMdsalTestBase;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.PaxExam;
+import org.ops4j.pax.exam.karaf.options.LogLevelOption.LogLevel;
+import org.ops4j.pax.exam.options.MavenUrlReference;
+import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
+import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Ignore
+@RunWith(PaxExam.class)
+@ExamReactorStrategy(PerClass.class)
+public class LibraryIT extends AbstractMdsalTestBase {
+    private static final Logger LOG = LoggerFactory.getLogger(LibraryIT.class);
+
+    @Override
+    public String getModuleName() {
+        return "library";
+    }
+
+    @Override
+    public String getInstanceName() {
+        return "library-default";
+    }
+
+    @Override
+    public MavenUrlReference getFeatureRepo() {
+        return maven()
+                .groupId("org.opendaylight.ovsdb")
+                .artifactId("library-features")
+                .classifier("features")
+                .type("xml")
+                .versionAsInProject();
+    }
+
+    @Override
+    public String getFeatureName() {
+        return "odl-library-ui";
+    }
+
+    @Override
+    public Option getLoggingOption() {
+        Option option = editConfigurationFilePut(ORG_OPS4J_PAX_LOGGING_CFG,
+                logConfiguration(LibraryIT.class),
+                LogLevel.INFO.name());
+        option = composite(option, super.getLoggingOption());
+        return option;
+    }
+
+    @Test
+    public void testlibraryFeatureLoad() {
+        Assert.assertTrue(true);
+    }
+}

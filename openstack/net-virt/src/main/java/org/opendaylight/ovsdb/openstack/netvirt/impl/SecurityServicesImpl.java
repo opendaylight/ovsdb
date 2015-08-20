81bb0df0c46abdb53814a3ca65ff0fa97bf317eb
Isaku Yamahata <isaku.yamahata@intel.com>
2015-08-19 18:47:11 -0700

    Bug 4160 - null pointer exception in getDHCPServerPort()
    null pointer exception in SecurityServicesImpl.getDHCPServerPort()
    because neutronPortCache.getPort() may return null. It needs null check.
    NOTE: the order of neutron port creation/deletion via neutron northbound
    api and ovs port creation/deletion is not deterministic. Either order can
    happen. SecurityServicesImpl seems to be fixed to be aware of it.
    it's another story. This patch addresses only null pointer exception
    for now.
    
    > java.lang.NullPointerException
    >         at org.opendaylight.ovsdb.openstack.netvirt.impl.SecurityServicesImpl.getDHCPServerPort(SecurityServicesImpl.java:121)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13.OF13Provider.removeLocalRules(OF13Provider.java:845)[401:org.opendaylight.ovsdb.openstack.net-virt-providers:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.providers.openflow13.OF13Provider.handleInterfaceDelete(OF13Provider.java:1148)[401:org.opendaylight.ovsdb.openstack.net-virt-providers:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.SouthboundHandler.handleInterfaceDelete(SouthboundHandler.java:113)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.SouthboundHandler.processPortDelete(SouthboundHandler.java:166)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.SouthboundHandler.processPortEvent(SouthboundHandler.java:290)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.SouthboundHandler.processEvent(SouthboundHandler.java:233)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.impl.EventDispatcherImpl.dispatchEvent(EventDispatcherImpl.java:95)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.impl.EventDispatcherImpl.access$200(EventDispatcherImpl.java:28)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at org.opendaylight.ovsdb.openstack.netvirt.impl.EventDispatcherImpl$1.run(EventDispatcherImpl.java:57)[400:org.opendaylight.ovsdb.openstack.net-virt:1.2.0.SNAPSHOT]
    >         at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:471)[:1.7.0_79]
    >         at java.util.concurrent.FutureTask.run(FutureTask.java:262)[:1.7.0_79]
    >         at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1145)[:1.7.0_79]
    >         at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:615)[:1.7.0_79]
    >         at java.lang.Thread.run(Thread.java:745)[:1.7.0_79]
    
    Change-Id: I40f78b1477f1c2a6ad3e23b2e60e21be6d9abc23
    Signed-off-by: Isaku Yamahata <isaku.yamahata@intel.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/impl/SecurityServicesImpl.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/impl/SecurityServicesImpl.java
@@ -121,6 +121,10 @@ public class SecurityServicesImpl implements ConfigInterface, SecurityServicesMa
             return null;
         }
         NeutronPort neutronPort = neutronPortCache.getPort(neutronPortId);
+        if (neutronPort == null) {
+            LOG.error("getDHCPServerPort: neutron port of {} is not found", neutronPortId);
+            return null;
+        }
         //Since all the fixed ip assigned to a port should be from the same network, first port is sufficient.
         List<Neutron_IPs> fixedIps = neutronPort.getFixedIPs();
         if(null==fixedIps || 0 == fixedIps.size() )
@@ -178,6 +182,10 @@ public class SecurityServicesImpl implements ConfigInterface, SecurityServicesMa
             return false;
         }
         NeutronPort neutronPort = neutronPortCache.getPort(neutronPortId);
+        if (neutronPort == null) {
+            LOG.error("isLastPortinSubnet: neutron port of {} is not found", neutronPortId);
+            return false;
+        }
         List<Neutron_IPs> neutronPortFixedIp = neutronPort.getFixedIPs();
         if(null == neutronPortFixedIp || neutronPortFixedIp.isEmpty()) {
             return false;
@@ -247,8 +255,11 @@ public class SecurityServicesImpl implements ConfigInterface, SecurityServicesMa
             return null;
         }
         NeutronPort neutronPort = neutronPortCache.getPort(neutronPortId);
-        List<Neutron_IPs> fixedIps = neutronPort.getFixedIPs();
-        return fixedIps;
+        if (neutronPort == null) {
+            LOG.error("getIpAddress: neutron port of {} is not found", neutronPortId);
+            return null;
+        }
+        return neutronPort.getFixedIPs();
     }
 
     @Override

5e732947b82c4b8525cc058d4c517ef4514ddb35
Stephen Kitt <skitt@redhat.com>
2015-08-21 14:57:36 +0200

    Use org.junit instead of junit.framework
    Change-Id: I980d4a6da5e181266214c124959d84d3f5e34f9e
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- integrationtest/src/test/java/org/opendaylight/ovsdb/integrationtest/ovsdbclient/OvsdbClientTestIT.java
+++ integrationtest/src/test/java/org/opendaylight/ovsdb/integrationtest/ovsdbclient/OvsdbClientTestIT.java
@@ -18,9 +18,8 @@ import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
-import junit.framework.Assert;
-
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.opendaylight.ovsdb.lib.MonitorCallBack;

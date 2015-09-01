2a05bc086bf71c9c3d87ee330f797526c3309bff
Vishal Thapar <vishal.thapar@ericsson.com>
2015-09-01 15:02:08 +0530

    Initial bundle creation for HWTEP SB
    1. Creating initial bundles for hwvtep southbound using Startup Project
    archetype.
    2. Adds hwvtepsouthbound bundle and entry in ovsdb/pom.xml
    
    PatchSet2:
    1. Addressed review comments and added hwvtepsouthbound-it
    
    PatchSet3:
    1. Fixed IT failure.
    
    PatchSet4:
    Fixed minor issue in southbound/default-config.xml
    
    Change-Id: Ib185c1a9a90b08dc84f64231b8fee414d1e209a0
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

---
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/hwvtepsouthbound/impl/rev150901/HwvtepSouthboundModule.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2015 Ericsson India Global Services Pvt Ltd. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hwvtepsouthbound.impl.rev150901;
+
+import org.opendaylight.ovsdb.hwvtepsouthbound.impl.HwvtepSouthboundProvider;
+
+public class HwvtepSouthboundModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hwvtepsouthbound.impl.rev150901.AbstractHwvtepSouthboundModule {
+    public HwvtepSouthboundModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public HwvtepSouthboundModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.hwvtepsouthbound.impl.rev150901.HwvtepSouthboundModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        HwvtepSouthboundProvider provider = new HwvtepSouthboundProvider();
+        getBrokerDependency().registerProvider(provider);
+        return provider;
+    }
+
+}

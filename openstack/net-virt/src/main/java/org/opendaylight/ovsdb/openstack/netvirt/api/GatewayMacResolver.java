4b3ea4ab8889e5ac380a459a41e4abd7cecdb561
Alexis de Talhouët <adetalhouet@inocybe.com>
2015-09-17 12:48:21 -0400

    Fix Java 8 javadoc compatibility
    Change-Id: Iee2be4381922d7fb8a08b42192f277dcd2600a85
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/GatewayMacResolver.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/api/GatewayMacResolver.java
@@ -24,14 +24,14 @@ public interface GatewayMacResolver {
     /**
      * Method will trigger the mac resolution for gateway IP. If user set periodicRefresh to true,
      * it will periodically trigger the gateway resolution after a specific time interval using the
-     * given source IP and MAC addresses. It will also cache the source IP & MAC in case of periodicRefresh.
+     * given source IP and MAC addresses. It will also cache the source IP and MAC in case of periodicRefresh.
      * If periodicRefresh is false, it will just do one time gateway resolution and won't cache any internal data.
      * If user call the same method with different source ip and mac address, GatewayMacResolver service will
      * update the internally cached data with these new source ip and mac address and will use it as per
      * periodicRefresh flag.
      * @param externalNetworkBridgeDpid This bridge will be used for sending ARP request
      * @param gatewayIp ARP request will be send for this ip address
-     * @param periodicReferesh Do you want to periodically refresh the gateway mac?
+     * @param periodicRefresh Do you want to periodically refresh the gateway mac?
      * @return
      */
     public ListenableFuture<MacAddress> resolveMacAddress( final Long externalNetworkBridgeDpid, final Ipv4Address gatewayIp,

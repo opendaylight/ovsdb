cf713198237767ce4e3814a6136d1c0e30309cc1
Anil Vishnoi <vishnoianil@gmail.com>
2015-09-24 00:19:12 +0530

    Fixed unit test testDisconnect in OvsdbConnectionmanagerTest
    Change-Id: I2b2eca244036dc7567c24e85a71bd968f04ac587
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionManagerTest.java
+++ southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionManagerTest.java
@@ -127,6 +127,8 @@ public class OvsdbConnectionManagerTest {
         MemberModifier.suppress(MemberMatcher.method(OvsdbConnectionManager.class, "getConnectionInstance", ConnectionInfo.class));
         OvsdbConnectionInstance ovsdbConnectionInstance = mock(OvsdbConnectionInstance.class);
         when(ovsdbConnectionManager.getConnectionInstance(any(ConnectionInfo.class))).thenReturn(ovsdbConnectionInstance);
+
+        MemberModifier.suppress(MemberMatcher.method(OvsdbConnectionManager.class, "removeInstanceIdentifier", ConnectionInfo.class));
         ovsdbConnectionManager.disconnect(ovsdbNode);
         verify((OvsdbClient)ovsdbConnectionInstance).disconnect();
     }

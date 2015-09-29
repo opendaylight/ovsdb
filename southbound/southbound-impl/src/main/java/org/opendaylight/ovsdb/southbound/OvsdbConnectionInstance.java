5a0b0bd5a6984157437ebde9d73c193dd1204a57
Anil Vishnoi <vishnoianil@gmail.com>
2015-09-30 02:15:26 +0530

    Fixed iid issue for switch initiated connections and refactored the code a bit
    Change-Id: I9f8adfba4a4f866d423503b24f400c40bd43131b
    
    Conflicts:
    	southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionInstance.java
    	southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionManager.java
    
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionInstance.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbConnectionInstance.java
@@ -13,6 +13,7 @@ import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
 
+import org.opendaylight.controller.md.sal.common.api.clustering.Entity;
 import org.opendaylight.controller.md.sal.common.api.clustering.EntityOwnershipCandidateRegistration;
 import org.opendaylight.ovsdb.lib.EchoServiceCallbackFilters;
 import org.opendaylight.ovsdb.lib.LockAquisitionCallback;
@@ -60,6 +61,7 @@ public class OvsdbConnectionInstance implements OvsdbClient {
     // private ConnectionInfo key;
     private InstanceIdentifier<Node> instanceIdentifier;
     private volatile boolean hasDeviceOwnership = false;
+    private Entity connectedEntity;
     private EntityOwnershipCandidateRegistration deviceOwnershipCandidateRegistration;
 
     OvsdbConnectionInstance(ConnectionInfo key,OvsdbClient client,TransactionInvoker txInvoker,
@@ -241,6 +243,14 @@ public class OvsdbConnectionInstance implements OvsdbClient {
             DatabaseSchema schema, List<MonitorRequest<E>> monitorRequests,
             MonitorHandle monitorHandle, MonitorCallBack callback) {
         return null;
+    }
+
+    public Entity getConnectedEntity() {
+        return this.connectedEntity;
+    }
+
+    public void setConnectedEntity(Entity entity ) {
+        this.connectedEntity = entity;
     }
 
     public Boolean hasOvsdbClient(OvsdbClient otherClient) {

6aabe6c31e776a9483c1ad3c6b77493b5646ba30
Mohnish Anumala <Mohnish_Anumala@Dell.com>
2015-10-01 10:46:43 -0700

    Support for sending ICMPv6 pkts to controller.
    Signed-off-by: Mohnish Anumala <Mohnish_Anumala@Dell.com>

--- routemgr/routemgr-impl/src/main/java/org/opendaylight/ovsdb/routemgr/net/PktHandler.java
+++ routemgr/routemgr-impl/src/main/java/org/opendaylight/ovsdb/routemgr/net/PktHandler.java
@@ -10,6 +10,7 @@ package org.opendaylight.ovsdb.routemgr.net;
 
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketProcessingListener;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.packet.service.rev130709.PacketReceived;
+import org.opendaylight.ovsdb.routemgr.utils.BitBufferHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +21,14 @@ import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 public class PktHandler implements PacketProcessingListener {
     private static final Logger LOG = LoggerFactory.getLogger(PktHandler.class);
     public static final int IPv6_ETHTYPE = 34525;
+    public static final int ICMP_v6 = 1;
+
+    public static final int ETHTYPE_START = 96;
+    public static final int ONE_BYTE  = 8;
+    public static final int TWO_BYTES = 16;
+    public static final int IPv6_HDR_START = 112;
+    public static final int IPv6_NEXT_HDR = 48;
+
     private DataBroker dataService;
 
     public void setDataBrokerService(DataBroker dataService) {
@@ -29,6 +38,9 @@ public class PktHandler implements PacketProcessingListener {
     @Override
     public void onPacketReceived(PacketReceived packetReceived) {
         boolean result = false;
+        int     ethType;
+        int     v6NxtHdr;
+
         if (packetReceived == null) {
             LOG.debug("receiving null packet. returning without any processing");
             return;
@@ -39,12 +51,19 @@ public class PktHandler implements PacketProcessingListener {
             return;
         }
         try {
-            //int ethType = BitBufferHelper.getInt(BitBufferHelper.getBits(data, 96, 16));
-            //if (ethType == IPv6_ETHTYPE) {
-            LOG.debug("IPv6 Pdu received on port {} ", packetReceived.getIngress());
-            //} else {
-            //    return;
-            //}
+            ethType = BitBufferHelper.getInt(BitBufferHelper.getBits(data, ETHTYPE_START, TWO_BYTES));
+            if (ethType == IPv6_ETHTYPE) {
+                v6NxtHdr = BitBufferHelper.getInt(BitBufferHelper.getBits(data,
+                            IPv6_HDR_START + IPv6_NEXT_HDR, TWO_BYTES));
+                if (v6NxtHdr == ICMP_v6) {
+                    LOG.debug("ICMPv6 Pdu received on port {} ", packetReceived.getIngress());
+                } else {
+                    LOG.debug("IPv6 Pdu received on port {} ", packetReceived.getIngress());
+                }
+            } else {
+                LOG.debug("Pdu received on port {} ", packetReceived.getIngress());
+                return;
+            }
         } catch (Exception e) {
             LOG.warn("Failed to decode packet: {}", e.getMessage());
             return;

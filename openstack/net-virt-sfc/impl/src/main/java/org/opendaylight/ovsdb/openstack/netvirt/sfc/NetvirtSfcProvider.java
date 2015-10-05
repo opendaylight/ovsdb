0a293ac6b41432017ed5e430619a5c87924c12eb
arunsarat <arunsarat@yahoo.com>
2015-10-05 13:32:20 -0700

    Net-virt-sfc Data Change Listener code first pass.
    Change-Id: Ie1e8e4471b2bddf476b8a55a1d022d4ace517868
    Signed-off-by: arunsarat <arunsarat@yahoo.com>
    Signed-off-by: Sam Hague <shague@redhat.com>

--- openstack/net-virt-sfc/impl/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/sfc/NetvirtSfcProvider.java
+++ openstack/net-virt-sfc/impl/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/sfc/NetvirtSfcProvider.java
@@ -1,13 +1,21 @@
 /*
- * Copyright © 2015 Red Hat, Inc. and others.  All rights reserved.
+ * Copyright © 2015 Dell, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
+
 package org.opendaylight.ovsdb.openstack.netvirt.sfc;
 
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import com.google.common.base.Preconditions;
+
+import org.osgi.framework.BundleContext;
+
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.ovsdb.openstack.netvirt.sfc.openflow13.NetvirtSfcOF13Provider;
+import org.opendaylight.ovsdb.openstack.netvirt.sfc.openflow13.INetvirtSfcOF13Provider;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,15 +23,32 @@ import org.slf4j.LoggerFactory;
 public class NetvirtSfcProvider implements BindingAwareProvider, AutoCloseable {
 
     private static final Logger LOG = LoggerFactory.getLogger(NetvirtSfcProvider.class);
+    private DataBroker dataBroker = null;
+    private BundleContext bundleContext = null;
+    private NetvirtSfcAclListener aclListener;
+
+    private NetvirtSfcClassifierListener classfierListener;
+    private INetvirtSfcOF13Provider provider;
+
+    public NetvirtSfcProvider (BundleContext bundleContext) {
+        LOG.info("NetvirtProvider: bundleContext: {}", bundleContext);
+        this.bundleContext = bundleContext;
+    }
 
     @Override
     public void onSessionInitiated(ProviderContext session) {
         LOG.info("NetvirtSfcProvider Session Initiated");
+        dataBroker = session.getSALService(DataBroker.class);
+
+        provider = new NetvirtSfcOF13Provider (this.dataBroker);
+        aclListener = new NetvirtSfcAclListener(provider, this.dataBroker);
+        classfierListener = new NetvirtSfcClassifierListener (provider, this.dataBroker);
     }
 
     @Override
     public void close() throws Exception {
         LOG.info("NetvirtSfcProvider Closed");
+        aclListener.close();
+        classfierListener.close();
     }
-
 }

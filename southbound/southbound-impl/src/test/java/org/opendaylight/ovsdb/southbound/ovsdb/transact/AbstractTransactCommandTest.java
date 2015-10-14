577b263d646be3567c83c53f05812397e3c8ddb7
Mohamed El-Serngawy <melserngawy@inocybe.com>
2015-10-14 16:09:48 -0400

    Unit test for ovsdb.southbound.ovsdb.transact
    Change-Id: I200bf945bf2d8875d5e8de23c101420092a9161e
    Signed-off-by: Mohamed El-Serngawy <melserngawy@inocybe.com>

---
+++ southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/AbstractTransactCommandTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2015 Inocybe inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.ovsdb.southbound.ovsdb.transact;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+@RunWith(PowerMockRunner.class)
+public class AbstractTransactCommandTest {
+
+    @Mock private BridgeOperationalState operationalState;
+    @Mock private AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes;
+
+    public class AbstractTransactCommandChild extends AbstractTransactCommand {
+        public AbstractTransactCommandChild(BridgeOperationalState state, AsyncDataChangeEvent<InstanceIdentifier<?>,
+                DataObject> changes) {
+            super(state, changes);
+        }
+
+        @Override
+        public void execute(TransactionBuilder transaction) {
+            // TODO Auto-generated method stub
+        }
+    }
+
+    @Test
+    public void testAbstractTransactCommand() {
+        AbstractTransactCommandChild abstractTransCmd = new AbstractTransactCommandChild(operationalState, changes);
+        assertEquals(operationalState, abstractTransCmd.getOperationalState());
+        assertEquals(changes, abstractTransCmd.getChanges());
+    }
+}

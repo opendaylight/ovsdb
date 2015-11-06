1f7cfe3745bfe5da78a7e2b8453b2052f744880b
Anil Vishnoi <vishnoianil@gmail.com>
2015-11-07 04:07:33 +0530

    Fixed wildcarded imports
    Change-Id: I4a8650aefddd0455e55d005a2ffd017a34d64293
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

--- openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/impl/NeutronL3Adapter.java
+++ openstack/net-virt/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/impl/NeutronL3Adapter.java
@@ -10,7 +10,12 @@ package org.opendaylight.ovsdb.openstack.netvirt.impl;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 

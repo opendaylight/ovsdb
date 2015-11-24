a8022bc0e8b3364327b1d4213a7b2a7eefd625a7
karanrajsingh <karan.raj.s.singh@ericsson.com>
2015-11-24 21:43:56 +0530

    Changes to LogicalSwitchUpdate as per new yang
    Patchset2: review comments
    
    Change-Id: I0bff6c4a2dbcb63fde20da8c2595061af5e8bca2
    Signed-off-by: karanrajsingh <karan.raj.s.singh@ericsson.com>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transactions/md/LogicalSwitchUpdateCommand.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transactions/md/LogicalSwitchUpdateCommand.java
@@ -16,25 +16,21 @@ import java.util.Map.Entry;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
 import org.opendaylight.ovsdb.hwvtepsouthbound.HwvtepConnectionInstance;
-import org.opendaylight.ovsdb.hwvtepsouthbound.HwvtepSouthboundMapper;
 import org.opendaylight.ovsdb.hwvtepsouthbound.HwvtepSouthboundUtil;
 import org.opendaylight.ovsdb.lib.message.TableUpdates;
 import org.opendaylight.ovsdb.lib.notation.UUID;
 import org.opendaylight.ovsdb.lib.schema.DatabaseSchema;
 import org.opendaylight.ovsdb.lib.schema.typed.TyperUtils;
 import org.opendaylight.ovsdb.schema.hardwarevtep.LogicalSwitch;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentationBuilder;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepNodeName;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepPhysicalSwitchRef;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitches;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitchesBuilder;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.Switches;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.SwitchesBuilder;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.LogicalSwitchesKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
-import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeKey;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -70,67 +66,34 @@ public class LogicalSwitchUpdateCommand extends AbstractTransactionCommand {
             LOG.debug("Connection {} is present",connection);
             Node connectionNode = buildConnectionNode(lSwitch);
             transaction.merge(LogicalDatastoreType.OPERATIONAL, connectionIId, connectionNode);
-            // Update the Logical Switch with whatever data we are getting
-            InstanceIdentifier<Node> lsIid = getInstanceIdentifier(lSwitch);
-            Node lsNode = buildLogicalSwitchNode(lSwitch);
-            transaction.merge(LogicalDatastoreType.OPERATIONAL, lsIid, lsNode);
 //            TODO: Delete entries that are no longer needed
         }
     }
 
-    private Node buildLogicalSwitchNode(LogicalSwitch lSwitch) {
-        return null;
-/*        NodeBuilder lsNodeBuilder = new NodeBuilder();
-        NodeId psNodeId = getNodeId(lSwitch);
-        lsNodeBuilder.setNodeId(psNodeId);
-        LogicalSwitchesBuilder lsAugBuilder = new LogicalSwitchesBuilder();
-        setLogicalSwitchId(lsAugBuilder, lSwitch);
-        lsNodeBuilder.addAugmentation(LogicalSwitches.class, lsAugBuilder.build());
-
-        LOG.trace("Built with the intent to store PhysicalSwitch data {}",
-                lsAugBuilder.build());
-        return lsNodeBuilder.build();*/
-    }
-
-    private void setLogicalSwitchId(LogicalSwitchesBuilder lsAugBuilder, LogicalSwitch lSwitch) {
-        lsAugBuilder.setHwvtepNodeName(new HwvtepNodeName(lSwitch.getName()));
-        if(lSwitch.getDescription() != null) {
-            lsAugBuilder.setHwvtepNodeDescription(lSwitch.getDescription());
-        }
-    }
 
     private Node buildConnectionNode(LogicalSwitch lSwitch) {
-        //Update node with PhysicalSwitch reference
+        //Update node with LogicalSwitch reference
         NodeBuilder connectionNode = new NodeBuilder();
         connectionNode.setNodeId(getOvsdbConnectionInstance().getNodeId());
 
         HwvtepGlobalAugmentationBuilder hgAugmentationBuilder = new HwvtepGlobalAugmentationBuilder();
-        List<Switches> switches = new ArrayList<>();
-        InstanceIdentifier<Node> switchIid = HwvtepSouthboundMapper.createInstanceIdentifier(getOvsdbConnectionInstance(),
-                        lSwitch);
-        hgAugmentationBuilder.setSwitches(switches);
-        /* FIXME:
-         * TODO: This need to be revisited after fix in yang file.
-         * It should ideally be HwvtepSwitchRef
-         */
-        Switches logicalSwitch = new SwitchesBuilder().setSwitchRef(
-                        new HwvtepPhysicalSwitchRef(switchIid)).build(); 
-        switches.add(logicalSwitch);
+        List<LogicalSwitches> lSwitches = new ArrayList<>();
+
+        LogicalSwitchesBuilder lsBuilder = new LogicalSwitchesBuilder();
+        lsBuilder.setHwvtepLogicalSwitchExternalId(new Uuid(lSwitch.getUuid().toString()));
+        lsBuilder.setHwvtepNodeDescription(lSwitch.getDescription());
+        HwvtepNodeName hwvtepName = new HwvtepNodeName(lSwitch.getName());
+        lsBuilder.setHwvtepNodeName(hwvtepName);
+        lsBuilder.setKey(new LogicalSwitchesKey(hwvtepName));
+        if(lSwitch.getTunnelKeyColumn().getData()!=null && !lSwitch.getTunnelKeyColumn().getData().isEmpty()){
+            lsBuilder.setTunnelKey(lSwitch.getTunnelKeyColumn().getData().iterator().next().toString());
+        }
+        lSwitches.add(lsBuilder.build());
+        hgAugmentationBuilder.setLogicalSwitches(lSwitches);
 
         connectionNode.addAugmentation(HwvtepGlobalAugmentation.class, hgAugmentationBuilder.build());
 
-        LOG.debug("Update node with logicalswitch ref {}",
-                hgAugmentationBuilder.getSwitches().iterator().next());
+        LOG.debug("Update node with logicalswitches {}", lSwitches);
         return connectionNode.build();
-    }
-
-    private InstanceIdentifier<Node> getInstanceIdentifier(LogicalSwitch lSwitch) {
-        return HwvtepSouthboundMapper.createInstanceIdentifier(getOvsdbConnectionInstance(),
-                lSwitch);
-    }
-
-    private NodeId getNodeId(LogicalSwitch lSwitch) {
-        NodeKey nodeKey = getInstanceIdentifier(lSwitch).firstKeyOf(Node.class, NodeKey.class);
-        return nodeKey.getNodeId();
     }
 }

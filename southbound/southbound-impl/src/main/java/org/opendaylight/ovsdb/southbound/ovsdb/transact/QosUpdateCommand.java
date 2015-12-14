d64c55d65dda23f61737086d1522b0c2347b2391
Eric Multanen <eric.w.multanen@intel.com>
2015-12-14 07:10:10 -0800

    Bug 5008 - QoS and Queue fixes for ovsdb southbound
    Update the operational MD-SAL for Queue and Qos updates from the OVS
    host.
    
    Support configuration of QoS and Queue via configuration MD-SAL.
    
    - Support for adding and deleting QoS entries.  A qos-id is used
      as a handle for qos entries in the MD-SAL.  This is added to
      the 'external-ids' of the qos entry.  Qos entries originating
      from the OVS host are assigned a qos-id of "qos://<uuid>".
    - Qos entries can be assigned to a Port (termination point)
      which is present in the MD-SAL by setting the port qos
      attribute to the UUID of the relevant Qos entry.
    - Qos entries are cleared from a port by updating the
      port configuration with the qos attribute removed.
    - Support for adding and deleting Queue entries. a queue-id
      is used as a handle for queue entries in the MD-SAL.  This
      is added to the 'external-ids' of the queue entry.  Queue
      entries originating from the OVS host are assign a qos-id
      of "queue://<uuid>".
    - Queue entries can be added or deleted to the queue list
      of a Qos entry present in the config by filling out (or
      removing from) the queue-list attribute of the qos entry.
    
    v2 - added setting of exising qos uuid to an existing port
       - added clearing of a qos setting for a port
    v3 - ovsdb.yang whitespace fixes
       - fixes for failed tests
    v4 - fixed model - queue list in Qos entries needed
         a queuenumber key value
       - fixed up comments on whitespace and log messages
    v5 - finished set/clear of queues list in a qos entry
       - added more description about the functionality in
         the patch set.
    v6 - fixed up more whitespace, and copyright comments.
    v7 - address review comments:
         - white space
         - remove unnecessary code
         - remove commented out code
         - adjust some LOG levels
         - remove unused imports
    
    Change-Id: Idec02aaa82e1ffa66e58862ecfc5edff05050444
    Signed-off-by: Eric Multanen <eric.w.multanen@intel.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/QosUpdateCommand.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright (c) 2016 Intel Corporation and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.southbound.ovsdb.transact;
+
+import static org.opendaylight.ovsdb.lib.operations.Operations.op;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.ovsdb.lib.notation.Condition;
+import org.opendaylight.ovsdb.lib.notation.Mutator;
+import org.opendaylight.ovsdb.lib.notation.UUID;
+import org.opendaylight.ovsdb.lib.operations.Mutate;
+import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
+import org.opendaylight.ovsdb.lib.schema.GenericTableSchema;
+import org.opendaylight.ovsdb.lib.schema.typed.TyperUtils;
+import org.opendaylight.ovsdb.schema.openvswitch.Port;
+import org.opendaylight.ovsdb.schema.openvswitch.Qos;
+import org.opendaylight.ovsdb.southbound.SouthboundConstants;
+import org.opendaylight.ovsdb.southbound.SouthboundMapper;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.inet.types.rev100924.Uri;
+import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.QosEntries;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.qos.entries.QosExternalIds;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.qos.entries.QosOtherConfig;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.qos.entries.QueueList;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableMap;
+
+public class QosUpdateCommand extends AbstractTransactCommand {
+    private static final Logger LOG = LoggerFactory.getLogger(QosUpdateCommand.class);
+
+
+    public QosUpdateCommand(BridgeOperationalState state, AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> changes) {
+        super(state, changes);
+    }
+
+    @Override
+    public void execute(TransactionBuilder transaction) {
+        Map<InstanceIdentifier<OvsdbNodeAugmentation>, OvsdbNodeAugmentation> created =
+                TransactUtils.extractCreated(getChanges(),OvsdbNodeAugmentation.class);
+        for (Entry<InstanceIdentifier<OvsdbNodeAugmentation>, OvsdbNodeAugmentation> ovsdbNodeEntry:
+            created.entrySet()) {
+            updateQos(transaction,  ovsdbNodeEntry.getKey(), ovsdbNodeEntry.getValue());
+        }
+        Map<InstanceIdentifier<OvsdbNodeAugmentation>, OvsdbNodeAugmentation> updated =
+                TransactUtils.extractUpdated(getChanges(),OvsdbNodeAugmentation.class);
+        for (Entry<InstanceIdentifier<OvsdbNodeAugmentation>, OvsdbNodeAugmentation> ovsdbNodeEntry:
+            updated.entrySet()) {
+            updateQos(transaction,  ovsdbNodeEntry.getKey(), ovsdbNodeEntry.getValue());
+        }
+    }
+
+    private void updateQos(
+            TransactionBuilder transaction,
+            InstanceIdentifier<OvsdbNodeAugmentation> iid, OvsdbNodeAugmentation ovsdbNode) {
+
+        List<QosEntries> qosEntries = ovsdbNode.getQosEntries();
+
+        if (!getOperationalState().getBridgeNode(iid).isPresent()) {
+            return;
+        }
+        OvsdbNodeAugmentation operNode = getOperationalState().getBridgeNode(iid).get().getAugmentation(OvsdbNodeAugmentation.class);
+        List<QosEntries> operQosEntries = operNode.getQosEntries();
+
+        if (qosEntries != null) {
+            for (QosEntries qosEntry : qosEntries) {
+                Qos qos = TyperUtils.getTypedRowWrapper(transaction.getDatabaseSchema(), Qos.class);
+
+                if (qosEntry.getQosType() != null) {
+                    qos.setType(SouthboundMapper.createQosType(qosEntry.getQosType()));
+                }
+
+                Uuid qosUuid = getQosEntryUuid(operQosEntries, qosEntry.getQosId());
+                UUID uuid = null;
+                if (qosUuid != null) {
+                    uuid = new UUID(qosUuid.getValue());
+                }
+
+                List<QueueList> queueList = qosEntry.getQueueList();
+                Map<Long, UUID>newQueueList = new HashMap<>();
+                if (queueList != null && !queueList.isEmpty()) {
+                    for (QueueList queue : queueList) {
+                        newQueueList.put(queue.getQueueNumber(), new UUID(queue.getQueueUuid().getValue()));
+                    }
+                }
+                qos.setQueues(newQueueList);
+
+                List<QosExternalIds> externalIds = qosEntry.getQosExternalIds();
+                Map<String, String> externalIdsMap = new HashMap<>();
+                if (externalIds != null) {
+                    for (QosExternalIds externalId : externalIds) {
+                        externalIdsMap.put(externalId.getQosExternalIdKey(), externalId.getQosExternalIdValue());
+                    }
+                }
+                externalIdsMap.put(SouthboundConstants.QOS_ID_EXTERNAL_ID_KEY, qosEntry.getQosId().getValue());
+                try {
+                    qos.setExternalIds(ImmutableMap.copyOf(externalIdsMap));
+                } catch (NullPointerException e) {
+                    LOG.warn("Incomplete Qos external IDs");
+                }
+
+                List<QosOtherConfig> otherConfigs = qosEntry.getQosOtherConfig();
+                if (otherConfigs != null) {
+                    Map<String, String> otherConfigsMap = new HashMap<>();
+                    for (QosOtherConfig otherConfig : otherConfigs) {
+                        otherConfigsMap.put(otherConfig.getOtherConfigKey(), otherConfig.getOtherConfigValue());
+                    }
+                    try {
+                        qos.setOtherConfig(ImmutableMap.copyOf(otherConfigsMap));
+                    } catch (NullPointerException e) {
+                        LOG.warn("Incomplete Qos other_config", e);
+                    }
+                }
+                if (uuid == null) {
+                    transaction.add(op.insert(qos)).build();
+                } else {
+                    Qos extraQos = TyperUtils.getTypedRowWrapper(
+                            transaction.getDatabaseSchema(), Qos.class, null);
+                    extraQos.getUuidColumn().setData(uuid);
+                    transaction.add(op.update(qos)
+                            .where(extraQos.getUuidColumn().getSchema().opEqual(uuid)).build());
+                }
+                transaction.build();
+            }
+        }
+    }
+
+    private Uuid getQosEntryUuid(List<QosEntries> operQosEntries, Uri qosId) {
+        if (operQosEntries != null && !operQosEntries.isEmpty()) {
+            for (QosEntries qosEntry : operQosEntries) {
+                if (qosEntry.getQosId().equals(qosId)) {
+                    return qosEntry.getQosUuid();
+                }
+            }
+        }
+        return null;
+    }
+}

8df95a934cf1940fa4676d0ffe7a93fd452cb592
Andre Fredette <afredette@redhat.com>
2015-12-22 18:52:30 -0500

    First netvirt renderer framework: Hardware Gateway (hwgw)
    Change-Id: Ic84ba7c7fda140e1bba304541da2d7cc4c0254de
    Signed-off-by: Andre Fredette <afredette@redhat.com>

---
+++ netvirt/renderers/hwgw/src/test/java/org/opendaylight/ovsdb/netvirt/renderers/hwgw/impl/HwgwProviderTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Red Hat, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.netvirt.renderers.hwgw.impl;
+
+import org.junit.Test;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+
+import static org.mockito.Mockito.mock;
+
+public class HwgwProviderTest {
+    @Test
+    public void testOnSessionInitiated() {
+        HwgwProvider provider = new HwgwProvider();
+
+        // ensure no exceptions
+        // currently this method is empty
+        provider.onSessionInitiated(mock(BindingAwareBroker.ProviderContext.class));
+    }
+
+    @Test
+    public void testClose() throws Exception {
+        HwgwProvider provider = new HwgwProvider();
+
+        // ensure no exceptions
+        // currently this method is empty
+        provider.close();
+    }
+}

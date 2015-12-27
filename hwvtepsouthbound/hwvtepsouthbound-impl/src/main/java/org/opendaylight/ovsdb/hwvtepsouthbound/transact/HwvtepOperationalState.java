9b3005bd8034e9ef2dec29567094e2891cd6d818
Vishal Thapar <vishal.thapar@ericsson.com>
2015-12-27 19:13:58 +0530

    Add Tunnel configuration
    Yang changes to configure tunnels [including bfd]
    Code to store tunnels information in Operational DataStore.
    Code to configure tunnels
    
    Change-Id: Ibd882c6737305af9931488e4cda394e92bcdaf91
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transact/HwvtepOperationalState.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transact/HwvtepOperationalState.java
@@ -36,11 +36,14 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hw
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.RemoteUcastMacs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.RemoteUcastMacsKey;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.global.attributes.Switches;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.physical._switch.attributes.Tunnels;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.hwvtep.physical._switch.attributes.TunnelsKey;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import com.google.common.base.Optional;
 import com.google.common.base.Preconditions;
 
@@ -132,6 +135,26 @@ public class HwvtepOperationalState {
                 for (LogicalSwitches lswitch: lswitchList) {
                     if (lswitch.getKey().equals(logicalSwitchesKey)) {
                         return Optional.fromNullable(lswitch);
+                    }
+                }
+            }
+        }
+        return Optional.absent();
+    }
+
+    public Optional<Tunnels> getTunnels(InstanceIdentifier<?> iid, TunnelsKey tunnelsKey) {
+        Preconditions.checkNotNull(iid);
+        Optional<PhysicalSwitchAugmentation> psOptional = getPhysicalSwitchAugmentation(iid);
+        if (psOptional.isPresent()) {
+            PhysicalSwitchAugmentation psAugmentation = psOptional.get();
+            List<Tunnels> tunnelList = null;
+            if (psAugmentation != null) {
+                tunnelList = psAugmentation.getTunnels();
+            }
+            if (tunnelList != null) {
+                for (Tunnels tunnel: tunnelList) {
+                    if (tunnel.getKey().equals(tunnelsKey)) {
+                        return Optional.fromNullable(tunnel);
                     }
                 }
             }
@@ -264,7 +287,12 @@ public class HwvtepOperationalState {
         Optional<LogicalSwitches> lswitch = HwvtepSouthboundUtil.readNode(transaction, iid);
         return lswitch;
     }
-    
+
+    public Optional<Tunnels> getTunnels(InstanceIdentifier<Tunnels> iid) {
+        Optional<Tunnels> tunnels = HwvtepSouthboundUtil.readNode(transaction, iid);
+        return tunnels;
+    }
+
     public ReadWriteTransaction getReadWriteTransaction() {
         return transaction;
     }

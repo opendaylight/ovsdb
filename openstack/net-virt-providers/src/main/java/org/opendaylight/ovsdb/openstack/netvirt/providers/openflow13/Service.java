c7ced46e90e7a8259e3ce51f787ee1a74d7a366d
Josh <jhershbe@redhat.com>
2016-01-10 13:13:01 +0200

    Bug 4132 config ovs to respond to pings to router
    Relies on https://git.opendaylight.org/gerrit/#/c/32135/
    N.B. I added a unit test, IcmpEchoResponderServiceTest,
    however it's currently skipped along with all the other
    unit tests under services.
    
    Change-Id: If5d212c29acaf49d3cdd16cb9a02a63073202a88
    Signed-off-by: Josh <jhershbe@redhat.com>

--- openstack/net-virt-providers/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/Service.java
+++ openstack/net-virt-providers/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/providers/openflow13/Service.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015 Red Hat, Inc. and others. All rights reserved.
+ * Copyright (c) 2014 - 2016 Red Hat, Inc. and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -17,6 +17,7 @@ public enum Service {
     DIRECTOR ((short) 10, "Director"),
     SFC_CLASSIFIER ((short) 10, "SFC Classifier"),
     ARP_RESPONDER ((short) 20, "Distributed ARP Responder"),
+    ICMP_ECHO ((short) 20, "Distributed ICMP Echo Responder"),
     INBOUND_NAT ((short) 30, "DNAT for inbound floating-ip traffic"),
     EGRESS_ACL ((short) 40, "Egress Acces-control"),
     LOAD_BALANCER ((short) 50, "Distributed LBaaS"),

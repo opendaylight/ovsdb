d35462c1bfa4984a1914f9936080bec68734a39b
Vishal Thapar <vishal.thapar@ericsson.com>
2016-03-09 23:30:06 +0530

    Bug 5480 - bfd not supported by SB plugin
    Schema definition for it is present in library but definition in
    ovsdb.yang and relevant code is missing.
    
    This fix adds yang model and related code for bfd cofiguration and status.
    
    Change-Id: I171478949079a4c39d8de9f0f7fb2bfcfb53e8b5
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/TerminationPointUpdateCommand.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/TerminationPointUpdateCommand.java
@@ -28,6 +28,7 @@ import org.opendaylight.ovsdb.utils.yang.YangUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.yang.types.rev130715.Uuid;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbPortInterfaceAttributes.VlanMode;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.InterfaceBfd;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.InterfaceExternalIds;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.InterfaceLldp;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.port._interface.attributes.InterfaceOtherConfigs;
@@ -113,6 +114,7 @@ public class TerminationPointUpdateCommand extends AbstractTransactCommand {
         updateInterfaceOtherConfig(terminationPoint, ovsInterface);
         updateInterfaceExternalIds(terminationPoint, ovsInterface);
         updateInterfaceLldp(terminationPoint, ovsInterface);
+        updateInterfaceBfd(terminationPoint, ovsInterface);
     }
 
     private void updatePort(
@@ -228,6 +230,29 @@ public class TerminationPointUpdateCommand extends AbstractTransactCommand {
             } catch (NullPointerException e) {
                 LOG.warn("Incomplete OVSDB interface other_config", e);
             }
+        }
+    }
+
+    private void updateInterfaceBfd(
+            final OvsdbTerminationPointAugmentation terminationPoint,
+            final Interface ovsInterface) {
+
+        try {
+            List<InterfaceBfd> interfaceBfdList =
+                    terminationPoint.getInterfaceBfd();
+            if (interfaceBfdList != null && !interfaceBfdList.isEmpty()) {
+                Map<String, String> interfaceBfdMap = new HashMap<>();
+                for (InterfaceBfd interfaceBfd : interfaceBfdList) {
+                    interfaceBfdMap.put(interfaceBfd.getBfdKey(), interfaceBfd.getBfdValue());
+                }
+                try {
+                    ovsInterface.setBfd(ImmutableMap.copyOf(interfaceBfdMap));
+                } catch (NullPointerException e) {
+                    LOG.warn("Incomplete OVSDB interface bfd", e);
+                }
+            }
+        } catch (SchemaVersionMismatchException e) {
+            LOG.debug("bfd column for Interface Table unsupported for this version of ovsdb schema", e);
         }
     }
 

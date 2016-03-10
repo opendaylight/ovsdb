078f3e527cf3ea0214cb22ed1c1dd8404e7a0510
Anil Vishnoi <vishnoianil@gmail.com>
2016-03-09 23:35:37 -0800

    Bug 5221 - passive connection not reconnected if ovs service is restarted
    Change-Id: I830aeeed4ba7e51adb0ad952c11fd1603d207b1b
    Signed-off-by: Anil Vishnoi <vishnoianil@gmail.com>

---
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/reconciliation/connection/ConnectionReconciliationTask.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.southbound.reconciliation.connection;
+
+import org.opendaylight.ovsdb.lib.OvsdbClient;
+import org.opendaylight.ovsdb.southbound.OvsdbConnectionManager;
+import org.opendaylight.ovsdb.southbound.reconciliation.ReconciliationManager;
+import org.opendaylight.ovsdb.southbound.reconciliation.ReconciliationTask;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.UnknownHostException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Anil Vishnoi (avishnoi@Brocade.com) on 3/9/16.
+ */
+public class ConnectionReconciliationTask extends ReconciliationTask {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ConnectionReconciliationTask.class);
+
+    private static final int RETRY_INTERVAL_FACTOR = 10000;
+    private static final int MAX_ATTEMPT = 10;
+
+    private AtomicInteger connectionAttempt = new AtomicInteger(0);
+
+    public ConnectionReconciliationTask(ReconciliationManager reconciliationManager, OvsdbConnectionManager
+            connectionManager, InstanceIdentifier<?> nodeIid, DataObject configData) {
+        super(reconciliationManager, connectionManager, nodeIid, configData);
+
+    }
+
+    @Override
+    public boolean reconcileConfiguration(OvsdbConnectionManager connectionManager) {
+        boolean result = false;
+        connectionAttempt.incrementAndGet();
+        InstanceIdentifier<Node> nIid = (InstanceIdentifier<Node>) nodeIid;
+        OvsdbNodeAugmentation ovsdbNode = (OvsdbNodeAugmentation)configData;
+
+        LOG.info("Retry({}) connection to Ovsdb Node {} ", connectionAttempt.get(), ovsdbNode.getConnectionInfo());
+        OvsdbClient client = null;
+        try {
+            client = connectionManager.connect(nIid, ovsdbNode);
+            if (client != null) {
+                LOG.info("Successfully connected to Ovsdb Node {} ", ovsdbNode.getConnectionInfo());
+                result = true;
+            } else {
+                LOG.warn("Connection retry({}) failed for {}.",
+                        connectionAttempt.get(), ovsdbNode.getConnectionInfo());
+            }
+        } catch (UnknownHostException e) {
+            LOG.warn("Connection retry({}) failed with exception. ",connectionAttempt.get(), e);
+        }
+        return result;
+    }
+
+    @Override
+    public void doRetry(boolean wasLastAttemptSuccessful) {
+
+        if( !wasLastAttemptSuccessful && connectionAttempt.get() <= MAX_ATTEMPT ) {
+            reconciliationManager.enqueueForRetry(ConnectionReconciliationTask.this);
+        } else {
+            reconciliationManager.dequeue(this);
+        }
+    }
+
+    @Override
+    public void checkReadinessAndProcess() {
+        reconciliationManager.enqueue(this);
+    }
+
+    @Override
+    public long retryDelayInMills() {
+        return connectionAttempt.get() * RETRY_INTERVAL_FACTOR;
+    }
+}

ad80aeb8871b39ff40e5995b4ada4606e6c14f88
Victor Pickard <vpickard@redhat.com>
2016-03-29 14:21:11 -0400

    WIP for neutron->netvirt port rendering.
    Patch set 4
    -----------
    Addresses comments from patch 3, mainly,
    renaming class INeutronDataProcessor to DataProcessor.
    
    Patch set 3 addresses comments from previous patch.
    
    New DTCL model, based on Stephen's change:
    
    Split listeners from data processors
    https://git.opendaylight.org/gerrit/#/c/37014/
    
    This patch supports add/delete/update for
    neutron ports.
    
    Change-Id: Icf616683f169e96d0e9307ea2b00b455e5c820ab
    Signed-off-by: Victor Pickard <vpickard@redhat.com>

---
+++ netvirt/renderers/neutron/src/main/java/org/opendaylight/ovsdb/netvirt/renderers/neutron/DataProcessor.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2013, 2016 Red Hat, Inc. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.ovsdb.netvirt.renderers.neutron;
+
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+
+/**
+ * netvirt data processor
+ * org.opendaylight.ovsdb.netvirt
+ */
+public interface DataProcessor<D extends DataObject> {
+
+    /**
+     * Method removes DataObject which is identified by InstanceIdentifier.
+     *
+     * @param identifier - the whole path to DataObject
+     * @param del - DataObject for removing
+     */
+    void remove(InstanceIdentifier<D> identifier, D del);
+
+    /**
+     * Method updates the original DataObject to the update DataObject.
+     * Both are identified by same InstanceIdentifier.
+     *
+     * @param identifier - the whole path to DataObject
+     * @param original - original DataObject (for update)
+     * @param update - changed DataObject (contain updates)
+     */
+    void update(InstanceIdentifier<D> identifier, D original, D update);
+
+    /**
+     * Method adds the DataObject which is identified by InstanceIdentifier
+     * to device.
+     *
+     * @param identifier - the whole path to new DataObject
+     * @param add - new DataObject
+     */
+    void add(InstanceIdentifier<D> identifier, D add);
+
+}
--- netvirt/renderers/neutron/src/test/java/org/opendaylight/ovsdb/netvirt/renderers/neutron/NeutronProviderTest.java
+++ netvirt/renderers/neutron/src/test/java/org/opendaylight/ovsdb/netvirt/renderers/neutron/NeutronProviderTest.java
@@ -9,17 +9,21 @@ package org.opendaylight.ovsdb.netvirt.renderers.neutron;
 
 import org.junit.Test;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 
 import static org.mockito.Mockito.mock;
 
 public class NeutronProviderTest {
     @Test
     public void testOnSessionInitiated() {
+
         NeutronProvider provider = new NeutronProvider();
 
         // ensure no exceptions
         // currently this method is empty
-        provider.onSessionInitiated(mock(BindingAwareBroker.ProviderContext.class));
+
+        //TODO
+        //provider.onSessionInitiated(mock(BindingAwareBroker.ProviderContext.class));
     }
 
     @Test

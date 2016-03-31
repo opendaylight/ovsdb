3c5e0b25b9a16364e17dd5154ba49d316eb7b0af
Stephen Kitt <skitt@redhat.com>
2016-03-31 16:58:00 +0200

    Use DataTreeChangeListener instead of DataChangeListener
    For now this patch sticks to the old approach, where we just listen
    for all Node events and rebuild the changes and re-extract them.
    
    Change-Id: I0f8e4ed09998a28326d39306cbb1b3486260e7f8
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/TransactInvoker.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/TransactInvoker.java
@@ -7,7 +7,11 @@
  */
 package org.opendaylight.ovsdb.southbound.ovsdb.transact;
 
+import java.util.Collection;
+
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.controller.md.sal.common.api.data.AsyncDataChangeEvent;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 
@@ -24,4 +28,14 @@ public interface TransactInvoker {
      */
     void invoke(TransactCommand command, BridgeOperationalState state,
                 AsyncDataChangeEvent<InstanceIdentifier<?>, DataObject> events);
+
+    /**
+     * Invoke the given transactional command, with the given bridge state, on the given modifications.
+     *
+     * @param command The transactional command.
+     * @param state The bridge state.
+     * @param modifications The modifications to be processed.
+     */
+    void invoke(TransactCommand command, BridgeOperationalState state,
+                Collection<DataTreeModification<Node>> modifications);
 }

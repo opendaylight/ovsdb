df672cf3e25ba0d10bc557f1f2deb5e556b32768
Stephen Kitt <skitt@redhat.com>
2016-04-01 17:10:07 +0200

    Split listeners from data processors
    Listening for changes is different from processing them, so separate
    the two concerns. This should make the data processors easier to
    test.
    
    Change-Id: I7a385b13c37ac2d02f12799042fc956084bd3670
    Signed-off-by: Stephen Kitt <skitt@redhat.com>
    Signed-off-by: Sam Hague <shague@redhat.com>

--- openstack/net-virt-sfc/impl/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/sfc/NetvirtSfcAclListener.java
+++ openstack/net-virt-sfc/impl/src/main/java/org/opendaylight/ovsdb/openstack/netvirt/sfc/NetvirtSfcAclListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2015 Dell, Inc. and others.  All rights reserved.
+ * Copyright © 2015, 2016 Dell, Inc. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,85 +8,25 @@
 
 package org.opendaylight.ovsdb.openstack.netvirt.sfc;
 
-import com.google.common.base.Preconditions;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeIdentifier;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
-import org.opendaylight.ovsdb.utils.mdsal.utils.MdsalUtils;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.AccessLists;
 import org.opendaylight.yang.gen.v1.urn.ietf.params.xml.ns.yang.ietf.access.control.list.rev150317.access.lists.Acl;
-import org.opendaylight.yangtools.concepts.ListenerRegistration;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Data tree listener for AccessList.
  */
-public class NetvirtSfcAclListener extends AbstractDataTreeListener<Acl> {
-    private static final Logger LOG = LoggerFactory.getLogger(NetvirtSfcAclListener.class);
-    private ListenerRegistration<NetvirtSfcAclListener> listenerRegistration;
-
+public class NetvirtSfcAclListener extends DelegatingDataTreeListener<Acl> {
     /**
      * {@link NetvirtSfcAclListener} constructor.
      * @param provider OpenFlow 1.3 Provider
      * @param db MdSal {@link DataBroker}
      */
     public NetvirtSfcAclListener(final INetvirtSfcOF13Provider provider, final DataBroker db) {
-        super(provider, Acl.class);
-        Preconditions.checkNotNull(db, "DataBroker can not be null!");
-
-        registrationListener(db);
-    }
-
-    private void registrationListener(final DataBroker db) {
-        final DataTreeIdentifier<Acl> treeId =
-                new DataTreeIdentifier<>(LogicalDatastoreType.CONFIGURATION, getIetfAclIid());
-        try {
-            LOG.info("Registering Data Change Listener for NetvirtSfc AccessList configuration.");
-            listenerRegistration = db.registerDataTreeChangeListener(treeId, this);
-        } catch (final Exception e) {
-            LOG.warn("Netvirt AccessList DataChange listener registration fail!", e);
-            throw new IllegalStateException("NetvirtSfcAccessListListener startup fail! System needs restart.", e);
-        }
-    }
-
-    @Override
-    public void close() {
-        if (listenerRegistration != null) {
-            try {
-                listenerRegistration.close();
-            } catch (final Exception e) {
-                LOG.warn("Error while stopping IETF ACL ChangeListener", e);
-            }
-            listenerRegistration = null;
-        }
-    }
-
-    @Override
-    public void remove(final InstanceIdentifier<Acl> identifier,
-                       final Acl change) {
-        Preconditions.checkNotNull(change, "Removed object can not be null!");
-        provider.removeClassifierRules(change);
-    }
-
-    @Override
-    public void update(final InstanceIdentifier<Acl> identifier,
-                       final Acl original, final Acl change) {
-        Preconditions.checkNotNull(original, "Updated original object can not be null!");
-        Preconditions.checkNotNull(original, "Updated update object can not be null!");
-        remove(identifier, original);
-        provider.addClassifierRules(change);
-    }
-
-    @Override
-    public void add(final InstanceIdentifier<Acl> identifier,
-                    final Acl change) {
-        Preconditions.checkNotNull(change, "Added object can not be null!");
-        provider.addClassifierRules(change);
-    }
-
-    public InstanceIdentifier<Acl> getIetfAclIid() {
-        return InstanceIdentifier.create(AccessLists.class).child(Acl.class);
+        super(provider, new NetvirtSfcAclDataProcessor(provider), db,
+                new DataTreeIdentifier<>(LogicalDatastoreType.CONFIGURATION,
+                        InstanceIdentifier.create(AccessLists.class).child(Acl.class)));
     }
 }

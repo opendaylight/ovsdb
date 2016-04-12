39468e3be40098d8bd80cb1baaab61b46da97231
Josh <jhershbe@redhat.com>
2016-04-12 13:50:27 +0200

    split it-utils: ovsdb-it-utils + netvirt-it-utils
    Change-Id: I5b512b65c3502431f919028a6383178c4f2dd140
    Signed-off-by: Josh <jhershbe@redhat.com>

--- utils/ovsdb-it-utils/src/main/java/org/opendaylight/ovsdb/utils/ovsdb/it/utils/ItConstants.java
+++ utils/ovsdb-it-utils/src/main/java/org/opendaylight/ovsdb/utils/ovsdb/it/utils/ItConstants.java
@@ -5,10 +5,11 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-package org.opendaylight.ovsdb.utils.it.utils;
+
+package org.opendaylight.ovsdb.utils.ovsdb.it.utils;
 
 /**
- * Constants for SouthboundIT
+ * Constants for SouthboundIT.
  */
 public final class ItConstants {
     private ItConstants() {

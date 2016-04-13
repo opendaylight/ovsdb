46b3c7cc3424d64e54568c8902cdbdaf0b22bb6c
Hsin-Yi Shen <syshen66@gmail.com>
2016-04-13 13:11:07 -0700

    Bug 5875 - OVSDB library: Retry when SSL handshake doesn't begin yet
    Sometimes the listener get notified before SSL handshake is done.
    The root cause is we don't handle the handshake state "NOT_HANDSHAKING" properly.
    "NOT_HANDSHAKING" could mean handshake process is finished or doesn't begin yet.
    We should handle latter case(handshake process doesn't begin yet) and retry before
     notifying lsteners.
    
    Signed-off-by: Hsin-Yi Shen <syshen66@gmail.com>
    Signed-off-by: Sam Hague <shague@redhat.com>

--- library/impl/src/main/java/org/opendaylight/ovsdb/lib/impl/OvsdbConnectionService.java
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/impl/OvsdbConnectionService.java
@@ -31,6 +31,7 @@ import io.netty.handler.timeout.IdleStateHandler;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLEngine;
 import javax.net.ssl.SSLEngineResult.HandshakeStatus;
+import javax.net.ssl.SSLPeerUnverifiedException;
 
 import java.net.InetAddress;
 import java.util.Arrays;
@@ -318,20 +319,26 @@ public class OvsdbConnectionService implements AutoCloseable, OvsdbConnection {
                     switch (status) {
                         case FINISHED:
                         case NOT_HANDSHAKING:
-                            //Handshake done. Notify listener.
-                            OvsdbClient client = getChannelClient(channel, ConnectionType.PASSIVE,
-                                                 Executors.newFixedThreadPool(NUM_THREADS));
-
                             if (sslHandler.engine().getSession().getCipherSuite()
-                                    .equals("SSL_NULL_WITH_NULL_NULL")
-                                    && status.equals(HandshakeStatus.NOT_HANDSHAKING)) {
+                                    .equals("SSL_NULL_WITH_NULL_NULL")) {
                                 // Not begin handshake yet. Retry later.
                                 LOG.debug("handshake not begin yet {}", status);
-                                executorService.schedule(this,  retryPeriod, TimeUnit.MILLISECONDS);
+                                executorService.schedule(this, retryPeriod, TimeUnit.MILLISECONDS);
                             } else {
-                                LOG.debug("Notify listener");
-                                for (OvsdbConnectionListener listener : connectionListeners) {
-                                    listener.connected(client);
+                              //Check if peer is trusted before notifying listeners
+                                try {
+                                    sslHandler.engine().getSession().getPeerCertificates();
+                                    //Handshake done. Notify listener.
+                                    OvsdbClient client = getChannelClient(channel, ConnectionType.PASSIVE,
+                                                         Executors.newFixedThreadPool(NUM_THREADS));
+                                    LOG.debug("Notify listener");
+                                    for (OvsdbConnectionListener listener : connectionListeners) {
+                                        listener.connected(client);
+                                    }
+                                } catch (SSLPeerUnverifiedException e) {
+                                    //Trust manager is still checking peer certificate. Retry later
+                                    LOG.debug("Peer certifiacte is not verified yet {}", status);
+                                    executorService.schedule(this, retryPeriod, TimeUnit.MILLISECONDS);
                                 }
                             }
                             break;

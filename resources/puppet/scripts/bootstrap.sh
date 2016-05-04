8c1aaa7eff4d05ca25528f0a8a2407a6ed516d48
Daniel Farrell <dfarrell@redhat.com>
2016-05-04 10:45:25 -0400

    Fix Puppet install in OVSDB Vagrantfile
    The Vagrantfile's initial shell provisioner call attempted to
    install Puppet via an old method that now fails. The repos
    for installing Puppet on Ubuntu seem to have changed.
    
    http://pastebin.com/6A4eBzM2
    
    Puppet is now included in the default repos, so a simple apt-get
    install works as expected.
    
    Change-Id: Id5bb8e4d9d966ac69397fc7ab877bc5495e2dce5
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- resources/puppet/scripts/bootstrap.sh
+++ resources/puppet/scripts/bootstrap.sh
@@ -31,18 +31,8 @@ apt-get update >/dev/null
 echo "Installing wget..."
 apt-get install -y wget >/dev/null
 
-# Install the PuppetLabs repo
-echo "Configuring PuppetLabs repo..."
-repo_deb_path=$(mktemp)
-wget --output-document="${repo_deb_path}" "${REPO_DEB_URL}" 2>/dev/null
-dpkg -i "${repo_deb_path}" >/dev/null
-apt-get update >/dev/null
-
 # Install Puppet
-echo "Installing Puppet..."
-DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install puppet >/dev/null
-
-echo "Puppet installed!"
+apt-get install -y puppet
 
 # Install RubyGems for the provider
 echo "Installing RubyGems..."

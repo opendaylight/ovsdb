2a5f7ba2949490f43febe886d79e36752dc96fbd
Vishal Thapar <vishal.thapar@ericsson.com>
2016-05-25 09:47:51 +0530

    BUG 5479: HWVtep Southbound doesn't retry connection
    This fix is hwvtep counterpart of
    https://git.opendaylight.org/gerrit/#/c/36028/ which fixes BUG 5221.
    
    Wiki:
    https://wiki.opendaylight.org/view/OVSDB_Integration:OVSDB_SB_Reconciliation
    
    Change-Id: I497dbfa339de157d12afe94ce5c36b35ffda5dd9
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>
    (cherry picked from commit 9d80362e33fb2d6a2c0dc4b7994c976e46e8799f)

---
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/reconciliation/ReconciliationTask.java
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.hwvtepsouthbound.reconciliation;
+
+import com.google.common.base.Preconditions;
+
+import org.opendaylight.ovsdb.hwvtepsouthbound.HwvtepConnectionManager;
+import org.opendaylight.ovsdb.hwvtepsouthbound.reconciliation.connection.ConnectionReconciliationTask;
+import org.opendaylight.yangtools.yang.binding.DataObject;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Copied from org.opendaylight.ovsdb.southbound.reconciliation.ReconciliationTask
+ *
+ * Abstract implementation of a reconciliation task. Each new type of
+ * resource configuration reconciliation task should extend this class
+ * and implement the abstract methods.
+ * Created by Anil Vishnoi (avishnoi@Brocade.com) on 3/9/16.
+ */
+public abstract class ReconciliationTask implements Runnable {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReconciliationTask.class);
+
+    protected final ReconciliationManager reconciliationManager;
+    protected final HwvtepConnectionManager connectionManager;
+    protected final InstanceIdentifier<?> nodeIid;
+    protected final DataObject configData;
+
+    protected ReconciliationTask(ReconciliationManager reconciliationManager, HwvtepConnectionManager connectionManager,
+                                 InstanceIdentifier<?> nodeIid,
+                                 DataObject configData) {
+        Preconditions.checkNotNull(reconciliationManager, "Reconciliation manager must not be null");
+        Preconditions.checkNotNull(connectionManager, "Connection manager must not be null");
+        Preconditions.checkNotNull(nodeIid, "Node Iid must not be null");
+        this.reconciliationManager = reconciliationManager;
+        this.connectionManager = connectionManager;
+        this.nodeIid = nodeIid;
+        this.configData = configData;
+    }
+
+    /**
+     * Method contains task reconciliation logic. Please refer to
+     * {@link ConnectionReconciliationTask#reconcileConfiguration(HwvtepConnectionManager)}
+     * for example.
+     * @param connectionManager Connection manager to get connection instance of the device
+     * @return True if reconciliation was successful, else false
+     */
+    public abstract boolean reconcileConfiguration(HwvtepConnectionManager connectionManager);
+
+    /**
+     * Extended task should implement the logic that decides whether retry for the task
+     * is required or not. If retry is required but it does not requires any delay, submit
+     * the task immediately using {@link ReconciliationManager#enqueue(ReconciliationTask)}.
+     * If retry requires delay, use {@link ReconciliationManager#enqueueForRetry(ReconciliationTask)}
+     * and specify the delay using {@link #retryDelayInMills()}.
+     * If data store operation is required to decide if the task need retry, please implement
+     * it as an async operation and submit the task on the callback of the future.
+     * <p>
+     * Note:Please do not write blocking data store operations
+     * {@link ConnectionReconciliationTask#doRetry(boolean)}
+     * </p>
+     * @param wasPreviousAttemptSuccessful Status of the previous attempt
+     */
+    public abstract void doRetry(boolean wasPreviousAttemptSuccessful);
+
+    /**
+     * Extended task should implement the logic that check the readiness of the task
+     * for execution. If task is ready for the execution, submit it for immediate
+     * execution using {@link ReconciliationManager#enqueue(ReconciliationTask)}.
+     * If task is not ready for execution yet, enqueue it again for delayed execution
+     * using {@link ReconciliationManager#enqueueForRetry(ReconciliationTask)}.
+     * To check the readiness of the task, if the data store operation is required, please
+     * implement it as an async operation and submit the task on the callback of the future.
+     * <p>
+     * Note:Please do not write blocking data store operations
+     * {@link ConnectionReconciliationTask#doRetry(boolean)}
+     * </p>
+     */
+    public abstract void checkReadinessAndProcess();
+
+    /**
+     * Method returns the time interval for retrying the failed task.
+     * {@link ReconciliationTask#doRetry(boolean)}
+     * @return time
+     */
+    public abstract long retryDelayInMills();
+
+    @Override
+    public void run() {
+        boolean status = this.reconcileConfiguration(connectionManager);
+        doRetry(status);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ReconciliationTask that = (ReconciliationTask) o;
+
+        return nodeIid.equals(that.nodeIid);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode() + nodeIid.hashCode();
+    }
+
+
+    @Override
+    public String toString() {
+        return "ReconciliationTask{ type=" + getClass().toString()+
+                ", nodeIid=" + nodeIid +
+                '}';
+    }
+}

9d80362e33fb2d6a2c0dc4b7994c976e46e8799f
Vishal Thapar <vishal.thapar@ericsson.com>
2016-05-25 09:47:51 +0530

    BUG 5479: HWVtep Southbound doesn't retry connection
    This fix is hwvtep counterpart of
    https://git.opendaylight.org/gerrit/#/c/36028/ which fixes BUG 5221.
    
    Wiki:
    https://wiki.opendaylight.org/view/OVSDB_Integration:OVSDB_SB_Reconciliation
    
    Change-Id: I497dbfa339de157d12afe94ce5c36b35ffda5dd9
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

---
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/reconciliation/ReconciliationTaskManager.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2016 Brocade Communications Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.hwvtepsouthbound.reconciliation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Future;
+
+/**
+ * Copied from org.opendaylight.ovsdb.southbound.reconciliation.ReconciliationTaskManager
+ *
+ * This class is a task cache manager that provides a cache to store
+ * the task that is queued for the reconciliation. Whenever new task
+ * is submitted to the reconciliation manager, it will be cached in
+ * the cache. If the reconciliation is successful or it's done with
+ * all the attempt of reconciliation,
+ *
+ * Caching of the task is required, because reconciliation task might
+ * require longer duration to reconcile and there is a possibility that
+ * meanwhile user can change the configuration in config data store while
+ * task is queued for the reconciliation. In that scenario, reconciliation
+ * manager should not attempt any further reconciliation attempt for that
+ * task. ReconciliationManager will call cancelTask() to cancel the task.
+ *
+ * Created by Anil Vishnoi (avishnoi@Brocade.com) on 3/15/16.
+ */
+class ReconciliationTaskManager {
+    private static final Logger LOG = LoggerFactory.getLogger(ReconciliationTaskManager.class);
+
+    private final ConcurrentHashMap<ReconciliationTask,Future<?>> reconciliationTaskCache
+            = new ConcurrentHashMap();
+
+    public boolean isTaskQueued(ReconciliationTask task) {
+        return reconciliationTaskCache.containsKey(task);
+    }
+    public boolean cancelTask(ReconciliationTask task) {
+        if(reconciliationTaskCache.containsKey(task)){
+            Future<?> taskFuture = reconciliationTaskCache.remove(task);
+            if( !taskFuture.isDone() && !taskFuture.isCancelled() ) {
+                LOG.info("Reconciliation task is cancelled : {}",task);
+                return taskFuture.cancel(true);
+            }
+        }
+        return false;
+
+    }
+    public void cacheTask(ReconciliationTask task, Future<?> taskFuture) {
+        reconciliationTaskCache.put(task,taskFuture);
+    }
+}

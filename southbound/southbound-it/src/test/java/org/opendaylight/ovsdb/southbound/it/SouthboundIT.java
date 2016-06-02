a5fe69d33619c911fc211d9e4ba14ae354bbd9f2
Rashmi Pujar <rpujar@inocybe.com>
2016-06-02 17:36:56 -0400

    [DO NOT MERGE} Bug 4790 - 'ifindex' column from Interface table on OVS
    To-Do items:
    - Test the impl
    - IT tests need to be updated
    
    Change-Id: I66f7a15a36a7c877cdeb3ca6accf761b16a7a962
    Signed-off-by: Rashmi Pujar <rpujar@inocybe.com>

--- southbound/southbound-it/src/test/java/org/opendaylight/ovsdb/southbound/it/SouthboundIT.java
+++ southbound/southbound-it/src/test/java/org/opendaylight/ovsdb/southbound/it/SouthboundIT.java
@@ -168,6 +168,7 @@ public class SouthboundIT extends AbstractMdsalTestBase {
     private static final int OVSDB_ROUNDTRIP_TIMEOUT = 10000;
     private static final String FORMAT_STR = "%s_%s_%d";
     private static final Version AUTOATTACH_FROM_VERSION = Version.fromString("7.11.2");
+    private static final Version IF_INDEX_FROM_VERSION = Version.fromString("7.2.0");
     private static String addressStr;
     private static int portNumber;
     private static String connectionType;
@@ -1274,6 +1275,46 @@ public class SouthboundIT extends AbstractMdsalTestBase {
             return null;
         }
         return tpList.get(index).getAugmentation(OvsdbTerminationPointAugmentation.class);
+    }
+
+    @Test
+    public void testCRUDTerminationPointIfIndex() throws InterruptedException {
+        final boolean isOldSchema = schemaVersion.compareTo(IF_INDEX_FROM_VERSION) < 0;
+        Assume.assumeFalse(isOldSchema);
+
+        final Long IF_INDEX_EXPECTED = 5000L;
+        ConnectionInfo connectionInfo = getConnectionInfo(addressStr, portNumber);
+
+        // Test create ifIndex
+        try (TestBridge testBridge = new TestBridge(connectionInfo, SouthboundITConstants.BRIDGE_NAME)) {
+            OvsdbBridgeAugmentation bridge = getBridge(connectionInfo);
+            Assert.assertNotNull(bridge);
+            LOG.info("bridge: {}", bridge);
+            NodeId nodeId = SouthboundMapper.createManagedNodeId(SouthboundUtils.createInstanceIdentifier(
+                    connectionInfo, bridge.getBridgeName()));
+            OvsdbTerminationPointAugmentationBuilder ovsdbTerminationBuilder =
+                    createGenericOvsdbTerminationPointAugmentationBuilder();
+            String portName = "testIfIndex";
+            ovsdbTerminationBuilder.setName(portName);
+
+            ovsdbTerminationBuilder.setIfindex(IF_INDEX_EXPECTED);
+            Assert.assertTrue(addTerminationPoint(nodeId, portName, ovsdbTerminationBuilder));
+            InstanceIdentifier<Node> terminationPointIid = getTpIid(connectionInfo, bridge);
+            Node terminationPointNode = mdsalUtils.read(LogicalDatastoreType.OPERATIONAL, terminationPointIid);
+            Assert.assertNotNull(terminationPointNode);
+
+            // Test read ifIndex
+            List<TerminationPoint> terminationPoints = terminationPointNode.getTerminationPoint();
+            for (TerminationPoint terminationPoint : terminationPoints) {
+                OvsdbTerminationPointAugmentation ovsdbTerminationPointAugmentation =
+                        terminationPoint.getAugmentation(OvsdbTerminationPointAugmentation.class);
+                if (ovsdbTerminationPointAugmentation.getName().equals(portName)) {
+                    Long ifIndex = ovsdbTerminationPointAugmentation.getIfindex();
+                    Assert.assertTrue(ifIndex.equals(IF_INDEX_EXPECTED));
+                    LOG.info("ofPort: {}", ifIndex);
+                }
+            }
+        }
     }
 
     @Test

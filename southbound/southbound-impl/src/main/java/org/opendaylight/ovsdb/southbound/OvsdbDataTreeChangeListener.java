6f14c17a3d4525a91e4fbb88cb13d09f163055a7
Sam Hague <shague@redhat.com>
2016-06-10 18:43:31 -0400

    Do not merge: test for csit port delete issues
    Change-Id: I1733a5a3adc5b9c0b796170db0cf526df5f37b74
    Signed-off-by: Sam Hague <shague@redhat.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbDataTreeChangeListener.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/OvsdbDataTreeChangeListener.java
@@ -8,7 +8,9 @@
 
 package org.opendaylight.ovsdb.southbound;
 
+import com.google.common.collect.Iterables;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -29,6 +31,7 @@ import org.opendaylight.ovsdb.southbound.ovsdb.transact.BridgeOperationalState;
 import org.opendaylight.ovsdb.southbound.ovsdb.transact.TransactCommandAggregator;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbBridgeAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbNodeAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.OvsdbTerminationPointAugmentation;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.ConnectionInfo;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
@@ -36,6 +39,7 @@ import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
 import org.opendaylight.yangtools.concepts.ListenerRegistration;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -194,29 +198,73 @@ public class OvsdbDataTreeChangeListener implements ClusteredDataTreeChangeListe
                     change.getRootNode().getModifiedAugmentation(OvsdbBridgeAugmentation.class);
             OvsdbConnectionInstance client = null;
             Node node = change.getRootNode().getDataAfter();
+
+            LOG.info("\nshague 1. modType: {} change: {}:::{},,,\n bridgeModification: {}:::{},,,\n node: {}:::{}",
+                    change.getRootNode().getModificationType(), change.getRootNode(), change.getRootPath(),
+                    bridgeModification != null ? bridgeModification.getDataBefore() : null,
+                    bridgeModification != null ? bridgeModification.getDataAfter() : null,
+                    change.getRootNode().getDataBefore(), change.getRootNode().getDataAfter());
             if (bridgeModification != null && bridgeModification.getDataAfter() != null) {
                 client = cm.getConnectionInstance(bridgeModification.getDataAfter());
+                LOG.info("\nshague 2. bridgeModification: {},,,\n bridge.getDataAfter: {}",
+                        bridgeModification, bridgeModification.getDataAfter());
             } else if (bridgeModification != null && bridgeModification.getDataBefore() != null &&
                     change.getRootNode().getModificationType() == DataObjectModification.ModificationType.DELETE) {
                 client = cm.getConnectionInstance(bridgeModification.getDataBefore());
+                LOG.info("\nshague 3. bridgeModification: {},,,\n bridge.getDataBefore: {}",
+                        bridgeModification, bridgeModification.getDataBefore());
             } else {
                 DataObjectModification<OvsdbNodeAugmentation> nodeModification =
                         change.getRootNode().getModifiedAugmentation(OvsdbNodeAugmentation.class);
+                LOG.info("\nshague 4. nodeModification: {}", nodeModification);
                 if (nodeModification != null && nodeModification.getDataAfter() != null && nodeModification
                         .getDataAfter().getConnectionInfo() != null) {
                     client = cm.getConnectionInstance(nodeModification.getDataAfter().getConnectionInfo());
+                    LOG.info("\nshague 5. nodeModification: {},\n node.GetDataAfter",
+                            nodeModification, nodeModification.getDataAfter());
                 } else if (nodeModification != null && nodeModification.getDataAfter() != null && nodeModification
                             .getDataAfter().getConnectionInfo() == null) {
                     InstanceIdentifier<Node> nodeIid = SouthboundMapper.createInstanceIdentifier(
-                            node.getNodeId());
+                            node != null ? node.getNodeId() : null);
                     client = cm.getConnectionInstance(nodeIid);
+                    LOG.info("\nshague 5. nodeModification: {},\n node.GetDataAfter",
+                            nodeModification, nodeModification.getDataAfter());
                 } else {
+                    LOG.info("\nshague 6.");
                     if (node != null) {
                         List<TerminationPoint> terminationPoints = node.getTerminationPoint();
+                        LOG.info("\nshague 7. node: {},\n terminationPoints: {}", node, terminationPoints);
                         if (terminationPoints != null && !terminationPoints.isEmpty()) {
                             InstanceIdentifier<Node> nodeIid = SouthboundMapper.createInstanceIdentifier(
                                     node.getNodeId());
                             client = cm.getConnectionInstance(nodeIid);
+                            LOG.info("\nshague 8.");
+                        }
+                    }
+                    if (client == null) {
+                        node = change.getRootNode().getDataBefore();
+                        if (node != null) {
+                            List<TerminationPoint> terminationPoints = node.getTerminationPoint();
+                            LOG.info("\nshague 7-2. node: {},\n terminationPoints: {}", node, terminationPoints);
+                            if (terminationPoints != null && !terminationPoints.isEmpty()) {
+                                InstanceIdentifier<Node> nodeIid = SouthboundMapper.createInstanceIdentifier(
+                                        node.getNodeId());
+                                client = cm.getConnectionInstance(nodeIid);
+                                LOG.info("\nshague 8-2.");
+                            }
+                        }
+                    }
+                    if (client == null) {
+                        InstanceIdentifier<Node> nodeIid ;
+                        node = change.getRootNode().getDataBefore() != null ? change.getRootNode().getDataBefore() :
+                                change.getRootNode().getDataAfter();
+                        LOG.info("\nshague 7-3. node: {}", node);
+                        if (node instanceof OvsdbBridgeAugmentation) {
+                            OvsdbBridgeAugmentation bridgeAugmentation = (OvsdbBridgeAugmentation) node;
+                            if (bridgeAugmentation.getManagedBy() != null) {
+                                nodeIid = (InstanceIdentifier<Node>) bridgeAugmentation.getManagedBy().getValue();
+                                client = cm.getConnectionInstance(nodeIid);
+                            }
                         }
                     }
                 }

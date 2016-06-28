b8196e4712a6eed28ecda9228e8ff62fa913484d
Vishal Thapar <vishal.thapar@ericsson.com>
2016-06-28 16:51:28 +0530

    Schema changes for 1.7.0 support
    This adds the following:
    1. ACL.java for ACL table
    2. ACLEntry.java for ACL_entries table
    3. Reference to ACL table entry in LogicalRouter
    4. Reference to ACL table entry in PhysicalPort
    5. Add tunnel-key to PhysicalLocator
    6. Add other-config to multiple classes as per 1.5.1 an 1.7.0
    7. Add replication_mode to LogicalSwitch
    
    Change-Id: I0af839a9792890d5a6b753b8e26fb3ffcc1678de
    Signed-off-by: Vishal Thapar <vishal.thapar@ericsson.com>

--- schemas/hardwarevtep/src/main/java/org/opendaylight/ovsdb/schema/hardwarevtep/PhysicalPort.java
+++ schemas/hardwarevtep/src/main/java/org/opendaylight/ovsdb/schema/hardwarevtep/PhysicalPort.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015 Red Hat, Inc. and others. All rights reserved.
+ * Copyright (c) 2014, 2016 Red Hat, Inc. and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -48,11 +48,25 @@ public interface PhysicalPort extends TypedBaseTable<GenericTableSchema> {
     void setVlanBindings(Map<Long, UUID> vlanBindings);
 
 
+    @TypedColumn(name="acl_bindings", method=MethodType.GETCOLUMN, fromVersion="1.4.0")
+    Column<GenericTableSchema, Map<Long, UUID>> getAclBindingsColumn();
+
+    @TypedColumn(name="acl_bindings", method=MethodType.SETDATA, fromVersion="1.4.0")
+    void setAclBindings(Map<Long, UUID> aclBindings);
+
+
     @TypedColumn(name="vlan_stats", method=MethodType.GETCOLUMN, fromVersion="1.0.0")
     Column<GenericTableSchema, Map<Long, UUID>> getVlanStatsColumn();
 
     @TypedColumn(name="vlan_stats", method=MethodType.SETDATA, fromVersion="1.0.0")
     void setVlanStats(Map<Long, UUID> vlanStats);
+
+
+    @TypedColumn(name="other_config", method=MethodType.GETCOLUMN, fromVersion="1.5.1")
+    Column<GenericTableSchema, Map<String, String>> getOtherConfigColumn();
+
+    @TypedColumn(name="other_config", method=MethodType.SETDATA, fromVersion="1.5.1")
+    void setOtherConfig(Map<String, String> otherConfig);
 
 
     @TypedColumn(name="port_fault_status", method=MethodType.GETCOLUMN, fromVersion="1.1.0")

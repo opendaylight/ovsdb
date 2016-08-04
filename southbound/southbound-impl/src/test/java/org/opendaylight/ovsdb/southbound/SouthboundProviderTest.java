84a01b44d5206652b67d85ef7ba6d12d440a448f
Robert Varga <rovarga@cisco.com>
2016-08-04 12:35:26 +0200

    Switch from yangtools to mdsal binding APIs
    This reflects the move of these interfaces from yangtools
    to mdsal.
    
    Change-Id: I5532deea7396104a4a850124f9a0ca21c8f275af
    Signed-off-by: Robert Varga <rovarga@cisco.com>

--- southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/SouthboundProviderTest.java
+++ southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/SouthboundProviderTest.java
@@ -38,7 +38,7 @@ import org.opendaylight.ovsdb.lib.OvsdbConnection;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NetworkTopology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.Topology;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.TopologyKey;
-import org.opendaylight.yangtools.binding.data.codec.api.BindingNormalizedNodeSerializer;
+import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.binding.KeyedInstanceIdentifier;
 

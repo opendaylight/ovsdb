6b16fc06de2ea18cbe3f2dd1cdfdd9c5d2917e49
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2016-08-16 23:02:52 +0530

    bug 6643 hwvtep configuration reconcilation
    Added operational node listener to trigger reconcillation
    listens for physical switch node added event and schedules the reconcillation.
    trigger transactions for changes in macs and logical switches and vlan bindings
    trigger transactions for removal of local ucast and mcast macs aswell
    so that to be deleted logical switches will not have any references
    cancel the queued reconcilation task if any upon receiving node disconnect event
    
    fixed some npes in mac remove commands
    run the logical switch remove command post mac entries remove command as
    those logical switches removed logical swithes have to be visible in the
    other remove commands
    
    validated the following test cases
    1) disconnect the device and push the config (logical switch and remote
    macs and vlan bindings ) and connect the device again
    2) disconnect the device and delete some confing and connect the device
    again
    
    Change-Id: I12d0692e4c8b040f261a9c2cc5cffc05bef96f0d
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>

---
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/reconciliation/configuration/SwitchConfigOperationalChangeGetter.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2016 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.hwvtepsouthbound.reconciliation.configuration;
+
+import com.google.common.collect.Lists;
+import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepPhysicalPortAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepPhysicalPortAugmentationBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.PhysicalSwitchAugmentation;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.NodeBuilder;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPoint;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.node.TerminationPointBuilder;
+import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SwitchConfigOperationalChangeGetter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SwitchConfigOperationalChangeGetter.class);
+
+    public static DataTreeModification<Node> getModification(InstanceIdentifier<Node> psNodeId,
+                                                             Node configNode, Node operationalNode) {
+
+        NodeBuilder newNodeBuilder = getNodeBuilderFromNode(configNode);
+        NodeBuilder oldNodeBuilder = getNodeBuilderFromNode(operationalNode);
+
+        List<TerminationPoint> tpList = getPorts(configNode);
+        if (tpList.size() > 0) {
+            newNodeBuilder.setTerminationPoint(tpList);
+        }
+
+        tpList = getPorts(operationalNode);
+        if (tpList.size() > 0) {
+            oldNodeBuilder.setTerminationPoint(tpList);
+        }
+
+        return new DataTreeModificationImpl<Node>(psNodeId, newNodeBuilder.build(), oldNodeBuilder.build());
+
+    }
+
+
+    static NodeBuilder getNodeBuilderFromNode(Node node) {
+        NodeBuilder newNodeBuilder;
+        if (node != null) {
+            newNodeBuilder = new NodeBuilder(node);
+            newNodeBuilder.removeAugmentation(PhysicalSwitchAugmentation.class);
+        } else {
+            newNodeBuilder = new NodeBuilder();
+        }
+        List<TerminationPoint> emptyList = Lists.newArrayList();
+        newNodeBuilder.setTerminationPoint(emptyList);
+
+        return newNodeBuilder;
+    }
+
+
+    static List<TerminationPoint> getPorts(Node node) {
+        ArrayList<TerminationPoint> tpList = Lists.newArrayList();
+        if (node == null || node.getTerminationPoint() == null) {
+            return tpList;
+        }
+        for (TerminationPoint tp: node.getTerminationPoint()) {
+            TerminationPointBuilder terminationPointBuilder = new TerminationPointBuilder(tp);
+            terminationPointBuilder.removeAugmentation(HwvtepPhysicalPortAugmentation.class);
+
+            HwvtepPhysicalPortAugmentation augmentation = tp.getAugmentation(HwvtepPhysicalPortAugmentation.class);
+            HwvtepPhysicalPortAugmentationBuilder builder = new HwvtepPhysicalPortAugmentationBuilder();
+            if (augmentation != null) {
+                builder = new HwvtepPhysicalPortAugmentationBuilder(augmentation);
+            }
+            if (augmentation != null && augmentation.getVlanBindings() != null && !augmentation.getVlanBindings().isEmpty() ) {
+                builder.setVlanBindings(augmentation.getVlanBindings());
+                terminationPointBuilder.addAugmentation(HwvtepPhysicalPortAugmentation.class, builder.build());
+                tpList.add(terminationPointBuilder.build());
+            }
+        }
+        return tpList;
+    }
+}

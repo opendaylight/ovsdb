5ef7a5b0a08b45004005f676fb2e7d0cf75f99d2
Alexis de Talhouët <adetalhouet@inocybe.com>
2016-09-13 16:59:04 -0400

    Add southbound-cli bundle scaffholding
    Change-Id: Ib198916096e01e18d88c71c236e02c26adb55e91
    Signed-off-by: Alexis de Talhouët <adetalhouet@inocybe.com>

---
+++ southbound/southbound-cli/src/main/java/org/opendaylight/ovsdb/cli/api/SouthboundCliCommands.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright © 2016 Inocybe Technologies and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.cli.api;
+
+public interface SouthboundCliCommands {
+
+    /**
+     * Define the Karaf command method signatures and the Javadoc for each.
+     * Below method is just an example
+     */
+    Object testCommand(Object testArgument);
+}

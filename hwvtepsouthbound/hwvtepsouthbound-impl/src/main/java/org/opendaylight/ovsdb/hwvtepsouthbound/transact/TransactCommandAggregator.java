92372d2c515232120bdb926a693343803fa4a57c
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2017-02-05 11:26:07 +0530

    bug 7599 performance improvement for ucast macs
    Using the getModifiedChildren api on the modification to identify the
    modified children data under node.
    
    updated data and deleted data is stored in operational state for access
    by all the commands.
    
    Added the following accessor methods in operational state.
    List<? extends Identifiable> getUpdatedData(InstanceIdentifier<Node> key,
    Class<? extends Identifiable> cls)
    List<? extends Identifiable> getDeletedData(InstanceIdentifier<Node> key,
    Class<? extends Identifiable> cls)
    
    Change-Id: I76584d7c90323313da35e28a600019c05081f9d0
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transact/TransactCommandAggregator.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transact/TransactCommandAggregator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 China Telecom Beijing Research Institute and others.  All rights reserved.
+ * Copyright (c) 2015, 2017 China Telecom Beijing Research Institute and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -8,21 +8,45 @@
 
 package org.opendaylight.ovsdb.hwvtepsouthbound.transact;
 
+import org.apache.commons.lang3.tuple.Pair;
+import org.opendaylight.controller.md.sal.binding.api.DataObjectModification;
 import org.opendaylight.controller.md.sal.binding.api.DataTreeModification;
 import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.HwvtepGlobalAugmentation;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.hwvtep.rev150901.PhysicalSwitchAugmentation;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.network.topology.topology.Node;
+import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.Identifiable;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class TransactCommandAggregator implements TransactCommand {
 
+    private static final Logger LOG = LoggerFactory.getLogger(TransactCommandAggregator.class);
+
     private List<TransactCommand> commands = new ArrayList<TransactCommand>();
+    private final HwvtepOperationalState operationalState;
+    /* stores the modified and deleted data for each child type of each node id
+       Map<nodeid , Pair < updated, deleted >
+       each updated/ deleted contains Map < child type, List<ChildData>>
+       child type is the child of hwvtep Global augmentation
+     */
+    private final Map<InstanceIdentifier<Node>,
+            Pair<Map<Class<? extends Identifiable>, List<Identifiable>>,
+                Map<Class<? extends Identifiable>, List<Identifiable>>>> modifiedData = new HashMap<>();
+
 
     public TransactCommandAggregator(HwvtepOperationalState state, Collection<DataTreeModification<Node>> changes) {
+        this.operationalState = state;
+        onDataTreeChanged(changes);
         commands.add(new PhysicalSwitchUpdateCommand(state,changes));
         commands.add(new PhysicalSwitchRemoveCommand(state,changes));
         commands.add(new LogicalSwitchUpdateCommand(state,changes));
@@ -58,5 +82,98 @@ public class TransactCommandAggregator implements TransactCommand {
     public void doDeviceTransaction(TransactionBuilder transaction, InstanceIdentifier nodeIid, Identifiable data,
                                     InstanceIdentifier key,
                                     Object... extraData) {
+    }
+
+    private void onDataTreeChanged(final Collection<DataTreeModification<Node>> changes) {
+        for (DataTreeModification<Node> change : changes) {
+            final InstanceIdentifier<Node> key = change.getRootPath().getRootIdentifier();
+            final DataObjectModification<Node> mod = change.getRootNode();
+            final Map<Class<? extends Identifiable>, List<Identifiable>> updatedData = new HashMap<>();
+            final Map<Class<? extends Identifiable>, List<Identifiable>> deletedData = new HashMap<>();
+            extractDataChanged(key, mod, updatedData, deletedData);
+            modifiedData.put(key, Pair.of(updatedData, deletedData));
+            operationalState.setModifiedData(modifiedData);
+        }
+    }
+
+    private void extractDataChanged(final InstanceIdentifier<Node> key,
+                                    final DataObjectModification<Node> mod,
+                                    final Map<Class<? extends Identifiable>, List<Identifiable>> updatedData,
+                                    final Map<Class<? extends Identifiable>, List<Identifiable>> deletedData) {
+
+        extractDataChanged(mod.getModifiedChildren(), updatedData, deletedData);
+        DataObjectModification<HwvtepGlobalAugmentation> aug = mod.getModifiedAugmentation(
+                HwvtepGlobalAugmentation.class);
+        if (aug != null && getModificationType(aug) != null) {
+            extractDataChanged(aug.getModifiedChildren(), updatedData, deletedData);
+        }
+        DataObjectModification<PhysicalSwitchAugmentation> psAug = mod.getModifiedAugmentation(
+                PhysicalSwitchAugmentation.class);
+        if (psAug != null && getModificationType(psAug) != null) {
+            extractDataChanged(psAug.getModifiedChildren(), updatedData, deletedData);
+        }
+    }
+
+    private void extractDataChanged(final Collection<DataObjectModification<? extends DataObject>> children,
+                                    final Map<Class<? extends Identifiable>, List<Identifiable>> updatedData,
+                                    final Map<Class<? extends Identifiable>, List<Identifiable>> deletedData) {
+        if (children == null) {
+            return;
+        }
+        for (DataObjectModification<? extends DataObject> child : children) {
+            DataObjectModification.ModificationType type = getModificationType(child);
+            if (type == null) {
+                continue;
+            }
+            InstanceIdentifier instanceIdentifier = null;
+            Class<? extends Identifiable> childClass = (Class<? extends Identifiable>) child.getDataType();
+            InstanceIdentifier.PathArgument pathArgument = child.getIdentifier();
+            switch (type) {
+                case WRITE:
+                case SUBTREE_MODIFIED:
+                    DataObject dataAfter = child.getDataAfter();
+                    if (!(dataAfter instanceof Identifiable)) {
+                        continue;
+                    }
+                    DataObject before = child.getDataBefore();
+                    if (Objects.equals(dataAfter, before)) {
+                        /*
+                        in cluster reboot scenarios,
+                        application rewrites the data tx.put( logicalswitchiid, logicalswitch )
+                        that time it fires the update again ignoring such updates here
+                         */
+                        continue;
+                    }
+                    Identifiable identifiable = (Identifiable) dataAfter;
+                    addToUpdatedData(updatedData, childClass, identifiable);
+                    break;
+                case DELETE:
+                    Identifiable dataBefore = (Identifiable) child.getDataBefore();
+                    if (!(dataBefore instanceof Identifiable)) {
+                        continue;
+                    }
+                    addToUpdatedData(deletedData, childClass, dataBefore);
+                    break;
+            }
+        }
+    }
+
+    private void addToUpdatedData(Map<Class<? extends Identifiable>, List<Identifiable>> updatedData,
+                                  Class<? extends Identifiable> childClass, Identifiable identifiable) {
+        if (!updatedData.containsKey(childClass)) {
+            updatedData.put(childClass, new ArrayList<>());
+        }
+        updatedData.get(childClass).add(identifiable);
+    }
+
+    private DataObjectModification.ModificationType getModificationType(
+            DataObjectModification<? extends DataObject> mod) {
+        try {
+            return mod.getModificationType();
+        } catch (IllegalStateException e) {
+            //not sure why this getter throws this exception, could be some mdsal bug
+            LOG.warn("Failed to get the modification type for mod {}", mod);
+        }
+        return null;
     }
 }

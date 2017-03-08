a4963b69460bab97da759bcdf93f11c04ee76cf1
Stephen Kitt <skitt@redhat.com>
2017-03-08 11:46:18 +0100

    Bug 8055: remove unnecessary type parameters
    Change-Id: I8baabd6823a1876c84db323e56c120c8e2474629
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/TyperUtils.java
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/TyperUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, 2015 Red Hat, Inc. and others. All rights reserved.
+ * Copyright © 2014, 2017 Red Hat, Inc. and others. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -230,7 +230,7 @@ public class TyperUtils {
      * @param klazz Typed Class that represents a Table
      */
     public static <T> T getTypedRowWrapper(final DatabaseSchema dbSchema, final Class<T> klazz) {
-        return getTypedRowWrapper(dbSchema, klazz,new Row<GenericTableSchema>());
+        return getTypedRowWrapper(dbSchema, klazz, new Row<>());
     }
 
     /**

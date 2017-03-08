f913deadff08f1803b60630ee4b138cbb86c31cc
Stephen Kitt <skitt@redhat.com>
2017-03-08 11:46:18 +0100

    Bug 8055: remove unnecessary type parameters
    Change-Id: I8baabd6823a1876c84db323e56c120c8e2474629
    Signed-off-by: Stephen Kitt <skitt@redhat.com>

--- southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/ProtocolRemovedCommandTest.java
+++ southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/ProtocolRemovedCommandTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Inocybe Technologies. and others.  All rights reserved.
+ * Copyright © 2015, 2017 Inocybe Technologies. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -87,7 +87,7 @@ public class ProtocolRemovedCommandTest {
         Bridge bridge = mock(Bridge.class);
         when(bridge.getProtocolsColumn()).thenReturn(column);
         when(column.getSchema()).thenReturn(mock(ColumnSchema.class));
-        when(column.getData()).thenReturn(new HashSet<String>());
+        when(column.getData()).thenReturn(new HashSet<>());
         when(mutate.addMutation(any(ColumnSchema.class), any(Mutator.class), any(Set.class))).thenReturn(mutate);
 
         PowerMockito.mockStatic(TyperUtils.class);

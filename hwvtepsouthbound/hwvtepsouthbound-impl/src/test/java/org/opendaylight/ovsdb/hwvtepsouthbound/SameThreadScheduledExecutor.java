8f01bcd183e79f47e68abce3ce07c09165f00976
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2017-08-11 14:53:56 +0530

    bug 8029 handle expired in transit entries
    when ref count goes to zero , locator is kept in transit state.
    
    if the device still has references to it from other sources like tunnels
    the locator will not be deleted from device.
    
    The transactions which wait for this locator will expire.
    
    If the intransit state expires , query the device itself to get its
    status and proceed with the transactions which depends on this locator.
    
    added a timer job which triggers every 30sec and checks for expired
    intransit keys and clears them.
    
    Change-Id: I0022da4a3a2e748629bc031246f6e6c1509d27a4
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
    (cherry picked from commit a7637041f73ce8438566702222e22030cde9e5b0)

---
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/test/java/org/opendaylight/ovsdb/hwvtepsouthbound/SameThreadScheduledExecutor.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.hwvtepsouthbound;
+
+import com.google.common.util.concurrent.SettableFuture;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+public abstract class SameThreadScheduledExecutor implements ScheduledExecutorService {
+
+    @Override
+    public Future<?> submit(Runnable runnable) {
+        runnable.run();
+        SettableFuture ft = SettableFuture.create();
+        ft.set(null);
+        return ft;
+    }
+}

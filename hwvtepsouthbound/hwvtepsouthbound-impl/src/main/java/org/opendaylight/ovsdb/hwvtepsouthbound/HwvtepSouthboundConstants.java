146612166fb97830da94590999939adfb98f63f8
K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>
2017-08-11 16:14:08 +0530

    bug 7599 avoid unnecessary mdsal reads
    avoid the un necessary reads.
    skip monitoring version column
    
    Change-Id: I82e50b973a93c57eb65dd34c6dd624c8e47e2344
    Signed-off-by: K.V Suneelu Verma <k.v.suneelu.verma@ericsson.com>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/HwvtepSouthboundConstants.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/HwvtepSouthboundConstants.java
@@ -44,8 +44,9 @@ public class HwvtepSouthboundConstants {
 
     public static final ImmutableMap<String,List<String>> SKIP_COLUMN_FROM_HWVTEP_TABLE
             = new ImmutableMap.Builder<String,List<String>>()
-            .put("Manager", Arrays.asList("_version", "status"))
+            .put("Manager", Arrays.asList(VERSION_COLUMN, "status"))
             .build();
+    public static final String VERSION_COLUMN = "_version";
     public static final int WAITING_QUEUE_CAPACITY = 1000;
     public static final long WAITING_JOB_EXPIRY_TIME_MILLIS = 90000;
     public static final long IN_TRANSIT_STATE_EXPIRY_TIME_MILLIS = 10000;

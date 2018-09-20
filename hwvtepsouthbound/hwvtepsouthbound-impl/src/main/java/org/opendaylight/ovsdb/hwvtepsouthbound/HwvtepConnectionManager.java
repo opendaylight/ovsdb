c1e3986fb3f5094a3fad8dd4a93dd997915b1e63
Vishal Thapar <vthapar@redhat.com>
2018-09-20 20:38:36 +0530

    OVSDB Neon MRI [wip]
    TODO:
    1. Mockito changes
    
    Change-Id: I46d935fed3ef44dede188ffd25295586010cc8a2
    Signed-off-by: Vishal Thapar <vthapar@redhat.com>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/HwvtepConnectionManager.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/HwvtepConnectionManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015, 2017 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
+ * Copyright (c) 2015, 2018 Ericsson India Global Services Pvt Ltd. and others.  All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
@@ -370,7 +370,7 @@ public class HwvtepConnectionManager implements OvsdbConnectionListener, AutoClo
     private void handleOwnershipState(Entity candidateEntity, HwvtepConnectionInstance hwvtepConnectionInstance) {
         //If entity already has owner, it won't get notification from EntityOwnershipService
         //so cache the connection instances.
-        Optional<EntityOwnershipState> ownershipStateOpt =
+        java.util.Optional<EntityOwnershipState> ownershipStateOpt =
                 entityOwnershipService.getOwnershipState(candidateEntity);
         if (ownershipStateOpt.isPresent()) {
             EntityOwnershipState ownershipState = ownershipStateOpt.get();

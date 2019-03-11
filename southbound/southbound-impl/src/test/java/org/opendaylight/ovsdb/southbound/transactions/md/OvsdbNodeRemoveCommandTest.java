17adafec2040e523b1eff31bbc049492ab90d8b8
Robert Varga <robert.varga@pantheon.tech>
2019-03-11 15:18:49 +0100

    Upgrade powermock to 2.0.0
    This upgrades powermock to version 2.0.0.
    
    JIRA: OVSDB-476
    Change-Id: I54beb2db411cff0e126c8e8208ff9ee1530437ab
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbNodeRemoveCommandTest.java
+++ southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbNodeRemoveCommandTest.java
@@ -9,7 +9,7 @@
 package org.opendaylight.ovsdb.southbound.transactions.md;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;

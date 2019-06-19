c726a1d445cd6e6a84c70cbb50862ee23e3ae8f4
Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>
2019-06-19 11:42:32 +0530

    Avoid Bulk Topology Read During Reconciliation.
    Description:
    During Reconiliation, The full config network-topology DS is queried
    which is not efficient. Currently, only br-int is managed in
    network-toplogy config DS(by genius) which will only be resynced.
    
    Changes are done to query only the br-int Node from topology config
    instead of full topology.
    
    JIRA: OVSDB-459
    
    Change-Id: Ife8055bcc10bcb6deb2fd8e0b5444bdf194f1460
    Signed-off-by: Chetan Arakere Gowdru <chetan.arakere@altencalsoftlabs.com>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/SouthboundProviderConfigurator.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/SouthboundProviderConfigurator.java
@@ -7,7 +7,10 @@
  */
 package org.opendaylight.ovsdb.southbound;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.StringTokenizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,6 +24,7 @@ public class SouthboundProviderConfigurator {
     private static final Logger LOG = LoggerFactory.getLogger(SouthboundProviderConfigurator.class);
 
     private static final String SKIP_MONITORING_MANAGER_STATUS_PARAM = "skip-monitoring-manager-status";
+    private static final String BRIDGES_RECONCILIATION_LIST_PARAM = "bridges-reconciliation-list";
 
     private final SouthboundProvider southboundProvider;
 
@@ -32,6 +36,15 @@ public class SouthboundProviderConfigurator {
         southboundProvider.setSkipMonitoringManagerStatus(flag);
     }
 
+    public void setBridgesReconciliationList(String bridgeListStr) {
+        List<String> bridgeList = new ArrayList<>();
+        StringTokenizer tokenizer = new StringTokenizer(bridgeListStr, ",");
+        while (tokenizer.hasMoreTokens()) {
+            bridgeList.add(tokenizer.nextToken());
+        }
+        southboundProvider.setBridgesReconciliationList(bridgeList);
+    }
+
     public void updateConfigParameter(Map<String, Object> configParameters) {
         if (configParameters != null && !configParameters.isEmpty()) {
             LOG.debug("Config parameters received : {}", configParameters.entrySet());
@@ -39,7 +52,8 @@ public class SouthboundProviderConfigurator {
                 if (paramEntry.getKey().equalsIgnoreCase(SKIP_MONITORING_MANAGER_STATUS_PARAM)) {
                     southboundProvider
                             .setSkipMonitoringManagerStatus(Boolean.parseBoolean((String) paramEntry.getValue()));
-                    break;
+                } else if (paramEntry.getKey().equalsIgnoreCase(BRIDGES_RECONCILIATION_LIST_PARAM)) {
+                    setBridgesReconciliationList((String)paramEntry.getValue());
                 }
             }
         }

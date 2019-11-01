b739deddf9baee8eae847465f13e1ae53ecb0814
xcheara <chetan.arakere@altencalsoftlabs.com>
2019-11-02 04:44:55 +0530

    Naming Anonymous Threads in OVSDB
    JIRA : OVSDB-454
    
    Description:
    There are few Anonymous Threads created in OVSDB. It will be difficult to
    identify which component created these threads based on the logs
    available.
    
    Changes done to provide proper Name to these Threads when created which
    helps to easily identify the component owner of such threads.
    
    Signed-off-by: xcheara <chetan.arakere@altencalsoftlabs.com>
    Change-Id: I061abd449f81c7cc2c7c847cb1a0f82d62d7e654
    Signed-off-by: xcheara <chetan.arakere@altencalsoftlabs.com>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/HwvtepConnectionInstance.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/HwvtepConnectionInstance.java
@@ -14,6 +14,7 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.SettableFuture;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -77,7 +78,9 @@ public class HwvtepConnectionInstance {
     private final HwvtepDeviceInfo deviceInfo;
     private final DataBroker dataBroker;
     private final HwvtepConnectionManager hwvtepConnectionManager;
-    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private static final ScheduledExecutorService scheduledExecutorService = Executors
+        .newSingleThreadScheduledExecutor(new ThreadFactoryBuilder()
+        .setNameFormat("hwvtep-reconciliation-ft-%d").build());
     @VisibleForTesting
     final SettableFuture<Boolean> reconciliationFt = SettableFuture.create();
     @VisibleForTesting

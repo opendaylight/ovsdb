cd57e8b5a3fd1fcc17377b00eed50941db044c2d
Robert Varga <robert.varga@pantheon.tech>
2019-11-29 11:18:13 +0100

    Add generated serialVersionUUID to exceptions
    Exceptions are serializable and therefore should have a serial
    version. Fixes Eclipse warnings pointing this out.
    
    Change-Id: I2cf04412709cf3214b53aa3ac5dc40f332f67295
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 8b8f15eaed3f4ff2e4acba53e04f2c34b6861e2f)

--- library/impl/src/main/java/org/opendaylight/ovsdb/lib/error/TyperException.java
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/error/TyperException.java
@@ -5,19 +5,19 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.ovsdb.lib.error;
 
 /**
  * This is a generic exception thrown by the Typed Schema utilities.
  */
 public class TyperException extends RuntimeException {
+    private static final long serialVersionUID = 5464754787320848910L;
 
-    public TyperException(String message) {
+    public TyperException(final String message) {
         super(message);
     }
 
-    public TyperException(String message, Throwable cause) {
+    public TyperException(final String message, final Throwable cause) {
         super(message, cause);
     }
 }

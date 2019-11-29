b48d21e5933e724cd44a81527930564b02b8ae87
Robert Varga <robert.varga@pantheon.tech>
2019-11-29 16:27:50 +0100

    Split out BaseTypeFactories
    Instantiating BaseTypes with complex definition is a prime example
    of where either a builder or a factory pattern is appropriate. Given
    we are dealing with a few types with very few customizations, this
    patch applies the factory pattern to the problem.
    
    This makes all BaseType subclasses properly immutable and has the
    benefit of squashing instances to singletons when they are not
    otherwise customized.
    
    Change-Id: I86d09324624c1e5b1a73a0be31269f2233fc15b3
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/BaseTypeFactory.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.lib.schema;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+abstract class BaseTypeFactory<T extends BaseType<T>> {
+
+    abstract T create(JsonNode typeDefinition);
+}

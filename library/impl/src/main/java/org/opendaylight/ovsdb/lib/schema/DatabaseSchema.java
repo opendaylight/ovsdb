2d789da44333849b2bb2a93fbf7ae22abbbcfb29
Robert Varga <robert.varga@pantheon.tech>
2019-11-30 11:04:23 +0100

    Turn DatabaseSchema into an interface
    This moves all the implementation details out into a separate class,
    allowing DatabaseSchema interface to be extended with additional
    functionality.
    
    Change-Id: Ib7a0124993cb89bb448b3c08bda6c6de0fea1dde
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/DatabaseSchema.java
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/DatabaseSchema.java
@@ -5,121 +5,29 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.ovsdb.lib.schema;
 
-import static java.util.Objects.requireNonNull;
-
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Maps;
-import com.google.common.reflect.Invokable;
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Set;
-import org.opendaylight.ovsdb.lib.error.ParsingException;
 import org.opendaylight.ovsdb.lib.notation.Version;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * Represents an ovsdb database schema, which is comprised of a set of tables.
  */
-public class DatabaseSchema {
-    private static final Logger LOG = LoggerFactory.getLogger(DatabaseSchema.class);
+public interface DatabaseSchema {
+    Set<String> getTables();
 
-    private final String name;
-    private final Version version;
-    private final ImmutableMap<String, TableSchema> tables;
+    boolean hasTable(String table);
 
-    public DatabaseSchema(final String name, final Version version, final Map<String, TableSchema> tables) {
-        this.name = requireNonNull(name);
-        this.version = requireNonNull(version);
-        this.tables = ImmutableMap.copyOf(tables);
-    }
+    <E extends TableSchema<E>> E table(String tableName, Class<E> clazz);
 
-    public Set<String> getTables() {
-        return tables.keySet();
-    }
+    String getName();
 
-    public boolean hasTable(final String table) {
-        return tables.containsKey(table);
-    }
+    Version getVersion();
 
-    public <E extends TableSchema<E>> E table(final String tableName, final Class<E> clazz) {
-        TableSchema<E> table = tables.get(tableName);
+    DatabaseSchema withInternallyGeneratedColumns();
 
-        if (clazz.isInstance(table)) {
-            return clazz.cast(table);
-        }
-
-        return createTableSchema(clazz, table);
-    }
-
-    protected <E extends TableSchema<E>> E createTableSchema(final Class<E> clazz, final TableSchema<E> table) {
-        Constructor<E> declaredConstructor;
-        try {
-            declaredConstructor = clazz.getDeclaredConstructor(TableSchema.class);
-        } catch (NoSuchMethodException e) {
-            String message = String.format("Class %s does not have public constructor that accepts TableSchema object",
-                    clazz);
-            throw new IllegalArgumentException(message, e);
-        }
-        Invokable<E, E> invokable = Invokable.from(declaredConstructor);
-        try {
-            return invokable.invoke(null, table);
-        } catch (InvocationTargetException | IllegalAccessException e) {
-            String message = String.format("Not able to create instance of class %s using public constructor "
-                    + "that accepts TableSchema object", clazz);
-            throw new IllegalArgumentException(message, e);
-        }
-    }
-
-    //todo : this needs to move to a custom factory
-    public static DatabaseSchema fromJson(final String dbName, final JsonNode json) {
-        if (!json.isObject() || !json.has("tables")) {
-            throw new ParsingException("bad DatabaseSchema root, expected \"tables\" as child but was not found");
-        }
-        if (!json.isObject() || !json.has("version")) {
-            throw new ParsingException("bad DatabaseSchema root, expected \"version\" as child but was not found");
-        }
-
-        Version dbVersion = Version.fromString(json.get("version").asText());
-
-        Map<String, TableSchema> tables = new HashMap<>();
-        for (Iterator<Map.Entry<String, JsonNode>> iter = json.get("tables").fields(); iter.hasNext(); ) {
-            Map.Entry<String, JsonNode> table = iter.next();
-            LOG.trace("Read schema for table[{}]:{}", table.getKey(), table.getValue());
-
-            //todo : this needs to done by a factory
-            tables.put(table.getKey(), GenericTableSchema.fromJson(table.getKey(), table.getValue()));
-        }
-
-        return new DatabaseSchema(dbName, dbVersion, tables);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Version getVersion() {
-        return version;
-    }
-
-    public DatabaseSchema withInternallyGeneratedColumns() {
-        return haveInternallyGeneratedColumns() ? this : new DatabaseSchema(name, version,
-            Maps.transformValues(tables, TableSchema::withInternallyGeneratedColumns));
-    }
-
-    protected final boolean haveInternallyGeneratedColumns() {
-        for (TableSchema tableSchema : tables.values()) {
-            if (!tableSchema.haveInternallyGeneratedColumns()) {
-                return false;
-            }
-        }
-        return true;
+    static DatabaseSchema fromJson(final String dbName, final JsonNode json) {
+        return DatabaseSchemaImpl.fromJson(dbName, json);
     }
 }

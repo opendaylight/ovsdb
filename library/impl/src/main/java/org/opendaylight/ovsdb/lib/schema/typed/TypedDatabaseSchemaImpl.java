c16a9b327961cfa6a907197cf96769e94921c665
Robert Varga <robert.varga@pantheon.tech>
2019-12-01 19:09:17 +0100

    Migrate TyperUtils methods to TypedDatabaseSchemaImpl
    TyperUtils are utterly stateless while TypedDatabaseSchema can
    provide schema binding. Migrate method implementatios there, leaving
    a simple lookup compability layer in TyperUtils.
    
    Change-Id: If533c8fd8fe0027e8ca67dcd474b8b92fbadd767
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>
    (cherry picked from commit 6ebb6f84f55a7dbc8a0474f8dc8687ffd3e92a08)

--- library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/TypedDatabaseSchemaImpl.java
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/TypedDatabaseSchemaImpl.java
@@ -9,7 +9,10 @@ package org.opendaylight.ovsdb.lib.schema.typed;
 
 import static java.util.Objects.requireNonNull;
 
+import com.google.common.reflect.Reflection;
+import java.util.HashMap;
 import java.util.Map;
+import org.opendaylight.ovsdb.lib.message.TableUpdate;
 import org.opendaylight.ovsdb.lib.message.TableUpdates;
 import org.opendaylight.ovsdb.lib.notation.Row;
 import org.opendaylight.ovsdb.lib.notation.UUID;
@@ -41,27 +44,88 @@ final class TypedDatabaseSchemaImpl extends ForwardingDatabaseSchema implements 
     }
 
     @Override
-    public <T> T getTypedRowWrapper(final Class<T> klazz) {
-        return TyperUtils.getTypedRowWrapper(this, klazz);
-    }
-
-    @Override
     public <T> T getTypedRowWrapper(final Class<T> klazz, final Row<GenericTableSchema> row) {
-        return TyperUtils.getTypedRowWrapper(this, klazz, row);
+        // Check validity of  of the parameter passed to getTypedRowWrapper:
+        // -  checks for a valid Database Schema matching the expected Database for a given table
+        // - checks for the presence of the Table in Database Schema.
+        final String dbName = TypedReflections.getTableDatabase(klazz);
+        if (dbName != null && !dbName.equalsIgnoreCase(getName())) {
+            return null;
+        }
+        TyperUtils.checkVersion(getVersion(), TypedReflections.getTableVersionRange(klazz));
+
+        if (row != null) {
+            row.setTableSchema(getTableSchema(klazz));
+        }
+        return Reflection.newProxy(klazz, new TypedRowInvocationHandler(klazz, this, row));
     }
 
     @Override
     public <T> Map<UUID, T> extractRowsOld(final Class<T> klazz, final TableUpdates updates) {
-        return TyperUtils.extractRowsOld(klazz, updates, this);
+        Map<UUID,TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema>> rowUpdates = extractRowUpdates(
+            requireNonNull(klazz), requireNonNull(updates));
+        Map<UUID,T> result = new HashMap<>();
+        for (TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema> rowUpdate : rowUpdates.values()) {
+            if (rowUpdate != null && rowUpdate.getOld() != null) {
+                Row<GenericTableSchema> row = rowUpdate.getOld();
+                result.put(rowUpdate.getUuid(), getTypedRowWrapper(klazz, row));
+            }
+        }
+        return result;
     }
 
     @Override
     public <T> Map<UUID, T> extractRowsUpdated(final Class<T> klazz, final TableUpdates updates) {
-        return TyperUtils.extractRowsUpdated(klazz, updates, this);
+        final Map<UUID,TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema>> rowUpdates =
+                extractRowUpdates(klazz, updates);
+        final Map<UUID,T> result = new HashMap<>();
+        for (TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema> rowUpdate : rowUpdates.values()) {
+            if (rowUpdate != null && rowUpdate.getNew() != null) {
+                Row<GenericTableSchema> row = rowUpdate.getNew();
+                result.put(rowUpdate.getUuid(), getTypedRowWrapper(klazz, row));
+            }
+        }
+        return result;
     }
 
     @Override
     public <T> Map<UUID, T> extractRowsRemoved(final Class<T> klazz, final TableUpdates updates) {
-        return TyperUtils.extractRowsRemoved(klazz, updates, this);
+        final Map<UUID, TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema>> rowUpdates =
+                extractRowUpdates(klazz, updates);
+        final Map<UUID, T> result = new HashMap<>();
+        for (TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema> rowUpdate : rowUpdates.values()) {
+            if (rowUpdate != null && rowUpdate.getNew() == null && rowUpdate.getOld() != null) {
+                Row<GenericTableSchema> row = rowUpdate.getOld();
+                result.put(rowUpdate.getUuid(), getTypedRowWrapper(klazz, row));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * This method extracts all RowUpdates of Class&lt;T&gt; klazz from a TableUpdates that correspond to rows of type
+     * klazz. Example:
+     * <code>
+     * Map&lt;UUID,TableUpdate&lt;GenericTableSchema&gt;.RowUpdate&lt;GenericTableSchema&gt;&gt; updatedBridges =
+     *     extractRowsUpdates(Bridge.class,updates,dbSchema)
+     * </code>
+     *
+     * @param klazz Class for row type to be extracted
+     * @param updates TableUpdates from which to extract rowUpdates
+     * @return Map&lt;UUID,TableUpdate&lt;GenericTableSchema&gt;.RowUpdate&lt;GenericTableSchema&gt;&gt;
+     *     for the type of things being sought
+     */
+    private Map<UUID,TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema>> extractRowUpdates(
+            final Class<?> klazz, final TableUpdates updates) {
+        TableUpdate<GenericTableSchema> update = updates.getUpdate(table(TypedReflections.getTableName(klazz),
+            GenericTableSchema.class));
+        Map<UUID, TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema>> result = new HashMap<>();
+        if (update != null) {
+            Map<UUID, TableUpdate<GenericTableSchema>.RowUpdate<GenericTableSchema>> rows = update.getRows();
+            if (rows != null) {
+                result = rows;
+            }
+        }
+        return result;
     }
 }

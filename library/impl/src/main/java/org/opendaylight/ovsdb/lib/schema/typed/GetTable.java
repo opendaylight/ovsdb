729ea791507df5af70759a5a9cc4cfae9fc2d6b5
Robert Varga <robert.varga@pantheon.tech>
2019-12-01 14:34:50 +0100

    WIP: add MethodInvoker
    Change-Id: Ic97751992fea97b01a6fcfd124edbe783bc86e1c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

---
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/GetTable.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright © 2019 PANTHEON.tech, s.r.o. and others. All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.ovsdb.lib.schema.typed;
+
+import com.google.common.collect.Range;
+import org.eclipse.jdt.annotation.Nullable;
+import org.opendaylight.ovsdb.lib.notation.Row;
+import org.opendaylight.ovsdb.lib.schema.GenericTableSchema;
+
+public final class GetTable extends MethodDispatch.TablePrototype {
+    private static final class Invoker extends MethodDispatch.TableInvoker {
+        Invoker(final GenericTableSchema tableSchema) {
+            super(tableSchema);
+        }
+
+        @Override
+        @Nullable
+        Object invokeMethod(final Row<GenericTableSchema> row, final Object proxy, final Object[] args) {
+            return tableSchema();
+        }
+    }
+
+    GetTable(final String tableName) {
+        super(Range.all(), tableName);
+    }
+
+    @Override
+    Invoker bindToImpl(final TypedDatabaseSchema dbSchema) {
+        return new Invoker(findTableSchema(dbSchema));
+    }
+}
+

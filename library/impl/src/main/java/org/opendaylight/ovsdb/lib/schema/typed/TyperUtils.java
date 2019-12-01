a44d5429ce601754933fe69022bcc141a66bd593
Robert Varga <robert.varga@pantheon.tech>
2019-12-01 14:34:50 +0100

    Rework TypedRowInvocationHandler invocation path
    Performing schema lookup/validation is something we usually do only
    once for a particular schema and then we can freely perform invocation.
    
    Introduce MethodDispatch{Invoker,Prototype} constructs. MethodDispatch
    serves as a the result of reflective analysis of a particular access
    class.
    
    When TypedDatabaseSchema accesses that class, the MethodDispatch
    is loaded from a global cache and is bound to the schema, resulting
    in an Invoker map. That map is then used to dispatch InvocationHandler
    requests to the appropriate logic.
    
    Change-Id: Ic97751992fea97b01a6fcfd124edbe783bc86e1c
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/TyperUtils.java
+++ library/impl/src/main/java/org/opendaylight/ovsdb/lib/schema/typed/TyperUtils.java
@@ -49,16 +49,14 @@ public final class TyperUtils {
         return dbSchema.table(TypedReflections.getTableName(klazz), GenericTableSchema.class);
     }
 
-    static ColumnSchema<GenericTableSchema, Object> getColumnSchema(final GenericTableSchema tableSchema,
-            final String columnName, final Class<Object> metaClass) {
+    static <T> ColumnSchema<GenericTableSchema, T> getColumnSchema(final GenericTableSchema tableSchema,
+            final String columnName, final Class<T> metaClass) {
         return tableSchema.column(columnName, metaClass);
     }
 
     static void checkVersion(final Version schemaVersion, final Range<Version> range) {
         if (!range.contains(schemaVersion)) {
-            throw new SchemaVersionMismatchException(schemaVersion,
-                range.hasLowerBound() ? range.lowerEndpoint() : Version.NULL,
-                        range.hasUpperBound() ? range.upperEndpoint() : Version.NULL);
+            throw new SchemaVersionMismatchException(schemaVersion, range);
         }
     }
 

6675dac2829a8d979c89d71dea189f0e5a83be2f
Robert Varga <robert.varga@pantheon.tech>
2019-12-05 22:41:22 +0100

    Use ArrayDeque for TransactionInvokerImpl.pendingTransactions
    Rework the copy logic to operate on pure Iterator, disconnecting us
    from the List API. This makes the choice of interface/implementation
    a much broader one -- and pick Queue/ArrayDeque. It is a much better
    structure for the job at hand.
    
    JIRA: OVSDB-428
    Change-Id: I22a4da6f4194d10e0932470bf2054441a9c1d3c1
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/TransactionInvokerImpl.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/TransactionInvokerImpl.java
@@ -13,11 +13,14 @@ import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.MoreExecutors;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import com.google.errorprone.annotations.concurrent.GuardedBy;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -48,7 +51,7 @@ public class TransactionInvokerImpl implements TransactionInvoker,TransactionCha
     @GuardedBy("this")
     private final Map<ReadWriteTransaction, TransactionCommand> transactionToCommand = new HashMap<>();
     @GuardedBy("this")
-    private final List<ReadWriteTransaction> pendingTransactions = new ArrayList<>();
+    private final Queue<ReadWriteTransaction> pendingTransactions = new ArrayDeque<>();
 
     private BindingTransactionChain chain;
 
@@ -162,12 +165,17 @@ public class TransactionInvokerImpl implements TransactionInvoker,TransactionCha
         AsyncTransaction<?, ?> transaction = failedTransactionQueue.poll();
         List<TransactionCommand> commands = new ArrayList<>();
         if (transaction != null) {
-            int index = pendingTransactions.lastIndexOf(transaction);
-            List<ReadWriteTransaction> transactions =
-                    pendingTransactions.subList(index, pendingTransactions.size() - 1);
-            for (ReadWriteTransaction tx: transactions) {
-                commands.add(transactionToCommand.get(tx));
+            // Process all pending transactions, looking for the failed one...
+            final Iterator<ReadWriteTransaction> it = pendingTransactions.iterator();
+            while (it.hasNext()) {
+                final ReadWriteTransaction current = it.next();
+                if (transaction.equals(current)) {
+                    // .. collect all remaining pending transactions
+                    it.forEachRemaining(tx -> commands.add(transactionToCommand.get(tx)));
+                    break;
+                }
             }
+
             resetTransactionQueue();
         }
         return commands;

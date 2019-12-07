42092ce3ebb7be154b177127f3c95c47a38f0c54
Robert Varga <robert.varga@pantheon.tech>
2019-12-07 12:49:49 +0100

    WIP: Add TransactionComponent interface
    Current structure of command invocation leaves some things to be
    desired.
    
    First of all, we have a set of command classes, which are executed
    from the aggregator, but never alone. These share the same hierarchy
    as the aggregator and 'real' commands. While this allows for UT on
    individual transitions, the interface is inefficient as the production
    code ends up analyzing the updates multiple time.
    
    We introduce TransactionComponent, which is a stateless base class,
    hosting some logic that depends on certain input tables. Input tables
    are exposed from the component and it is the caller's responsibility
    to provide those tables (in a manner which is amenable to caching).
    
    // FIXME: add TranactionContext to hold lazy updates + transaction + whatever
    // FIXME: update command invocation code
    // FIXME: cleanup test classes
    
    Change-Id: I3236c06ea65f3771c2ca24ecf339dfc196f15e0b
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbOperationalCommandAggregator.java
+++ southbound/southbound-impl/src/main/java/org/opendaylight/ovsdb/southbound/transactions/md/OvsdbOperationalCommandAggregator.java
@@ -5,16 +5,22 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.ovsdb.southbound.transactions.md;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import org.opendaylight.controller.md.sal.binding.api.ReadWriteTransaction;
+import org.opendaylight.ovsdb.lib.message.TableUpdate.RowUpdate;
 import org.opendaylight.ovsdb.lib.message.TableUpdates;
+import org.opendaylight.ovsdb.lib.notation.UUID;
 import org.opendaylight.ovsdb.lib.notation.Version;
 import org.opendaylight.ovsdb.lib.schema.DatabaseSchema;
+import org.opendaylight.ovsdb.lib.schema.GenericTableSchema;
+import org.opendaylight.ovsdb.lib.schema.typed.TypedBaseTable;
 import org.opendaylight.ovsdb.southbound.InstanceIdentifierCodec;
 import org.opendaylight.ovsdb.southbound.OvsdbConnectionInstance;
 import org.opendaylight.ovsdb.southbound.SouthboundConstants;
@@ -22,58 +28,81 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class OvsdbOperationalCommandAggregator implements TransactionCommand {
-
     private static final Logger LOG = LoggerFactory.getLogger(OvsdbOperationalCommandAggregator.class);
-    private final List<TransactionCommand> commands = new ArrayList<>();
 
-    public OvsdbOperationalCommandAggregator(InstanceIdentifierCodec instanceIdentifierCodec,
-            OvsdbConnectionInstance key, TableUpdates updates, DatabaseSchema dbSchema) {
-        commands.add(new OpenVSwitchUpdateCommand(instanceIdentifierCodec, key, updates, dbSchema));
-        commands.add(new OvsdbManagersUpdateCommand(key, updates,  dbSchema));
-        commands.add(new OvsdbManagersRemovedCommand(key, updates,  dbSchema));
-        commands.add(new OvsdbQosUpdateCommand(instanceIdentifierCodec, key, updates,  dbSchema));
-        commands.add(new OvsdbQosRemovedCommand(key, updates,  dbSchema));
-        commands.add(new OvsdbQueueUpdateCommand(instanceIdentifierCodec, key, updates,  dbSchema));
-        commands.add(new OvsdbQueueRemovedCommand(key, updates,  dbSchema));
-        commands.add(new OvsdbBridgeUpdateCommand(instanceIdentifierCodec, key, updates,  dbSchema));
-        commands.add(new OvsdbBridgeRemovedCommand(instanceIdentifierCodec, key, updates,  dbSchema));
-        commands.add(new OvsdbControllerUpdateCommand(key, updates,  dbSchema));
-        commands.add(new OvsdbControllerRemovedCommand(instanceIdentifierCodec, key, updates,  dbSchema));
-        commands.add(new OvsdbPortUpdateCommand(instanceIdentifierCodec, key, updates, dbSchema));
-        commands.add(new OvsdbPortRemoveCommand(instanceIdentifierCodec, key, updates, dbSchema));
+    private static final ImmutableList<AbstractTransactionComponent> BASE = ImmutableList.of(
+        new OpenVSwitchUpdateCommand(),
+        new OvsdbManagersUpdateCommand(),
+        new OvsdbManagersRemovedCommand(),
+        new OvsdbQosUpdateCommand(),
+        new OvsdbQosRemovedCommand(),
+        new OvsdbQueueUpdateCommand(),
+        new OvsdbQueueRemovedCommand(),
+        new OvsdbBridgeUpdateCommand(),
+        new OvsdbBridgeRemovedCommand(),
+        new OvsdbControllerUpdateCommand(),
+        new OvsdbControllerRemovedCommand(),
+        new OvsdbPortUpdateCommand(),
+        new OvsdbPortRemoveCommand());
+    private static final ImmutableList<AbstractTransactionComponent> AUTOATTACH =
+            ImmutableList.<AbstractTransactionComponent>builderWithExpectedSize(BASE.size() + 2)
+                .addAll(BASE)
+                .add(new OvsdbAutoAttachUpdateCommand())
+                .add(new OvsdbAutoAttachRemovedCommand())
+                .build();
+    private static final ImmutableSet<Class<? extends TypedBaseTable<?>>> BASE_TABLES = BASE.stream()
+            .flatMap(component -> component.getInputTableTypes().stream())
+            .collect(ImmutableSet.toImmutableSet());
+    private static final ImmutableSet<Class<? extends TypedBaseTable<?>>> AUTOATTACH_TABLES = AUTOATTACH.stream()
+            .flatMap(component -> component.getInputTableTypes().stream())
+            .collect(ImmutableSet.toImmutableSet());
 
+    private final ImmutableSet<Class<? extends TypedBaseTable<?>>> inputTableTypes;
+    private final ImmutableList<AbstractTransactionComponent> components;
+
+    public OvsdbOperationalCommandAggregator(final InstanceIdentifierCodec instanceIdentifierCodec,
+            final OvsdbConnectionInstance key, final TableUpdates updates, final DatabaseSchema dbSchema) {
         if (dbSchema.getVersion().compareTo(
-                Version.fromString(SouthboundConstants.AUTOATTACH_SUPPORTED_OVS_SCHEMA_VERSION)) >= 0) {
-            commands.add(new OvsdbAutoAttachUpdateCommand(key, updates, dbSchema));
-            commands.add(new OvsdbAutoAttachRemovedCommand(key, updates, dbSchema));
+            Version.fromString(SouthboundConstants.AUTOATTACH_SUPPORTED_OVS_SCHEMA_VERSION)) >= 0) {
+            inputTableTypes = AUTOATTACH_TABLES;
+            components = AUTOATTACH;
         } else {
             LOG.debug("UNSUPPORTED FUNCTIONALITY: AutoAttach not supported in OVS schema version {}",
-                    dbSchema.getVersion().toString());
+                dbSchema.getVersion());
+            inputTableTypes = BASE_TABLES;
+            components = BASE;
         }
     }
 
+    @VisibleForTesting
+    OvsdbOperationalCommandAggregator(final InstanceIdentifierCodec instanceIdentifierCodec,
+            final OvsdbConnectionInstance key, final TableUpdates updates, final DatabaseSchema dbSchema,
+            final AbstractTransactionComponent component) {
+        inputTableTypes = component.getInputTableTypes();
+        components = ImmutableList.of(component);
+    }
+
     @Override
-    public void execute(ReadWriteTransaction transaction) {
-        for (TransactionCommand command: commands) {
+    public void execute(final ReadWriteTransaction transaction) {
+        final Map<Class<? extends TypedBaseTable<?>>, Map<UUID, RowUpdate<GenericTableSchema>>> rowUpdates
+                = new HashMap<>();
+
+        for (AbstractTransactionComponent component : components) {
             try {
-                command.execute(transaction);
+                component.execute(transaction);
             } catch (NullPointerException | NoSuchElementException | ClassCastException e) {
-                LOG.warn("Exception trying to execute {}", command, e);
+                LOG.warn("Exception trying to execute component {}", component, e);
             }
         }
     }
 
     @Override
     public void onSuccess() {
-        for (TransactionCommand command: commands) {
-            command.onSuccess();
-        }
+        // FIXME: log something meaningful
     }
 
     @Override
-    public void onFailure(Throwable throwable) {
-        for (TransactionCommand command: commands) {
-            command.onFailure(throwable);
-        }
+    public void onFailure(final Throwable throwable) {
+        // FIXME: log the failure
     }
 }

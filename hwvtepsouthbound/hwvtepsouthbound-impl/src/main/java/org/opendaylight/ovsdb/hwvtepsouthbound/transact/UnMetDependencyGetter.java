a5a57ce0a027ea7b13794b9e2f55b2c969d85182
Robert Varga <robert.varga@pantheon.tech>
2020-03-07 11:01:25 +0100

    Bump odlparent/yangtools/mdsal to 5.0.6/3.0.10/4.0.12
    This picks up latest upgrades, which necessitate a few fixes for
    checkstyle violations.
    
    Change-Id: I6b652ac2a38d18d8193798cd274f5fdd5dde963f
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transact/UnMetDependencyGetter.java
+++ hwvtepsouthbound/hwvtepsouthbound-impl/src/main/java/org/opendaylight/ovsdb/hwvtepsouthbound/transact/UnMetDependencyGetter.java
@@ -5,17 +5,14 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.ovsdb.hwvtepsouthbound.transact;
 
 import com.google.common.base.Optional;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.controller.md.sal.binding.api.ReadOnlyTransaction;
 import org.opendaylight.controller.md.sal.common.api.data.LogicalDatastoreType;
@@ -31,7 +28,6 @@ import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
  * Utility class to retrieve the unmet dependencies (config/operational) of the given object.
  */
 public abstract class UnMetDependencyGetter<T extends Identifiable> {
-
     private final ConfigDependencyGetter configDependencyGetter = new ConfigDependencyGetter();
     private final InTransitDependencyGetter inTransitDependencyGetter = new InTransitDependencyGetter();
 

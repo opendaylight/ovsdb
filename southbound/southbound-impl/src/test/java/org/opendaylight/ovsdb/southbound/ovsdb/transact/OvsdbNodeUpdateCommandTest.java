7bff6de54b2a2da2fae38cde00ee8a584ed773dd
Robert Varga <robert.varga@pantheon.tech>
2021-11-02 18:11:14 +0100

    WIP: Ditch powermock from southbound-impl
    We can easily mock construction here, cleanup let's not rely on
    powermock.
    
    Change-Id: If7bae44a0b4fb2c74937398ab21c91530bee5161
    Signed-off-by: Robert Varga <robert.varga@pantheon.tech>

--- southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/OvsdbNodeUpdateCommandTest.java
+++ southbound/southbound-impl/src/test/java/org/opendaylight/ovsdb/southbound/ovsdb/transact/OvsdbNodeUpdateCommandTest.java
@@ -5,29 +5,28 @@
  * terms of the Eclipse Public License v1.0 which accompanies this distribution,
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
-
 package org.opendaylight.ovsdb.southbound.ovsdb.transact;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.google.common.collect.ImmutableMap;
-import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.opendaylight.ovsdb.lib.notation.Column;
 import org.opendaylight.ovsdb.lib.operations.Mutate;
-import org.opendaylight.ovsdb.lib.operations.Operation;
 import org.opendaylight.ovsdb.lib.operations.Operations;
 import org.opendaylight.ovsdb.lib.operations.TransactionBuilder;
 import org.opendaylight.ovsdb.lib.schema.ColumnSchema;
@@ -43,88 +42,80 @@ import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.re
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.OpenvswitchOtherConfigs;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.ovsdb.rev150105.ovsdb.node.attributes.OpenvswitchOtherConfigsBuilder;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.api.support.membermodification.MemberMatcher;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({TransactUtils.class, OvsdbNodeUpdateCommand.class, InstanceIdentifier.class, Operations.class})
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
 public class OvsdbNodeUpdateCommandTest {
-
     private static final String EXTERNAL_ID_KEY = "external id key";
     private static final String EXTERNAL_ID_VALUE = "external id value";
     private static final String OTHER_CONFIG_KEY = "other config key";
     private static final String OTHER_CONFIG_VALUE = "other config value";
 
-    @Mock private DataChangeEvent changes;
+    @Mock
+    private DataChangeEvent changes;
     private OvsdbNodeUpdateCommand ovsdbNodeUpdateCommand;
 
     @Before
     public void setUp() {
-        ovsdbNodeUpdateCommand = mock(OvsdbNodeUpdateCommand.class, Mockito.CALLS_REAL_METHODS);
+        ovsdbNodeUpdateCommand = spy(OvsdbNodeUpdateCommand.class);
     }
 
-    @SuppressWarnings("unchecked")
     @Test
+    @SuppressWarnings("unchecked")
     public void testExecute() throws Exception {
         Map<InstanceIdentifier<OvsdbNodeAugmentation>, OvsdbNodeAugmentation> updated = new HashMap<>();
         InstanceIdentifier<OvsdbNodeAugmentation> iid = mock(InstanceIdentifier.class);
         OvsdbNodeAugmentation ovsdbNode = mock(OvsdbNodeAugmentation.class);
         updated.put(iid, ovsdbNode);
-        PowerMockito.mockStatic(TransactUtils.class);
-        PowerMockito.when(
-                TransactUtils.extractCreatedOrUpdated(any(DataChangeEvent.class), eq(OvsdbNodeAugmentation.class)))
-                .thenReturn(updated);
 
-        ConnectionInfo connectionInfo = mock(ConnectionInfo.class);
-        when(ovsdbNode.getConnectionInfo()).thenReturn(connectionInfo);
-        when(connectionInfo.getRemoteIp()).thenReturn(mock(IpAddress.class));
-        when(connectionInfo.getRemotePort()).thenReturn(mock(PortNumber.class));
+        try (var utils = mockStatic(TransactUtils.class)) {
+            utils.when(() -> TransactUtils.extractCreatedOrUpdated(any(DataChangeEvent.class),
+                eq(OvsdbNodeAugmentation.class))).thenReturn(updated);
 
-        OpenVSwitch ovs = mock(OpenVSwitch.class);
-        TransactionBuilder transaction = mock(TransactionBuilder.class);
-        when(transaction.getTypedRowWrapper(eq(OpenVSwitch.class))).thenReturn(ovs);
+            ConnectionInfo connectionInfo = mock(ConnectionInfo.class);
+            when(ovsdbNode.getConnectionInfo()).thenReturn(connectionInfo);
+            when(connectionInfo.getRemoteIp()).thenReturn(mock(IpAddress.class));
+            when(connectionInfo.getRemotePort()).thenReturn(mock(PortNumber.class));
 
+            OpenVSwitch ovs = mock(OpenVSwitch.class);
+            TransactionBuilder transaction = mock(TransactionBuilder.class);
+            when(transaction.getTypedRowWrapper(eq(OpenVSwitch.class))).thenReturn(ovs);
 
-        OpenvswitchExternalIds externalId = new OpenvswitchExternalIdsBuilder()
+            OpenvswitchExternalIds externalId = new OpenvswitchExternalIdsBuilder()
                 .setExternalIdKey(EXTERNAL_ID_KEY)
                 .setExternalIdValue(EXTERNAL_ID_VALUE)
                 .build();
-        when(ovsdbNode.getOpenvswitchExternalIds()).thenReturn(Map.of(externalId.key(), externalId));
-        PowerMockito.suppress(MemberMatcher.method(OvsdbNodeUpdateCommand.class, "stampInstanceIdentifier",
-                TransactionBuilder.class, InstanceIdentifier.class, InstanceIdentifierCodec.class));
-        PowerMockito.suppress(MemberMatcher.methodsDeclaredIn(InstanceIdentifier.class));
-        doNothing().when(ovs).setExternalIds(any(ImmutableMap.class));
+            when(ovsdbNode.getOpenvswitchExternalIds()).thenReturn(Map.of(externalId.key(), externalId));
+            doNothing().when(ovs).setExternalIds(anyMap());
 
-        Mutate<GenericTableSchema> mutate = mock(Mutate.class);
-        Operations op = setOpField();
-        Column<GenericTableSchema, Map<String, String>> column = mock(Column.class);
-        when(ovs.getExternalIdsColumn()).thenReturn(column);
-        when(column.getSchema()).thenReturn(mock(ColumnSchema.class));
-        when(column.getData()).thenReturn(new HashMap<>());
-        when(op.mutate(any(OpenVSwitch.class))).thenReturn(mutate);
-        when(transaction.add(any(Operation.class))).thenReturn(transaction);
+            Mutate<GenericTableSchema> mutate = mock(Mutate.class);
+            Operations op = setOpField();
+            Column<GenericTableSchema, Map<String, String>> column = mock(Column.class);
+            when(ovs.getExternalIdsColumn()).thenReturn(column);
+            when(column.getSchema()).thenReturn(mock(ColumnSchema.class));
+            when(column.getData()).thenReturn(new HashMap<>());
+            when(op.mutate(any(OpenVSwitch.class))).thenReturn(mutate);
+            when(transaction.add(null)).thenReturn(transaction);
 
-        OpenvswitchOtherConfigs otherConfig = new OpenvswitchOtherConfigsBuilder()
+            OpenvswitchOtherConfigs otherConfig = new OpenvswitchOtherConfigsBuilder()
                 .setOtherConfigKey(OTHER_CONFIG_KEY)
                 .setOtherConfigValue(OTHER_CONFIG_VALUE)
                 .build();
 
-        when(ovsdbNode.getOpenvswitchOtherConfigs()).thenReturn(Map.of(otherConfig.key(), otherConfig));
-        doNothing().when(ovs).setOtherConfig(any(ImmutableMap.class));
-        when(ovs.getOtherConfigColumn()).thenReturn(column);
+            when(ovsdbNode.getOpenvswitchOtherConfigs()).thenReturn(Map.of(otherConfig.key(), otherConfig));
+            doNothing().when(ovs).setOtherConfig(anyMap());
+            when(ovs.getOtherConfigColumn()).thenReturn(column);
 
-        ovsdbNodeUpdateCommand.execute(transaction, mock(BridgeOperationalState.class), changes,
+            ovsdbNodeUpdateCommand.execute(transaction, mock(BridgeOperationalState.class), changes,
                 mock(InstanceIdentifierCodec.class));
-        verify(ovs, times(2)).getExternalIdsColumn();
-        verify(transaction, times(2)).add(eq(null));
+            verify(ovs, times(3)).getExternalIdsColumn();
+            verify(transaction, times(2)).add(eq(null));
+        }
     }
 
-    static Operations setOpField() throws Exception {
-        Field opField = PowerMockito.field(Operations.class, "op");
+    static Operations setOpField() {
         Operations mockOp = mock(Operations.class);
-        opField.set(Operations.class, mockOp);
+        Whitebox.setInternalState(Operations.class, "op", mockOp);
         return mockOp;
     }
 }
